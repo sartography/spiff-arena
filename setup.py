6524778a763e7e1579a907288ee958c688a7524f
burnettk <burnettk@users.noreply.github.com>
2022-10-19 10:31:33 -0400

    Squashed 'SpiffWorkflow/' changes from d9fcd45a3..a094adad8
    a094adad8 Prep for release 1.2.1 - Minor fix for a dependency in Python 3.10
    b9e3dd80e Merge pull request #253 from sartography/feature/avoid_importlib_for_python_3.8_up
    df311639c do not install importlib-metadata if greater than python 3.7 w/ burnettk
    
    git-subtree-dir: SpiffWorkflow
    git-subtree-split: a094adad8767f82e9c5fa806a46597e066252a72

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(name='SpiffWorkflow',
-      version='1.2.0',
+      version='1.2.1',
       description='A workflow framework and BPMN/DMN Processor',
       long_description=README,
       long_description_content_type="text/markdown",
@@ -23,7 +23,7 @@ setup(name='SpiffWorkflow',
       packages=find_packages(exclude=['tests', 'tests.*']),
       install_requires=['configparser', 'lxml', 'celery', 'dateparser', 'pytz',
           # required for python 3.7 - https://stackoverflow.com/a/73932581
-          'importlib-metadata<5.0'],
+          'importlib-metadata<5.0; python_version <= "3.7"'],
       keywords='spiff workflow bpmn engine',
       url='https://github.com/sartography/SpiffWorkflow',
       classifiers=[
