fe54014107eb8a5c79e9baec069adccb98560496
jasquat <jasquat@users.noreply.github.com>
2022-10-31 11:50:14 -0400

    added script to get the frontend url w/ burnettk

---
+++ spiffworkflow-backend/src/spiffworkflow_backend/scripts/get_frontend_url.py
@@ -0,0 +1,25 @@
+"""Get_env."""
+from typing import Any
+from flask import current_app
+
+from spiffworkflow_backend.models.script_attributes_context import (
+    ScriptAttributesContext,
+)
+from spiffworkflow_backend.scripts.script import Script
+
+
+class GetFrontendUrl(Script):
+    """GetFrontendUrl."""
+
+    def get_description(self) -> str:
+        """Get_description."""
+        return """Return the url to the frontend."""
+
+    def run(
+        self,
+        script_attributes_context: ScriptAttributesContext,
+        *args: Any,
+        **kwargs: Any
+    ) -> Any:
+        """Run."""
+        return current_app.config['SPIFFWORKFLOW_FRONTEND_URL']
