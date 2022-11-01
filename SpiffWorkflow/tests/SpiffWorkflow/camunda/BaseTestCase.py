f388c9a34ea352eb3525d27ec757b91919aede56
Jon Herron <jon.herron@yahoo.com>
2022-11-01 17:08:45 -0400

    Clean out another __init__.py

--- SpiffWorkflow/tests/SpiffWorkflow/camunda/BaseTestCase.py
+++ SpiffWorkflow/tests/SpiffWorkflow/camunda/BaseTestCase.py
@@ -3,7 +3,7 @@ import os
 
 from SpiffWorkflow.bpmn.serializer.workflow import BpmnWorkflowSerializer
 from SpiffWorkflow.camunda.parser.CamundaParser import CamundaParser
-from SpiffWorkflow.camunda.serializer import UserTaskConverter, StartEventConverter, EndEventConverter, \
+from SpiffWorkflow.camunda.serializer.task_spec_converters import UserTaskConverter, StartEventConverter, EndEventConverter, \
     IntermediateCatchEventConverter, IntermediateThrowEventConverter, BoundaryEventConverter
 
 from SpiffWorkflow.dmn.serializer import BusinessRuleTaskConverter
