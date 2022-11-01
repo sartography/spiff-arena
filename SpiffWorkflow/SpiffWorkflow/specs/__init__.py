8cbd38508bf50c976023fd82e1c729ff0ebcbdd8
Jon Herron <jon.herron@yahoo.com>
2022-11-01 19:00:48 -0400

    Chipping away at the last one

--- SpiffWorkflow/SpiffWorkflow/specs/__init__.py
+++ SpiffWorkflow/SpiffWorkflow/specs/__init__.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
 
-#from .base import TaskSpec
-from .AcquireMutex import AcquireMutex
-from .Cancel import Cancel
-from .CancelTask import CancelTask
-from .Celery import Celery
-from .Choose import Choose
-from .ExclusiveChoice import ExclusiveChoice
-from .Execute import Execute
-from .Gate import Gate
-from .Join import Join
 from .Merge import Merge
 from .MultiChoice import MultiChoice
 from .MultiInstance import MultiInstance
