e713633060839e92983f76487698de435e2a739a
Jon Herron <jon.herron@yahoo.com>
2022-11-09 15:20:27 -0500

    Getting ./bin/pyl to pass

--- spiffworkflow-backend/src/spiffworkflow_backend/models/spiff_logging.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/models/spiff_logging.py
@@ -4,10 +4,6 @@ from typing import Optional
 
 from flask_bpmn.models.db import db
 from flask_bpmn.models.db import SpiffworkflowBaseDBModel
-from sqlalchemy import ForeignKey
-
-from spiffworkflow_backend.models.process_instance import ProcessInstanceModel
-from spiffworkflow_backend.models.user import UserModel
 
 
 @dataclass
@@ -16,7 +12,7 @@ class SpiffLoggingModel(SpiffworkflowBaseDBModel):
 
     __tablename__ = "spiff_logging"
     id: int = db.Column(db.Integer, primary_key=True)
-    process_instance_id: int = db.Column(db.Integer, nullable=False)  # type: ignore
+    process_instance_id: int = db.Column(db.Integer, nullable=False)
     bpmn_process_identifier: str = db.Column(db.String(255), nullable=False)
     bpmn_task_identifier: str = db.Column(db.String(255), nullable=False)
     bpmn_task_name: str = db.Column(db.String(255), nullable=True)
