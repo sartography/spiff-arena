21b6c947a8a2c74781a7c7efcfc5219c8cbfb74b
mike cullerton <michaelc@cullerton.com>
2022-11-09 15:45:45 -0500

    Nested group UI stuff

--- spiffworkflow-frontend/src/helpers.tsx
+++ spiffworkflow-frontend/src/helpers.tsx
@@ -117,11 +117,11 @@ export const truncateString = (text: string, len: number) => {
 // Because of limitations in the way openapi defines parameters, we have to modify process models ids
 // which are basically paths to the models
 export const modifyProcessModelPath = (path: String) => {
-  return path.replace('/', ':') || '';
+  return path.replace(/\//g, ':') || '';
 };
 
 export const unModifyProcessModelPath = (path: String) => {
-  return path.replace(':', '/') || '';
+  return path.replace(/:/g, '/') || '';
 };
 
 export const getGroupFromModifiedModelId = (modifiedId: String) => {
