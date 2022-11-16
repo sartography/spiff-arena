17e660530631a93cf86386aaed1a3554939318ec
jasquat <jasquat@users.noreply.github.com>
2022-11-16 17:12:01 -0500

    fixed error messaging a little bit for forms w/ burnettk cullerton

--- spiffworkflow-frontend/src/themes/carbon/FieldErrorTemplate/FieldErrorTemplate.tsx
+++ spiffworkflow-frontend/src/themes/carbon/FieldErrorTemplate/FieldErrorTemplate.tsx
@@ -1,29 +1,9 @@
-import React from 'react';
 import { FieldErrorProps } from '@rjsf/utils';
-import ListItem from '@mui/material/ListItem';
-import FormHelperText from '@mui/material/FormHelperText';
-import List from '@mui/material/List';
 
 /** The `FieldErrorTemplate` component renders the errors local to the particular field
  *
  * @param props - The `FieldErrorProps` for the errors being rendered
  */
-export default function FieldErrorTemplate(props: FieldErrorProps) {
-  const { errors = [], idSchema } = props;
-  if (errors.length === 0) {
-    return null;
-  }
-  const id = `${idSchema.$id}__error`;
-
-  return (
-    <List dense disablePadding>
-      {errors.map((error, i: number) => {
-        return (
-          <ListItem key={i} disableGutters>
-            <FormHelperText id={id}>{error}</FormHelperText>
-          </ListItem>
-        );
-      })}
-    </List>
-  );
+export default function FieldErrorTemplate(_props: FieldErrorProps) {
+  return null;
 }
