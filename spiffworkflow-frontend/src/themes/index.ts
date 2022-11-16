ddce5fcec1d7f8920f513318ec6c1369ff704655
jasquat <jasquat@users.noreply.github.com>
2022-11-16 13:52:44 -0500

    theme working with mui from the internet w/ burnettk cullerton

--- spiffworkflow-frontend/src/themes/index.ts
+++
@@ -1,8 +0,0 @@
-import Form from './Form/Form';
-
-export { default as Form } from './Form';
-export { default as Templates } from './Templates';
-export { default as Theme } from './Theme';
-export { default as Widgets } from './Widgets';
-
-export default Form;
