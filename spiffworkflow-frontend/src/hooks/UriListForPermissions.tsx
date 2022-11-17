d3019ac6396d49f6842ebb8870f1d68d5139c37d
jasquat <jasquat@users.noreply.github.com>
2022-11-17 17:30:51 -0500

    hide messages and configuration if not authorized w/ burnettk

--- spiffworkflow-frontend/src/hooks/UriListForPermissions.tsx
+++ spiffworkflow-frontend/src/hooks/UriListForPermissions.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 export const useUriListForPermissions = () => {
   const params = useParams();
   const targetUris = {
+    authenticationListPath: `/v1.0/authentications`,
     messageInstanceListPath: '/v1.0/messages',
     processGroupListPath: '/v1.0/process-groups',
     processGroupShowPath: `/v1.0/process-groups/${params.process_group_id}`,
@@ -12,6 +13,7 @@ export const useUriListForPermissions = () => {
     processModelFileCreatePath: `/v1.0/process-models/${params.process_model_id}/files`,
     processModelFileShowPath: `/v1.0/process-models/${params.process_model_id}/files/${params.file_name}`,
     processModelShowPath: `/v1.0/process-models/${params.process_model_id}`,
+    secretListPath: `/v1.0/secrets`,
   };
 
   return { targetUris };
