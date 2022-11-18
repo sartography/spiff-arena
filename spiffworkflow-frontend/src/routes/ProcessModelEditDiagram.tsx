72922dfe2a10b59b34996f8bccd952efeadfb254
jasquat <jasquat@users.noreply.github.com>
2022-11-18 15:01:41 -0500

    load diagram page even if processes are not set w/ burnettk cullerton

--- spiffworkflow-frontend/src/routes/ProcessModelEditDiagram.tsx
+++ spiffworkflow-frontend/src/routes/ProcessModelEditDiagram.tsx
@@ -838,9 +838,7 @@ export default function ProcessModelEditDiagram() {
 
   // if a file name is not given then this is a new model and the ReactDiagramEditor component will handle it
   if (
-    (bpmnXmlForDiagramRendering || !params.file_name) &&
-    processModel &&
-    processes.length > 0
+    (bpmnXmlForDiagramRendering || !params.file_name) && processModel
   ) {
     const processModelFileName = processModelFile ? processModelFile.name : '';
     return (
