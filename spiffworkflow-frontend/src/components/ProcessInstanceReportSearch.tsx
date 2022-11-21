8c465fdcce1246e79c6c9d82e20b5624921c7e26
Jon Herron <jon.herron@yahoo.com>
2022-11-21 08:57:03 -0500

    Report search component

---
+++ spiffworkflow-frontend/src/components/ProcessInstanceReportSearch.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import {
+  ComboBox,
+  // @ts-ignore
+} from '@carbon/react';
+import { truncateString } from '../helpers';
+import { ProcessInstanceReport } from '../interfaces';
+import HttpService from '../services/HttpService';
+
+type OwnProps = {
+  onChange: (..._args: any[]) => any;
+  selectedItem?: ProcessInstanceReport | null;
+  titleText?: string;
+};
+
+export default function ProcessInstanceReportSearch({
+  selectedItem,
+  onChange,
+  titleText = 'Process instance reports',
+}: OwnProps) {
+  const [processInstanceReports, setProcessInstanceReports] = useState<
+    ProcessInstanceReport[]
+  >([]);
+
+  function setProcessInstanceReportsFromResult(result: any) {
+    const processInstanceReportsFromApi = result.map((item: any) => {
+      return { id: item.identifier, display_name: item.identifier };
+    });
+    setProcessInstanceReports(processInstanceReportsFromApi);
+  }
+  HttpService.makeCallToBackend({
+    path: `/process-instances/reports`,
+    successCallback: setProcessInstanceReportsFromResult,
+  });
+
+  const shouldFilterProcessInstanceReport = (options: any) => {
+    const processInstanceReport: ProcessInstanceReport = options.item;
+    const { inputValue } = options;
+    return `${processInstanceReport.id} (${processInstanceReport.display_name})`.includes(
+      inputValue
+    );
+  };
+  return (
+    <ComboBox
+      onChange={onChange}
+      id="process-instance-report-select"
+      data-qa="process-instance-report-selection"
+      items={processInstanceReports}
+      itemToString={(processInstanceReport: ProcessInstanceReport) => {
+        if (processInstanceReport) {
+          return `${processInstanceReport.id} (${truncateString(
+            processInstanceReport.display_name,
+            20
+          )})`;
+        }
+        return null;
+      }}
+      shouldFilterItem={shouldFilterProcessInstanceReport}
+      placeholder="Choose a process instance report"
+      titleText={titleText}
+      selectedItem={selectedItem}
+    />
+  );
+}
