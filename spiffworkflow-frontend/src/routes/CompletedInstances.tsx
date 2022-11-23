e292e2550713c5006313b57b7ac8a820a8f377cb
jasquat <jasquat@users.noreply.github.com>
2022-11-23 16:37:33 -0500

    updated instances table descriptions w/ burnettk

--- spiffworkflow-frontend/src/routes/CompletedInstances.tsx
+++ spiffworkflow-frontend/src/routes/CompletedInstances.tsx
@@ -13,27 +13,31 @@ export default function CompletedInstances() {
         perPageOptions={[2, 5, 25]}
         reportIdentifier="system_report_instances_initiated_by_me"
         showReports={false}
-        textToShowIfEmpty="No completed instances"
+        textToShowIfEmpty="There are no completed instances at this time."
       />
-      <h2 style={{ marginTop: '1em' }}>Tasks actions by me</h2>
-      <p className="data-table-description" />
+      <h2 style={{ marginTop: '1em' }}>Tasks actioned by me</h2>
+      <p className="data-table-description">
+        This is a list of instances where you have completed tasks.
+      </p>
       <ProcessInstanceListTable
         filtersEnabled={false}
         paginationQueryParamPrefix="my_completed_tasks"
         perPageOptions={[2, 5, 25]}
         reportIdentifier="system_report_instances_with_tasks_completed_by_me"
         showReports={false}
-        textToShowIfEmpty="No completed instances"
+        textToShowIfEmpty="There are no instances with tasks that you completed."
       />
       <h2 style={{ marginTop: '1em' }}>With Tasks Completed By My Group</h2>
-      <p className="data-table-description" />
+      <p className="data-table-description">
+        This is a list of instances that were completed by groups you belong to.
+      </p>
       <ProcessInstanceListTable
         filtersEnabled={false}
         paginationQueryParamPrefix="group_completed_tasks"
         perPageOptions={[2, 5, 25]}
         reportIdentifier="system_report_instances_with_tasks_completed_by_my_groups"
         showReports={false}
-        textToShowIfEmpty="No completed instances"
+        textToShowIfEmpty="There are no instances with tasks that were completed by your groups."
       />
     </>
   );
