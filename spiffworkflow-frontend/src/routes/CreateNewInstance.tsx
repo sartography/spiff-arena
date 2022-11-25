26445eef4aa1e85afb96f809c683cb1e6378293e
burnettk <burnettk@users.noreply.github.com>
2022-11-25 12:25:37 -0500

    Processes you can start is now: Processes I can start

--- spiffworkflow-frontend/src/routes/CreateNewInstance.tsx
+++ spiffworkflow-frontend/src/routes/CreateNewInstance.tsx
@@ -2,6 +2,6 @@ import ProcessModelListTiles from '../components/ProcessModelListTiles';
 
 export default function CreateNewInstance() {
   return (
-    <ProcessModelListTiles headerElement={<h2>Processes you can start</h2>} />
+    <ProcessModelListTiles headerElement={<h2>Processes I can start</h2>} />
   );
 }
