0eeb096d433fca4273893d0836537ac17f0d6409
jasquat <jasquat@users.noreply.github.com>
2022-11-28 15:26:50 -0500

    added script to save process instance metadata and fixed permissions issue w/ burnettk cullerton

---
+++ spiffworkflow-backend/src/spiffworkflow_backend/scripts/save_process_instance_metadata.py
@@ -0,0 +1,35 @@
+"""Get_env."""
+from typing import Any
+from flask_bpmn.models.db import db
+
+from spiffworkflow_backend.models.process_instance_metadata import ProcessInstanceMetadataModel
+from spiffworkflow_backend.models.script_attributes_context import (
+    ScriptAttributesContext,
+)
+from spiffworkflow_backend.scripts.script import Script
+
+
+class SaveProcessInstanceMetadata(Script):
+    """SaveProcessInstanceMetadata."""
+
+    def get_description(self) -> str:
+        """Get_description."""
+        return """Save a given dict as process instance metadata (useful for creating reports)."""
+
+    def run(
+        self,
+        script_attributes_context: ScriptAttributesContext,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Any:
+        """Run."""
+        metadata_dict = args[0]
+        for key, value in metadata_dict.items():
+            pim = ProcessInstanceMetadataModel.query.filter_by(
+                process_instance_id=script_attributes_context.process_instance_id, key=key).first()
+            if pim is None:
+                pim = ProcessInstanceMetadataModel(
+                    process_instance_id=script_attributes_context.process_instance_id, key=key)
+            pim.value = value
+            db.session.add(pim)
+            db.session.commit()
