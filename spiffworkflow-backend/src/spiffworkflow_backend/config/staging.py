6d9b3c52761da60eff5dd0a3d29a7e604311ce1a
jasquat <jasquat@users.noreply.github.com>
2022-12-09 17:28:57 -0500

    update staging configs to allow env var overrides w/ burnettk

--- spiffworkflow-backend/src/spiffworkflow_backend/config/staging.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/config/staging.py
@@ -1,4 +1,6 @@
 """staging."""
-GIT_BRANCH = "staging"
-GIT_BRANCH_TO_PUBLISH_TO = "main"
+from os import environ
+
+GIT_BRANCH = environ.get("GIT_BRANCH_TO_PUBLISH_TO", default="staging")
+GIT_BRANCH_TO_PUBLISH_TO = environ.get("GIT_BRANCH_TO_PUBLISH_TO", default="main")
 GIT_COMMIT_ON_SAVE = False
