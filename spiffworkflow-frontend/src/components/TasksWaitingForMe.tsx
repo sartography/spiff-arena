77aa32646a3decf27e413460db13cd31b1f83d0f
jasquat <jasquat@users.noreply.github.com>
2022-12-12 12:21:37 -0500

    split group task tables by group and created component for group tables

--- spiffworkflow-frontend/src/components/TasksWaitingForMe.tsx
+++ spiffworkflow-frontend/src/components/TasksWaitingForMe.tsx
@@ -1,149 +1,15 @@
-import { useEffect, useState } from 'react';
-// @ts-ignore
-import { Button, Table } from '@carbon/react';
-import { Link, useSearchParams } from 'react-router-dom';
-import PaginationForTable from './PaginationForTable';
-import {
-  convertSecondsToFormattedDateTime,
-  getPageInfoFromSearchParams,
-  modifyProcessIdentifierForPathParam,
-} from '../helpers';
-import HttpService from '../services/HttpService';
-import { PaginationObject } from '../interfaces';
-import TableCellWithTimeAgoInWords from './TableCellWithTimeAgoInWords';
-
-const PER_PAGE_FOR_TASKS_ON_HOME_PAGE = 5;
+import TasksTable from './TasksTable';
 
 export default function TasksWaitingForMe() {
-  const [searchParams] = useSearchParams();
-  const [tasks, setTasks] = useState([]);
-  const [pagination, setPagination] = useState<PaginationObject | null>(null);
-
-  useEffect(() => {
-    const { page, perPage } = getPageInfoFromSearchParams(
-      searchParams,
-      PER_PAGE_FOR_TASKS_ON_HOME_PAGE,
-      undefined,
-      'tasks_waiting_for_me'
-    );
-    const setTasksFromResult = (result: any) => {
-      setTasks(result.results);
-      setPagination(result.pagination);
-    };
-    HttpService.makeCallToBackend({
-      path: `/tasks/for-me?per_page=${perPage}&page=${page}`,
-      successCallback: setTasksFromResult,
-    });
-  }, [searchParams]);
-
-  const buildTable = () => {
-    const rows = tasks.map((row) => {
-      const rowToUse = row as any;
-      const taskUrl = `/tasks/${rowToUse.process_instance_id}/${rowToUse.task_id}`;
-      const modifiedProcessModelIdentifier =
-        modifyProcessIdentifierForPathParam(rowToUse.process_model_identifier);
-      return (
-        <tr key={rowToUse.id}>
-          <td>
-            <Link
-              data-qa="process-instance-show-link"
-              to={`/admin/${modifiedProcessModelIdentifier}/${rowToUse.process_instance_id}`}
-              title={`View process instance ${rowToUse.process_instance_id}`}
-            >
-              {rowToUse.process_instance_id}
-            </Link>
-          </td>
-          <td>
-            <Link
-              data-qa="process-model-show-link"
-              to={`/admin/process-models/${modifiedProcessModelIdentifier}`}
-              title={rowToUse.process_model_identifier}
-            >
-              {rowToUse.process_model_display_name}
-            </Link>
-          </td>
-          <td
-            title={`task id: ${rowToUse.name}, spiffworkflow task guid: ${rowToUse.id}`}
-          >
-            {rowToUse.task_title}
-          </td>
-          <td>{rowToUse.username}</td>
-          <td>{rowToUse.group_identifier || '-'}</td>
-          <td>
-            {convertSecondsToFormattedDateTime(
-              rowToUse.created_at_in_seconds
-            ) || '-'}
-          </td>
-          <TableCellWithTimeAgoInWords
-            timeInSeconds={rowToUse.updated_at_in_seconds}
-          />
-          <td>
-            <Button
-              variant="primary"
-              href={taskUrl}
-              hidden={rowToUse.process_instance_status === 'suspended'}
-              disabled={!rowToUse.current_user_is_potential_owner}
-            >
-              Go
-            </Button>
-          </td>
-        </tr>
-      );
-    });
-    return (
-      <Table striped bordered>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Process</th>
-            <th>Task</th>
-            <th>Started By</th>
-            <th>Waiting For</th>
-            <th>Date Started</th>
-            <th>Last Updated</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </Table>
-    );
-  };
-
-  const tasksComponent = () => {
-    if (pagination && pagination.total < 1) {
-      return (
-        <p className="no-results-message with-large-bottom-margin">
-          You have no task assignments at this time.
-        </p>
-      );
-    }
-    const { page, perPage } = getPageInfoFromSearchParams(
-      searchParams,
-      PER_PAGE_FOR_TASKS_ON_HOME_PAGE,
-      undefined,
-      'tasks_waiting_for_me'
-    );
-    return (
-      <PaginationForTable
-        page={page}
-        perPage={perPage}
-        perPageOptions={[2, PER_PAGE_FOR_TASKS_ON_HOME_PAGE, 25]}
-        pagination={pagination}
-        tableToDisplay={buildTable()}
-        paginationQueryParamPrefix="tasks_waiting_for_me"
-        paginationClassName="with-large-bottom-margin"
-      />
-    );
-  };
-
   return (
-    <>
-      <h2>Tasks waiting for me</h2>
-      <p className="data-table-description">
-        These processes are waiting on you to complete the next task. All are
-        processes created by others that are now actionable by you.
-      </p>
-      {tasksComponent()}
-    </>
+    <TasksTable
+      apiPath="/tasks/for-me"
+      paginationQueryParamPrefix="tasks_waiting_for_me"
+      tableTitle="Tasks waiting for me"
+      tableDescription="These processes are waiting on you to complete the next task. All are processes created by others that are now actionable by you."
+      paginationClassName="with-large-bottom-margin"
+      autoReload
+      showWaitingOn={false}
+    />
   );
 }
