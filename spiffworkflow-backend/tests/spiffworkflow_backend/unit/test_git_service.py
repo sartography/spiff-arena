14be1fa06bfc36595092b16e2216424e0de13131
jasquat <jasquat@users.noreply.github.com>
2022-12-12 10:05:08 -0500

    strip off spaces from git service command stdout

---
+++ spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_git_service.py
@@ -0,0 +1,18 @@
+"""Process Model."""
+from flask.app import Flask
+from flask.testing import FlaskClient
+from tests.spiffworkflow_backend.helpers.base_test import BaseTest
+
+from spiffworkflow_backend.services.git_service import GitService
+
+
+class TestGitService(BaseTest):
+
+    def test_strips_output_of_stdout_from_command(
+        self,
+        app: Flask,
+        client: FlaskClient,
+        with_db_and_bpmn_file_cleanup: None,
+    ) -> None:
+        output = GitService.run_shell_command_to_get_stdout(["echo", '   This output should not end in space or newline  \n'])
+        assert output == 'This output should not end in space or newline'
