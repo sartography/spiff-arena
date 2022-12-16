d4925334046a4dbdfe3fecae81cd2b730032f206
jasquat <jasquat@users.noreply.github.com>
2022-12-16 14:41:13 -0500

    some more updates for text w/ burnettk

--- spiffworkflow-frontend/src/components/TasksForMyOpenProcesses.tsx
+++ spiffworkflow-frontend/src/components/TasksForMyOpenProcesses.tsx
@@ -7,7 +7,7 @@ export default function MyOpenProcesses() {
     <TaskListTable
       apiPath="/tasks/for-my-open-processes"
       paginationQueryParamPrefix={paginationQueryParamPrefix}
-      tableTitle="My open instances"
+      tableTitle="Tasks for my open instances"
       tableDescription="These tasks are for processes you started which are not complete. You may not have an action to take at this time. See below for tasks waiting on you."
       paginationClassName="with-large-bottom-margin"
       textToShowIfEmpty="There are no tasks for processes you started at this time."
