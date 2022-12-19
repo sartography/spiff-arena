89377ea881d0da95bb8f79b92bdc1c5e06511028
Dan <daniel.h.funk@gmail.com>
2022-12-19 10:09:19 -0500

    Fixing some tests.

--- spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_user_service.py
+++ spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_user_service.py
@@ -21,7 +21,7 @@ class TestUserService(BaseTest):
     ) -> None:
         """Test_waiting_group_assignments"""
         aTestGroup = GroupService.find_or_create_group("aTestGroup")
-        UserService.add_group_assignment("initiator_user", aTestGroup)
+        UserService.add_waiting_group_assignment("initiator_user", aTestGroup)
         initiator_user = self.find_or_create_user("initiator_user")
         assert initiator_user.groups[0] == aTestGroup
 
@@ -33,7 +33,7 @@ class TestUserService(BaseTest):
     ) -> None:
         """Test_waiting_group_assignments"""
         everybody_group = GroupService.find_or_create_group("everybodyGroup")
-        UserService.add_group_assignment(UserGroupAssignmentWaitingModel.MATCH_ALL_USERS, everybody_group)
+        UserService.add_waiting_group_assignment(UserGroupAssignmentWaitingModel.MATCH_ALL_USERS, everybody_group)
         initiator_user = self.find_or_create_user("initiator_user")
         assert initiator_user.groups[0] == everybody_group
 
@@ -46,5 +46,5 @@ class TestUserService(BaseTest):
         """Test_waiting_group_assignments"""
         initiator_user = self.find_or_create_user("initiator_user")
         everybody_group = GroupService.find_or_create_group("everybodyGroup")
-        UserService.add_group_assignment(UserGroupAssignmentWaitingModel.MATCH_ALL_USERS, everybody_group)
+        UserService.add_waiting_group_assignment(UserGroupAssignmentWaitingModel.MATCH_ALL_USERS, everybody_group)
         assert initiator_user.groups[0] == everybody_group
