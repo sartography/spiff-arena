7c55ab3dfb3729f5c0442bccffec9a184d556c4d
burnettk <burnettk@users.noreply.github.com>
2022-12-29 18:23:45 -0500

    new service tasks controller

--- spiffworkflow-backend/src/spiffworkflow_backend/routes/process_api_blueprint.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/routes/process_api_blueprint.py
@@ -4,13 +4,11 @@ from typing import Any
 from typing import Dict
 
 import flask.wrappers
-import werkzeug
 from flask import Blueprint
 from flask import current_app
 from flask import g
 from flask import jsonify
 from flask import make_response
-from flask import redirect
 from flask import request
 from flask.wrappers import Response
 from flask_bpmn.api.api_error import ApiError
@@ -32,15 +30,12 @@ from spiffworkflow_backend.models.process_instance import (
 from spiffworkflow_backend.models.process_model import ProcessModelInfo
 from spiffworkflow_backend.models.spec_reference import SpecReferenceCache
 from spiffworkflow_backend.models.spec_reference import SpecReferenceSchema
-from spiffworkflow_backend.routes.user import verify_token
 from spiffworkflow_backend.services.authorization_service import AuthorizationService
 from spiffworkflow_backend.services.git_service import GitService
 from spiffworkflow_backend.services.process_instance_processor import (
     ProcessInstanceProcessor,
 )
 from spiffworkflow_backend.services.process_model_service import ProcessModelService
-from spiffworkflow_backend.services.secret_service import SecretService
-from spiffworkflow_backend.services.service_task_service import ServiceTaskService
 
 
 process_api_blueprint = Blueprint("process_api", __name__)
@@ -94,41 +89,6 @@ def process_list() -> Any:
     """
     references = SpecReferenceCache.query.filter_by(type="process").all()
     return SpecReferenceSchema(many=True).dump(references)
-
-
-def service_task_list() -> flask.wrappers.Response:
-    """Service_task_list."""
-    available_connectors = ServiceTaskService.available_connectors()
-    return Response(
-        json.dumps(available_connectors), status=200, mimetype="application/json"
-    )
-
-
-def authentication_list() -> flask.wrappers.Response:
-    """Authentication_list."""
-    available_authentications = ServiceTaskService.authentication_list()
-    response_json = {
-        "results": available_authentications,
-        "connector_proxy_base_url": current_app.config["CONNECTOR_PROXY_URL"],
-        "redirect_url": f"{current_app.config['SPIFFWORKFLOW_BACKEND_URL']}/v1.0/authentication_callback",
-    }
-
-    return Response(json.dumps(response_json), status=200, mimetype="application/json")
-
-
-def authentication_callback(
-    service: str,
-    auth_method: str,
-) -> werkzeug.wrappers.Response:
-    """Authentication_callback."""
-    verify_token(request.args.get("token"), force_run=True)
-    response = request.args["response"]
-    SecretService().update_secret(
-        f"{service}/{auth_method}", response, g.user.id, create_if_not_exists=True
-    )
-    return redirect(
-        f"{current_app.config['SPIFFWORKFLOW_FRONTEND_URL']}/admin/configuration"
-    )
 
 
 def process_data_show(
