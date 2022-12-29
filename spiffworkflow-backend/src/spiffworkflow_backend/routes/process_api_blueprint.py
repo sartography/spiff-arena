9615f600affaac364e875205d9c409b202b043d6
Elizabeth Esswein <elizabeth.esswein@gmail.com>
2022-12-29 13:55:19 -0500

    fix show previous spiff steps

--- spiffworkflow-backend/src/spiffworkflow_backend/routes/process_api_blueprint.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/routes/process_api_blueprint.py
@@ -1402,7 +1402,7 @@ def process_instance_task_list(
         step_detail = (
             db.session.query(SpiffStepDetailsModel)
             .filter(
-                SpiffStepDetailsModel.process_instance.id == process_instance.id,
+                SpiffStepDetailsModel.process_instance_id == process_instance.id,
                 SpiffStepDetailsModel.spiff_step == spiff_step,
             )
             .first()
