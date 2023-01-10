bac203727e60d98415efe290bbfca754b4335852
jasquat <jasquat@users.noreply.github.com>
2023-01-10 15:15:21 -0500

    updated rjsf to beta.16 and updated validations from v6 to v8 w/ burnettk

--- spiffworkflow-frontend/src/themes/carbon/ArrayFieldTemplate/ArrayFieldTemplate.tsx
+++ spiffworkflow-frontend/src/themes/carbon/ArrayFieldTemplate/ArrayFieldTemplate.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import {
-  ArrayFieldTemplateItemType,
-  ArrayFieldTemplateProps,
   getTemplate,
   getUiOptions,
+  ArrayFieldTemplateProps,
+  ArrayFieldTemplateItemType,
+  FormContextType,
+  RJSFSchema,
+  StrictRJSFSchema,
 } from '@rjsf/utils';
 
-function ArrayFieldTemplate(props: ArrayFieldTemplateProps) {
+/** The `ArrayFieldTemplate` component is the template used to render all items in an array.
+ *
+ * @param props - The `ArrayFieldTemplateItemType` props for the component
+ */
+export default function ArrayFieldTemplate<
+  T = any,
+  S extends StrictRJSFSchema = RJSFSchema,
+  F extends FormContextType = any
+>(props: ArrayFieldTemplateProps<T, S, F>) {
   const {
     canAdd,
+    className,
     disabled,
     idSchema,
     uiSchema,
@@ -23,68 +32,62 @@ function ArrayFieldTemplate(props: ArrayFieldTemplateProps) {
     schema,
     title,
   } = props;
-  const uiOptions = getUiOptions(uiSchema);
-  const ArrayFieldDescriptionTemplate =
-    getTemplate<'ArrayFieldDescriptionTemplate'>(
-      'ArrayFieldDescriptionTemplate',
-      registry,
-      uiOptions
-    );
-  const ArrayFieldItemTemplate = getTemplate<'ArrayFieldItemTemplate'>(
+  const uiOptions = getUiOptions<T, S, F>(uiSchema);
+  const ArrayFieldDescriptionTemplate = getTemplate<
+    'ArrayFieldDescriptionTemplate',
+    T,
+    S,
+    F
+  >('ArrayFieldDescriptionTemplate', registry, uiOptions);
+  const ArrayFieldItemTemplate = getTemplate<'ArrayFieldItemTemplate', T, S, F>(
     'ArrayFieldItemTemplate',
     registry,
     uiOptions
   );
-  const ArrayFieldTitleTemplate = getTemplate<'ArrayFieldTitleTemplate'>(
+  const ArrayFieldTitleTemplate = getTemplate<
     'ArrayFieldTitleTemplate',
-    registry,
-    uiOptions
-  );
+    T,
+    S,
+    F
+  >('ArrayFieldTitleTemplate', registry, uiOptions);
   // Button templates are not overridden in the uiSchema
   const {
     ButtonTemplates: { AddButton },
   } = registry.templates;
   return (
-    <Paper elevation={2}>
-      <Box p={2}>
-        <ArrayFieldTitleTemplate
-          idSchema={idSchema}
-          title={uiOptions.title || title}
-          schema={schema}
-          uiSchema={uiSchema}
-          required={required}
-          registry={registry}
-        />
-        <ArrayFieldDescriptionTemplate
-          idSchema={idSchema}
-          description={uiOptions.description || schema.description}
-          schema={schema}
-          uiSchema={uiSchema}
-          registry={registry}
-        />
-        <Grid container key={`array-item-list-${idSchema.$id}`}>
-          {items &&
-            items.map(({ key, ...itemProps }: ArrayFieldTemplateItemType) => (
+    <fieldset className={className} id={idSchema.$id}>
+      <ArrayFieldTitleTemplate
+        idSchema={idSchema}
+        title={uiOptions.title || title}
+        required={required}
+        schema={schema}
+        uiSchema={uiSchema}
+        registry={registry}
+      />
+      <ArrayFieldDescriptionTemplate
+        idSchema={idSchema}
+        description={uiOptions.description || schema.description}
+        schema={schema}
+        uiSchema={uiSchema}
+        registry={registry}
+      />
+      <div className="row array-item-list">
+        {items &&
+          items.map(
+            ({ key, ...itemProps }: ArrayFieldTemplateItemType<T, S, F>) => (
               <ArrayFieldItemTemplate key={key} {...itemProps} />
-            ))}
-          {canAdd && (
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Box mt={2}>
-                  <AddButton
-                    className="array-item-add"
-                    onClick={onAddClick}
-                    disabled={disabled || readonly}
-                    uiSchema={uiSchema}
-                  />
-                </Box>
-              </Grid>
-            </Grid>
+            )
           )}
-        </Grid>
-      </Box>
-    </Paper>
+      </div>
+      {canAdd && (
+        <AddButton
+          className="array-item-add"
+          onClick={onAddClick}
+          disabled={disabled || readonly}
+          uiSchema={uiSchema}
+          registry={registry}
+        />
+      )}
+    </fieldset>
   );
 }
-
-export default ArrayFieldTemplate;
--- spiffworkflow-frontend/src/themes/carbon/ObjectFieldTemplate/ObjectFieldTemplate.tsx
+++ spiffworkflow-frontend/src/themes/carbon/ObjectFieldTemplate/ObjectFieldTemplate.tsx
@@ -1,89 +1,87 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
 import {
+  FormContextType,
+  ObjectFieldTemplatePropertyType,
   ObjectFieldTemplateProps,
+  RJSFSchema,
+  StrictRJSFSchema,
   canExpand,
   getTemplate,
   getUiOptions,
 } from '@rjsf/utils';
 
-function ObjectFieldTemplate({
-  description,
-  title,
-  properties,
-  required,
-  disabled,
-  readonly,
-  uiSchema,
-  idSchema,
-  schema,
-  formData,
-  onAddClick,
-  registry,
-}: ObjectFieldTemplateProps) {
-  const uiOptions = getUiOptions(uiSchema);
-  const TitleFieldTemplate = getTemplate<'TitleFieldTemplate'>(
+/** The `ObjectFieldTemplate` is the template to use to render all the inner properties of an object along with the
+ * title and description if available. If the object is expandable, then an `AddButton` is also rendered after all
+ * the properties.
+ *
+ * @param props - The `ObjectFieldTemplateProps` for this component
+ */
+export default function ObjectFieldTemplate<
+  T = any,
+  S extends StrictRJSFSchema = RJSFSchema,
+  F extends FormContextType = any
+>(props: ObjectFieldTemplateProps<T, S, F>) {
+  const {
+    description,
+    disabled,
+    formData,
+    idSchema,
+    onAddClick,
+    properties,
+    readonly,
+    registry,
+    required,
+    schema,
+    title,
+    uiSchema,
+  } = props;
+  const options = getUiOptions<T, S, F>(uiSchema);
+  const TitleFieldTemplate = getTemplate<'TitleFieldTemplate', T, S, F>(
     'TitleFieldTemplate',
     registry,
-    uiOptions
+    options
   );
-  const DescriptionFieldTemplate = getTemplate<'DescriptionFieldTemplate'>(
+  const DescriptionFieldTemplate = getTemplate<
     'DescriptionFieldTemplate',
-    registry,
-    uiOptions
-  );
+    T,
+    S,
+    F
+  >('DescriptionFieldTemplate', registry, options);
   // Button templates are not overridden in the uiSchema
   const {
     ButtonTemplates: { AddButton },
   } = registry.templates;
   return (
-    <>
-      {(uiOptions.title || title) && (
+    <fieldset id={idSchema.$id}>
+      {(options.title || title) && (
         <TitleFieldTemplate
-          id={`${idSchema.$id}-title`}
-          title={title}
+          id={`${idSchema.$id}__title`}
+          title={options.title || title}
           required={required}
           schema={schema}
           uiSchema={uiSchema}
           registry={registry}
         />
       )}
-      {(uiOptions.description || description) && (
+      {(options.description || description) && (
         <DescriptionFieldTemplate
-          id={`${idSchema.$id}-description`}
-          description={uiOptions.description || description!}
+          id={`${idSchema.$id}__description`}
+          description={options.description || description!}
           schema={schema}
           uiSchema={uiSchema}
           registry={registry}
         />
       )}
-      <Grid container spacing={2} style={{ marginTop: '10px' }}>
-        {properties.map((element, index) =>
-          // Remove the <Grid> if the inner element is hidden as the <Grid>
-          // itself would otherwise still take up space.
-          element.hidden ? (
-            element.content
-          ) : (
-            <Grid item xs={12} key={index} style={{ marginBottom: '10px' }}>
-              {element.content}
-            </Grid>
-          )
-        )}
-        {canExpand(schema, uiSchema, formData) && (
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <AddButton
-                className="object-property-expand"
-                onClick={onAddClick(schema)}
-                disabled={disabled || readonly}
-                uiSchema={uiSchema}
-              />
-            </Grid>
-          </Grid>
-        )}
-      </Grid>
-    </>
+      {properties.map((prop: ObjectFieldTemplatePropertyType) => prop.content)}
+      {canExpand<T, S, F>(schema, uiSchema, formData) && (
+        <AddButton
+          className="object-property-expand"
+          onClick={onAddClick(schema)}
+          disabled={disabled || readonly}
+          uiSchema={uiSchema}
+          registry={registry}
+        />
+      )}
+    </fieldset>
   );
 }
-
-export default ObjectFieldTemplate;
