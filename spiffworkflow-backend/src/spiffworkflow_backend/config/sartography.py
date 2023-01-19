5ef4ea7b6271b2c30ba69923979a81c96cc78b37
jasquat <jasquat@users.noreply.github.com>
2023-01-19 15:03:33 -0500

    pyl w/ burnettk

--- spiffworkflow-backend/src/spiffworkflow_backend/config/sartography.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/config/sartography.py
@@ -5,5 +5,6 @@ environment_identifier_for_this_config_file_only = environ["SPIFFWORKFLOW_BACKEN
 OPEN_ID_SERVER_URL = f"https://keycloak.{environment_identifier_for_this_config_file_only}.spiffworkflow.org/realms/sartography"
 GIT_BRANCH = environ.get("GIT_BRANCH", default="main")
 GIT_CLONE_URL_FOR_PUBLISHING = environ.get(
-    "GIT_CLONE_URL", default="https://github.com/sartography/sartography-process-models.git"
+    "GIT_CLONE_URL",
+    default="https://github.com/sartography/sartography-process-models.git",
 )
