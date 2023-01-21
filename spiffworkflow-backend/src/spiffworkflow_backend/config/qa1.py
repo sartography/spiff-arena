b4a19f2d71692746483f8d2f57c943b6d1f8b89a
burnettk <burnettk@users.noreply.github.com>
2023-01-20 20:44:01 -0500

    qa1

--- spiffworkflow-backend/src/spiffworkflow_backend/config/qa1.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/config/qa1.py
@@ -4,7 +4,7 @@ from os import environ
 GIT_BRANCH_TO_PUBLISH_TO = environ.get("GIT_BRANCH_TO_PUBLISH_TO", default="qa2")
 GIT_USERNAME = environ.get("GIT_USERNAME", default="sartography-automated-committer")
 GIT_USER_EMAIL = environ.get(
-    "GIT_USER_EMAIL", default="sartography-automated-committer@users.noreply.github.com"
+    "GIT_USER_EMAIL", default=f"{GIT_USERNAME}@users.noreply.github.com"
 )
 SPIFFWORKFLOW_BACKEND_PERMISSIONS_FILE_NAME = environ.get(
     "SPIFFWORKFLOW_BACKEND_PERMISSIONS_FILE_NAME", default="qa1.yml"
