39ecb70046136d82407c2636789f878171fe0efc
burnettk <burnettk@users.noreply.github.com>
2023-01-20 19:00:46 -0500

    commit on save yes for now

--- spiffworkflow-backend/src/spiffworkflow_backend/config/staging.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/config/staging.py
@@ -3,5 +3,5 @@ from os import environ
 
 GIT_BRANCH = environ.get("GIT_BRANCH", default="staging")
 GIT_BRANCH_TO_PUBLISH_TO = environ.get("GIT_BRANCH_TO_PUBLISH_TO", default="main")
-GIT_COMMIT_ON_SAVE = False
+GIT_COMMIT_ON_SAVE = True
 SPIFFWORKFLOW_BACKEND_PERMISSIONS_FILE_NAME = "staging.yml"
