a9f00ce1a7ed2c7b2c4c556e523fcda160c4290f
burnettk <burnettk@users.noreply.github.com>
2023-02-04 00:03:32 -0500

    if there are tenant specific fields in the config, transfer them from openid token to db

--- spiffworkflow-backend/src/spiffworkflow_backend/models/user.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/models/user.py
@@ -34,6 +34,9 @@ class UserModel(SpiffworkflowBaseDBModel):
     service_id = db.Column(db.String(255), nullable=False, unique=False)
     display_name = db.Column(db.String(255))
     email = db.Column(db.String(255))
+    tenant_specific_field_1 = db.Column(db.String(255))
+    tenant_specific_field_2 = db.Column(db.String(255))
+    tenant_specific_field_3 = db.Column(db.String(255))
     updated_at_in_seconds: int = db.Column(db.Integer)
     created_at_in_seconds: int = db.Column(db.Integer)
 
