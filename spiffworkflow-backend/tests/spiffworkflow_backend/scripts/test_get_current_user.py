cea7f746f62bb8ebed6c681d74e0759055e8966f
jasquat <jasquat@users.noreply.github.com>
2023-02-06 14:11:15 -0500

    use our json encoder to dump the user for get_current_user script w/ burnettk

--- spiffworkflow-backend/tests/spiffworkflow_backend/scripts/test_get_current_user.py
+++ spiffworkflow-backend/tests/spiffworkflow_backend/scripts/test_get_current_user.py
@@ -1,23 +1,20 @@
 """Test_get_localtime."""
-from flask.app import Flask
+import json
+
 from flask import g
+from flask.app import Flask
 from flask.testing import FlaskClient
-from spiffworkflow_backend.models.script_attributes_context import ScriptAttributesContext
-from spiffworkflow_backend.scripts.get_current_user import GetCurrentUser
 from tests.spiffworkflow_backend.helpers.base_test import BaseTest
-from tests.spiffworkflow_backend.helpers.test_data import load_test_spec
 
 from spiffworkflow_backend.models.db import db
-from spiffworkflow_backend.models.group import GroupModel
-from spiffworkflow_backend.models.user import UserModel
-from spiffworkflow_backend.services.process_instance_processor import (
-    ProcessInstanceProcessor,
+from spiffworkflow_backend.models.script_attributes_context import (
+    ScriptAttributesContext,
 )
-from spiffworkflow_backend.services.user_service import UserService
+from spiffworkflow_backend.models.user import UserModel
+from spiffworkflow_backend.scripts.get_current_user import GetCurrentUser
 
 
 class TestGetCurrentUser(BaseTest):
-
     def test_get_current_user(
         self,
         app: Flask,
@@ -29,6 +26,9 @@ class TestGetCurrentUser(BaseTest):
         testuser1 = self.find_or_create_user("testuser1")
         testuser1.tenant_specific_field_1 = "456"
         db.session.add(testuser1)
+        db.session.commit()
+
+        testuser1 = self.find_or_create_user("testuser1")
         g.user = testuser1
         process_model_identifier = "test_process_model"
         process_instance_id = 1
@@ -41,4 +41,6 @@ class TestGetCurrentUser(BaseTest):
         result = GetCurrentUser().run(
             script_attributes_context,
         )
-        assert result['username'] == "testuser1"
+        assert result["username"] == "testuser1"
+        assert result["tenant_specific_field_1"] == "456"
+        json.dumps(result)
