2fa462387641f32293c5bdb58b94fe899cc8e977
Jon Herron <jon.herron@yahoo.com>
2023-02-22 10:39:35 -0500

    Add cumulative task data size to script

---
+++ spiffworkflow-backend/src/spiffworkflow_backend/scripts/get_data_sizes.py
@@ -0,0 +1,37 @@
+"""Get_data_sizes."""
+from typing import Any
+
+from spiffworkflow_backend.models.script_attributes_context import (
+    ScriptAttributesContext,
+)
+from spiffworkflow_backend.scripts.script import Script
+#from spiffworkflow_backend.servces.process_instance_processor import ProcessInstanceProcessor
+from spiffworkflow_backend.services.process_instance_processor import (
+    ProcessInstanceProcessor,
+)
+
+
+class GetDataSizes(Script):
+    """GetDataSizes."""
+
+    @staticmethod
+    def requires_privileged_permissions() -> bool:
+        """We have deemed this function safe to run without elevated permissions."""
+        return False
+
+    def get_description(self) -> str:
+        """Get_description."""
+        return """Returns a dictionary of information about the size of task data and the python environment for the currently running process."""
+
+    def run(
+        self,
+        script_attributes_context: ScriptAttributesContext,
+        *_args: Any,
+        **kwargs: Any
+    ) -> Any:
+        """Run."""
+        task = script_attributes_context.task
+        return {
+            "cumulative_task_data_size": ProcessInstanceProcessor.get_task_data_size(task.workflow),
+            "python_env_size": 0,
+        }
