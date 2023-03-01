90b177221544d398a806bca936e897dfb5084f48
jasquat <jasquat@users.noreply.github.com>
2023-03-01 17:18:58 -0500

    do not require task to be given to evaluate a task unless that script specifically needs it w/ burnettk

--- spiffworkflow-backend/src/spiffworkflow_backend/models/script_attributes_context.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/models/script_attributes_context.py
@@ -1,5 +1,6 @@
 """Script_attributes_context."""
 from dataclasses import dataclass
+from typing import Optional
 
 from SpiffWorkflow.task import Task as SpiffTask  # type: ignore
 
@@ -8,7 +9,7 @@ from SpiffWorkflow.task import Task as SpiffTask  # type: ignore
 class ScriptAttributesContext:
     """ScriptAttributesContext."""
 
-    task: SpiffTask
+    task: Optional[SpiffTask]
     environment_identifier: str
     process_instance_id: int
     process_model_identifier: str
---
+++ spiffworkflow-backend/tests/spiffworkflow_backend/scripts/test_get_env.py
@@ -0,0 +1,28 @@
+"""Test_environment_var_script."""
+from flask import Flask
+from tests.spiffworkflow_backend.helpers.base_test import BaseTest
+
+from spiffworkflow_backend.models.script_attributes_context import (
+    ScriptAttributesContext,
+)
+from spiffworkflow_backend.scripts.get_env import GetEnv
+
+
+class TestGetEnv(BaseTest):
+    def test_get_env_script(
+        self,
+        app: Flask,
+        with_db_and_bpmn_file_cleanup: None,
+    ) -> None:
+        process_model_identifier = "test_process_model"
+        process_instance_id = 1
+        script_attributes_context = ScriptAttributesContext(
+            task=None,
+            environment_identifier="unit_testing",
+            process_instance_id=process_instance_id,
+            process_model_identifier=process_model_identifier,
+        )
+        result = GetEnv().run(
+            script_attributes_context,
+        )
+        assert result == "unit_testing"
