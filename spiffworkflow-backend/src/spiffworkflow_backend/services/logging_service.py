695e51a1fb3018708cef20cab91febebf8509da6
jasquat <jasquat@users.noreply.github.com>
2023-03-01 17:21:24 -0500

    use workflow_spec to match task_spec naming w/ burnettk

--- spiffworkflow-backend/src/spiffworkflow_backend/services/logging_service.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/services/logging_service.py
@@ -212,7 +212,7 @@ class DBHandler(logging.Handler):
         # if we do not have a process instance id then do not log and assume we are running a script unit test
         # that initializes a BpmnWorkflow without a process instance
         if record and record.process_instance_id:  # type: ignore
-            bpmn_process_identifier = record.workflow  # type: ignore
+            bpmn_process_identifier = record.workflow_spec  # type: ignore
             bpmn_process_name = record.workflow_name  # type: ignore
             spiff_task_guid = str(record.task_id)  # type: ignore
             bpmn_task_identifier = str(record.task_spec)  # type: ignore
