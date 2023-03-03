b4546e86e631092d7d2da71ed66bad114dbb2057
jbirddog <100367399+jbirddog@users.noreply.github.com>
2023-03-03 12:24:18 -0500

    Special case file data during user form submission (#164)

---
+++ spiffworkflow-backend/src/spiffworkflow_backend/scripts/get_encoded_file_data.py
@@ -0,0 +1,49 @@
+"""Get_encoded_file_data."""
+import base64
+from typing import Any
+
+from spiffworkflow_backend.models.process_instance_file_data import (
+    ProcessInstanceFileDataModel,
+)
+from spiffworkflow_backend.models.script_attributes_context import (
+    ScriptAttributesContext,
+)
+from spiffworkflow_backend.scripts.script import Script
+
+
+class GetEncodedFileData(Script):
+    """GetEncodedFileData."""
+
+    @staticmethod
+    def requires_privileged_permissions() -> bool:
+        """We have deemed this function safe to run without elevated permissions."""
+        return False
+
+    def get_description(self) -> str:
+        """Get_description."""
+        return """Returns a string which is the encoded file data. This is a very expensive call."""
+
+    def run(
+        self,
+        script_attributes_context: ScriptAttributesContext,
+        *args: Any,
+        **kwargs: Any,
+    ) -> Any:
+        """Run."""
+        # example input:
+        #  "data:some/mimetype;name=testing.txt;base64,spifffiledatadigest+7a2051ffefd1eaf475dbef9fda019cb3d4a10eb8aea4c2c2a84a50a797a541bf"
+        digest_reference = args[0]
+        digest = digest_reference[-64:]
+        process_instance_id = script_attributes_context.process_instance_id
+
+        file_data = ProcessInstanceFileDataModel.query.filter_by(
+            digest=digest,
+            process_instance_id=process_instance_id,
+        ).first()
+
+        base64_value = base64.b64encode(file_data.contents).decode("ascii")
+        encoded_file_data = (
+            f"data:{file_data.mimetype};name={file_data.filename};base64,{base64_value}"
+        )
+
+        return encoded_file_data
