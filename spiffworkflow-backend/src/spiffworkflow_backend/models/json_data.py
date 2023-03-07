dd5a9110493c9db79215717b12e1179c67ea3b3c
jasquat <jasquat@users.noreply.github.com>
2023-03-07 09:28:38 -0500

    a little cleanup of comments

--- spiffworkflow-backend/src/spiffworkflow_backend/models/json_data.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/models/json_data.py
@@ -4,6 +4,22 @@ from spiffworkflow_backend.models.db import db
 from spiffworkflow_backend.models.db import SpiffworkflowBaseDBModel
 
 
+# delta algorithm <- just to save it for when we want to try to implement it:
+#   a = {"hey": { "hey2": 2, "hey3": 3, "hey6": 7 }, "hey30": 3, "hey40": 4}
+#   b = {"hey": { "hey2": 4, "hey5": 3 }, "hey20": 2, "hey30": 3}
+#   a_keys = set(a.keys())
+#   b_keys = set(b.keys())
+#   removed = a_keys - b_keys
+#   added_keys = b_keys - a_keys
+#   keys_present_in_both = a_keys & b_keys
+#   changed = {}
+#   for key_in_both in keys_present_in_both:
+#       if a[key_in_both] != b[key_in_both]:
+#           changed[key_in_both] = b[key_in_both]
+#   added = {}
+#   for added_key in added_keys:
+#       added[added_key] = b[added_key]
+#   final_tuple = [added, removed, changed]
 class JsonDataModel(SpiffworkflowBaseDBModel):
     __tablename__ = "json_data"
     id: int = db.Column(db.Integer, primary_key=True)
