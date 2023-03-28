13144c59335f6355bf1fd40ebae2fa92e63abbda
Jon Herron <jon.herron@yahoo.com>
2023-03-28 16:14:50 -0400

    Getting run_pyl to pass

--- spiffworkflow-backend/src/spiffworkflow_backend/routes/connector_proxy_controller.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/routes/connector_proxy_controller.py
@@ -1,13 +1,15 @@
-import flask.wrappers
-import json
-import requests
+from typing import Any
 
+import flask.wrappers
+import requests
 from flask import current_app
 from flask.wrappers import Response
 
-def connector_proxy_type_ahead_url() -> str:
+
+def connector_proxy_type_ahead_url() -> Any:
     """Returns the connector proxy type ahead url."""
     return current_app.config["SPIFFWORKFLOW_BACKEND_CONNECTOR_PROXY_TYPE_AHEAD_URL"]
+
 
 def type_ahead(category: str, prefix: str, limit: int) -> flask.wrappers.Response:
     url = f"{connector_proxy_type_ahead_url()}/v1/type-ahead/{category}?prefix={prefix}&limit={limit}"
