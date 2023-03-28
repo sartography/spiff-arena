25a489d561fe220664be86219025df3f4be59b57
Jon Herron <jon.herron@yahoo.com>
2023-03-28 15:48:30 -0400

    Url update

--- spiffworkflow-backend/src/spiffworkflow_backend/routes/connector_proxy_controller.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/routes/connector_proxy_controller.py
@@ -10,14 +10,14 @@ def connector_proxy_type_ahead_url() -> str:
     return current_app.config["SPIFFWORKFLOW_BACKEND_CONNECTOR_PROXY_TYPE_AHEAD_URL"]
 
 def type_ahead(category: str, prefix: str, limit: int) -> flask.wrappers.Response:
-    # TODO: change url to /type-ahead/{category}
-    url = f"{connector_proxy_type_ahead_url()}/{category}?prefix={prefix}&limit={limit}"
+    url = f"{connector_proxy_type_ahead_url()}/v1/type-ahead/{category}?prefix={prefix}&limit={limit}"
 
-    # TODO: try/catch/log etc
     proxy_response = requests.get(url)
     status = proxy_response.status_code
     if status // 100 == 2:
         response = proxy_response.text
     else:
+        # supress pop up errors on the client
+        status = 200
         response = "[]"
     return Response(response, status=status, mimetype="application/json")
