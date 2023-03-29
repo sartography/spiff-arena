cdf5fc313a14d941eef12089e21a70f9f0ff93ce
burnettk <burnettk@users.noreply.github.com>
2023-03-29 15:51:04 -0400

    use text for sql

--- spiffworkflow-backend/src/spiffworkflow_backend/helpers/db_helper.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/helpers/db_helper.py
@@ -9,7 +9,7 @@ from spiffworkflow_backend.models.db import db
 def try_to_connect(start_time: float) -> None:
     """Try to connect."""
     try:
-        db.first_or_404("select 1")  # type: ignore
+        db.first_or_404(text("select 1"))  # type: ignore
     except sqlalchemy.exc.DatabaseError as exception:
         if time.time() - start_time > 15:
             raise exception
