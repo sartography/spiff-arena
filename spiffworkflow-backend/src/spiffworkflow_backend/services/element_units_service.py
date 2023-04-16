d52656e230cbb714a1467252990f082ff3c0f95d
Jon Herron <jon.herron@yahoo.com>
2023-04-16 11:31:54 -0400

    Tests back to passing using backend spec structure

--- spiffworkflow-backend/src/spiffworkflow_backend/services/element_units_service.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/services/element_units_service.py
@@ -1,7 +1,10 @@
+import json
 from typing import Optional
+from typing import Dict, Any
 
 from flask import current_app
 
+BpmnSpecDict = Dict[str, Any]
 
 class ElementUnitsService:
     """Feature gated glue between the backend and spiff-element-units."""
@@ -16,51 +19,54 @@ class ElementUnitsService:
         return enabled and cls._cache_dir()
 
     @classmethod
-    def cache_element_units(cls, cache_key: str, workflow_spec_json: str) -> None:
+    def cache_element_units(cls, cache_key: str, bpmn_spec_dict: BpmnSpecDict) -> None:
         if not cls._enabled():
             return None
-
-        # for now we are importing inside each of these functions, not sure the best
-        # way to do this in an overall feature flagged strategy but this gets things
-        # moving
-        import spiff_element_units
-
+        
         try:
-            return spiff_element_units.cache_element_units(cls._cache_dir(), cache_key, workflow_spec_json)
+            # for now we are importing inside each of these functions, not sure the best
+            # way to do this in an overall feature flagged strategy but this gets things
+            # moving
+            import spiff_element_units
+
+            bpmn_spec_json = json.dumps(bpmn_spec_dict)
+            return spiff_element_units.cache_element_units(cls._cache_dir(), cache_key, bpmn_spec_json)
         except Exception as e:
             current_app.logger.exception(e)
             return None
 
     @classmethod
-    def cached_element_unit_for_process(cls, cache_key: str, process_id: str) -> Optional[str]:
+    def cached_element_unit_for_process(cls, cache_key: str, process_id: str) -> Optional[BpmnSpecDict]:
         if not cls._enabled():
             return None
 
-        # for now we are importing inside each of these functions, not sure the best
-        # way to do this in an overall feature flagged strategy but this gets things
-        # moving
-        import spiff_element_units
-
         try:
-            return spiff_element_units.cached_element_unit_for_process(cls._cache_dir(), cache_key, process_id)
+            # for now we are importing inside each of these functions, not sure the best
+            # way to do this in an overall feature flagged strategy but this gets things
+            # moving
+            import spiff_element_units
+
+            bpmn_spec_json = spiff_element_units.cached_element_unit_for_process(cls._cache_dir(), cache_key, process_id)
+            return json.loads(bpmn_spec_json)
         except Exception as e:
             current_app.logger.exception(e)
             return None
 
     @classmethod
-    def cached_element_unit_for_element(cls, cache_key: str, process_id: str, element_id: str) -> Optional[str]:
+    def cached_element_unit_for_element(cls, cache_key: str, process_id: str, element_id: str) -> Optional[BpmnSpecDict]:
         if not cls._enabled():
             return None
 
-        # for now we are importing inside each of these functions, not sure the best
-        # way to do this in an overall feature flagged strategy but this gets things
-        # moving
-        import spiff_element_units
-
         try:
-            return spiff_element_units.cached_element_unit_for_element(
+            # for now we are importing inside each of these functions, not sure the best
+            # way to do this in an overall feature flagged strategy but this gets things
+            # moving
+            import spiff_element_units
+
+            bpmn_spec_json = spiff_element_units.cached_element_unit_for_element(
                 cls._cache_dir(), cache_key, process_id, element_id
             )
+            return json.loads(bpmn_spec_json)
         except Exception as e:
             current_app.logger.exception(e)
             return None
