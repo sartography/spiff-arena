20c85df0ce7dc713a37e2a74f2cf03a66991cb1f
Jon Herron <jon.herron@yahoo.com>
2023-04-15 22:00:14 -0400

    Prep to test cache

--- spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_element_units_service.py
+++ spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_element_units_service.py
@@ -1,4 +1,6 @@
+import os
 import pytest
+import tempfile
 from flask.app import Flask
 from tests.spiffworkflow_backend.helpers.base_test import BaseTest
 from typing import Optional
@@ -23,11 +25,28 @@ def app_disabled(app: Flask) -> Flask:
     yield app
 
 @pytest.fixture()
-def app_enabled(app: Flask) -> Flask:
-    app.config["SPIFFWORKFLOW_BACKEND_ELEMENT_UNITS_CACHE_DIR"] = "some_cache_dir"
-    app.config["SPIFFWORKFLOW_BACKEND_FEATURE_ELEMENT_UNITS_ENABLED"] = True
-    yield app
+def app_enabled(app_some_cache_dir: Flask) -> Flask:
+    app_some_cache_dir.config["SPIFFWORKFLOW_BACKEND_FEATURE_ELEMENT_UNITS_ENABLED"] = True
+    yield app_some_cache_dir
 
+@pytest.fixture()
+def example_specs_json_str(app: Flask) -> str:
+    path = os.path.join(
+            app.instance_path,
+            "..",
+            "..",
+            "tests",
+            "data",
+        "specs-json",
+        "no-tasks.json")
+    with open(path) as f:
+        yield f.read()
+
+@pytest.fixture()
+def tmp_cache_dir_name() -> str:
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        yield tmpdirname
+        
 class TestElementUnitsService(BaseTest):
     """Tests the ElementUnitsService."""
 
@@ -37,13 +56,13 @@ class TestElementUnitsService(BaseTest):
     ) -> None:
         assert ElementUnitsService._cache_dir() == "some_cache_dir"
 
-    def test_feature_disabled_env_is_false(
+    def test_feature_disabled_if_env_is_false(
         self,
         app_disabled: Flask,
     ) -> None:
         assert not ElementUnitsService._enabled()
         
-    def test_feature_enabled_env_is_true(
+    def test_feature_enabled_if_env_is_true(
         self,
         app_enabled: Flask,
     ) -> None:
@@ -61,3 +80,15 @@ class TestElementUnitsService(BaseTest):
     ) -> None:
         result = ElementUnitsService.cache_element_units()
         assert result is None
+        assert app_disabled.instance_path is not None
+
+    # TODO: ok to get for process when disabled
+    # TODO: ok to get for element when disabled
+
+    def test_can_write_to_cache(
+            self,
+            app_enabled: Flask,
+            example_specs_json_str: str,
+            tmp_cache_dir_name: str,
+            ) -> None:
+        pass
