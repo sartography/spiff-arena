b3cc1170aa4bcad7e59a91b5c5b44f4a9338c612
jbirddog <100367399+jbirddog@users.noreply.github.com>
2023-05-11 11:08:07 -0400

    Type ahead cleanup (#253)

--- spiffworkflow-backend/src/spiffworkflow_backend/routes/connector_proxy_controller.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/routes/connector_proxy_controller.py
@@ -8,18 +8,14 @@ from flask.wrappers import Response
 
 def connector_proxy_typeahead_url() -> Any:
     """Returns the connector proxy type ahead url."""
-    return current_app.config["SPIFFWORKFLOW_BACKEND_CONNECTOR_PROXY_TYPE_AHEAD_URL"]
+    return current_app.config["SPIFFWORKFLOW_BACKEND_CONNECTOR_PROXY_TYPEAHEAD_URL"]
 
 
 def typeahead(category: str, prefix: str, limit: int) -> flask.wrappers.Response:
-    url = f"{connector_proxy_typeahead_url()}/v1/type-ahead/{category}?prefix={prefix}&limit={limit}"
+    url = f"{connector_proxy_typeahead_url()}/v1/typeahead/{category}?prefix={prefix}&limit={limit}"
 
     proxy_response = requests.get(url)
     status = proxy_response.status_code
-    if status // 100 == 2:
-        response = proxy_response.text
-    else:
-        # supress pop up errors on the client
-        status = 200
-        response = "[]"
+    response = proxy_response.text
+
     return Response(response, status=status, mimetype="application/json")
