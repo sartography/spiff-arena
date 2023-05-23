520325adec61c6e2e1663fb8274070bd0558515e
jasquat <jasquat@users.noreply.github.com>
2023-05-23 10:33:37 -0400

    make the test case results errors work with WorkflowTaskException and use in ErrorForDisplay

--- spiffworkflow-frontend/src/components/ProcessModelTestRun.tsx
+++ spiffworkflow-frontend/src/components/ProcessModelTestRun.tsx
@@ -3,19 +3,24 @@ import { Button, Modal } from '@carbon/react';
 import { useState } from 'react';
 import { useUriListForPermissions } from '../hooks/UriListForPermissions';
 import HttpService from '../services/HttpService';
-import { ProcessFile } from '../interfaces';
+import { ProcessFile, TestCaseResult, TestCaseResults } from '../interfaces';
+import {
+  childrenForErrorObject,
+  errorForDisplayFromTestCaseErrorDetails,
+} from './ErrorDisplay';
 
 type OwnProps = {
   processModelFile: ProcessFile;
 };
 
 export default function ProcessModelTestRun({ processModelFile }: OwnProps) {
-  const [testCaseResults, setTestCaseResults] = useState<any>(null);
+  const [testCaseResults, setTestCaseResults] =
+    useState<TestCaseResults | null>(null);
   const [showTestCaseResultsModal, setShowTestCaseResultsModal] =
     useState<boolean>(false);
   const { targetUris } = useUriListForPermissions();
 
-  const onProcessModelTestRunSuccess = (result: any) => {
+  const onProcessModelTestRunSuccess = (result: TestCaseResults) => {
     setTestCaseResults(result);
   };
 
@@ -60,18 +65,80 @@ export default function ProcessModelTestRun({ processModelFile }: OwnProps) {
     });
   };
 
+  const testCaseFormattedResultTag = () => {
+    if (!testCaseResults) {
+      return null;
+    }
+
+    const passingRows: any[] = [];
+    const failingRows: any[] = [];
+
+    testCaseResults.passing.forEach((testCaseResult: TestCaseResult) => {
+      passingRows.push(<p>{testCaseResult.test_case_identifier}</p>);
+    });
+
+    testCaseResults.failing
+      .slice(0, 2)
+      .forEach((testCaseResult: TestCaseResult) => {
+        if (testCaseResult.test_case_error_details) {
+          const errorForDisplay = errorForDisplayFromTestCaseErrorDetails(
+            testCaseResult.test_case_error_details
+          );
+          const errorChildren = childrenForErrorObject(errorForDisplay);
+          failingRows.push(
+            <>
+              <br />
+              <p>
+                Test Case:{' '}
+                <strong>{testCaseResult.test_case_identifier}</strong>
+              </p>
+              {errorChildren}
+            </>
+          );
+        }
+      });
+
+    return (
+      <>
+        <p>Passing: {testCaseResults.passing.length}</p>
+        <p>Failing: {testCaseResults.failing.length}</p>
+        <br />
+        {failingRows.length > 0 ? (
+          <>
+            <p>Failure Details:</p>
+            {failingRows}
+          </>
+        ) : null}
+        {passingRows.length > 0 ? (
+          <>
+            <p>Successful Test Cases:</p>
+            {passingRows}
+          </>
+        ) : null}
+      </>
+    );
+  };
+
   const testCaseResultsModal = () => {
+    if (!testCaseResults) {
+      return null;
+    }
+
+    let modalHeading = 'All Tests PASSED';
+    if (!testCaseResults.all_passed) {
+      modalHeading = 'Some Tests FAILED';
+    }
     return (
       <Modal
         open={showTestCaseResultsModal}
         data-qa="test-case-results-modal"
-        modalHeading="RESULT FOR"
-        modalLabel="LABLE"
+        modalHeading={modalHeading}
+        modalLabel="Test Case Rsults"
         primaryButtonText="OK"
         onRequestSubmit={() => setShowTestCaseResultsModal(false)}
         onRequestClose={() => setShowTestCaseResultsModal(false)}
       >
-        {JSON.stringify(testCaseResults)}
+        {testCaseFormattedResultTag()}
       </Modal>
     );
   };
