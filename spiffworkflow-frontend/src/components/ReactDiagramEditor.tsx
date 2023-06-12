4c92e9fe4928ce265e2315372284dc767f806f9e
Elizabeth Esswein <elizabeth.esswein@gmail.com>
2023-06-12 16:54:10 -0400

    ignore dmn files when restoring references

--- spiffworkflow-frontend/src/components/ReactDiagramEditor.tsx
+++ spiffworkflow-frontend/src/components/ReactDiagramEditor.tsx
@@ -467,32 +467,33 @@ export default function ReactDiagramEditor({
       if (alreadyImportedXmlRef.current) {
         return;
       }
-      diagramModelerToUse._moddle // eslint-disable-line no-underscore-dangle
-        .fromXML(diagramXMLToDisplay)
-        .then((result: any) => {
-          const refs = result.references.filter(
-            (r: any) =>
-              r.property === 'bpmn:loopDataInputRef' ||
-              r.property === 'bpmn:loopDataOutputRef'
-          );
-          const desc =
-            diagramModelerToUse._moddle.registry.getEffectiveDescriptor( // eslint-disable-line
-              'bpmn:ItemAwareElement'
+      if (diagramType === 'bpmn') {
+        diagramModelerToUse._moddle // eslint-disable-line no-underscore-dangle
+          .fromXML(diagramXMLToDisplay)
+          .then((result: any) => {
+            const refs = result.references.filter(
+              (r: any) =>
+                r.property === 'bpmn:loopDataInputRef' ||
+                r.property === 'bpmn:loopDataOutputRef'
             );
-          refs.forEach((ref: any) => {
-            const props = {
-              id: ref.id,
-              name: ref.id ? typeof ref.name === 'undefined' : ref.name,
-            };
-            const elem = diagramModelerToUse._moddle.create(desc, props); // eslint-disable-line no-underscore-dangle
-            elem.$parent = ref.element;
-            ref.element.set(ref.property, elem);
+            const desc =
+              diagramModelerToUse._moddle.registry.getEffectiveDescriptor( // eslint-disable-line
+                'bpmn:ItemAwareElement'
+              );
+            refs.forEach((ref: any) => {
+              const props = {
+                id: ref.id,
+                name: ref.id ? typeof ref.name === 'undefined' : ref.name,
+              };
+              const elem = diagramModelerToUse._moddle.create(desc, props); // eslint-disable-line no-underscore-dangle
+              elem.$parent = ref.element;
+              ref.element.set(ref.property, elem);
+            });
+            diagramModelerToUse.importDefinitions(result.rootElement);
           });
-          diagramModelerToUse.importDefinitions(result.rootElement);
-          if (diagramType === 'bpmn' || diagramType === 'readonly') {
-            diagramModelerToUse.get('canvas').zoom('fit-viewport');
-          }
-        });
+      } else {
+        diagramModelerToUse.importXML(diagramXMLToDisplay);
+      }
 
       alreadyImportedXmlRef.current = true;
     }
