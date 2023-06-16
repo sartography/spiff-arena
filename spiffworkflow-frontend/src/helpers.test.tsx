de39a3ebd4b5a58c513db20413169aa3108a99a5
Jakub Sokołowski <jakub@status.im>
2023-06-16 10:21:24 +0200

    Revert "set undefined values to null rjsf form data w/ burnettk (#336)"
    This reverts commit 1f40f64695782de798c49b5b60d83fca299a174a.

--- spiffworkflow-frontend/src/helpers.test.tsx
+++ spiffworkflow-frontend/src/helpers.test.tsx
@@ -3,7 +3,6 @@ import {
   isInteger,
   slugifyString,
   underscorizeString,
-  recursivelyNullifyUndefinedValuesInPlace,
 } from './helpers';
 
 test('it can slugify a string', () => {
@@ -29,39 +28,4 @@ test('it can validate numeric values', () => {
   expect(isInteger('        ')).toEqual(false);
   expect(isInteger('1 2')).toEqual(false);
   expect(isInteger(2)).toEqual(true);
-});
-
-test('it can replace undefined values in object with null', () => {
-  const sampleData = {
-    talentType: 'foo',
-    rating: 'bar',
-    contacts: {
-      gmeets: undefined,
-      zoom: undefined,
-      phone: 'baz',
-      awesome: false,
-      info: '',
-    },
-    items: [
-      undefined,
-      {
-        contacts: {
-          gmeets: undefined,
-          zoom: undefined,
-          phone: 'baz',
-        },
-      },
-      'HEY',
-    ],
-    undefined,
-  };
-
-  expect((sampleData.items[1] as any).contacts.zoom).toEqual(undefined);
-
-  const result = recursivelyNullifyUndefinedValuesInPlace(sampleData);
-  expect(result).toEqual(sampleData);
-  expect(result.items[1].contacts.zoom).toEqual(null);
-  expect(result.items[2]).toEqual('HEY');
-  expect(result.contacts.awesome).toEqual(false);
-  expect(result.contacts.info).toEqual('');
 });
