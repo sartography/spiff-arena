dca6a7d58d67ca56d4d00d0d63d0b9b9198bbe3b
Jon Herron <jon.herron@yahoo.com>
2023-07-03 17:18:52 -0400

    WIP

---
+++ spiffworkflow-backend/src/spiffworkflow_backend/services/oauth_service.py
@@ -0,0 +1,18 @@
+# TODO: get this from somewhere dynamic, admins need to edit from the UI
+# TODO: ^ in the interim, need to get client_id/secret from env? secrets?
+AUTHS = {
+      "airtable": {
+            "name": "airtable",
+            "version": "2",
+            "client_id": "XXX",
+            "client_secret": "XXX",
+            "endpoint_url": "https://airtable.com/",
+            "authorization_url": "https://airtable.com/oauth2/v1/authorize",
+            "access_token_url": "https://airtable.com/oauth2/v1/token",
+            "refresh_token_url": "https://airtable.com/oauth2/v1/token",
+            "scope": "data.records:read schema.bases:read",
+      },
+}
+
+class OAuthService:
+      pass
