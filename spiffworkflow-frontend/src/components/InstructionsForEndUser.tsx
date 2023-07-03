99de1432b06034afb41c743335eec19f79afbf34
jasquat <2487833+jasquat@users.noreply.github.com>
2023-07-03 01:24:54 -0400

    Feature/interstitial summary part 3 (#367)
    * throw an error if backend gives a 500 from interstitial page w/ burnettk
    
    * turn unexpected errors for the interstitial page into event stream responses to avoid error misdirection
    
    * pyl
    
    * raise the api_error from the original and make the render_data method private
    
    * Feature/interstitial summary (#337)
    
    * * Process instance logs and messages are now components rather than pages, and are included within tabs on the process instance page, along with the diagram.
    * Removed the Zoom and Move modules when showing the readonly
      Diagram.  Assured this readonly view is resized to fit the space when possible.
    * Checkbox Widget no longer displays a duplicate label.
    * CSS Tweaks
      * All pages are limited to a max display width of 1440, with auto margins to center the main content on the page.
      * "Show" pages, like ProcessInstanceShow, TaskShow have the primary content limited to 1000, also with auto-margins.
      * Paragraphs, headings, blockquotes, list items are limited to a width of 640.
      * Reduced margin bottom on all breadcrumbs.
      * Slightly reduced the width and margin of tiles
      * ordered lists and unordered lists show numbers and bullets now.
    * End user Instructions component can, optionally, auto-collapse, so that only a portion is displayed, along with a toggle.  This is how it is set up for the ProcessInstanceShow page.
    * Greatly reduced the lag in the interstitial page when doing a re-direct.
    
    * run_pyl
    
    * kill console
    
    * wait for permissionsLoaded too since we are using ability.can
    
    * Previous change removed the top level Messages page - this re-adds it.
    
    * I am always, ALWAYS shocked at how I can not wrap my head around when and where to use "useEffect".
    
    This should cause the show/hide Instructions toggle to only show when useful.
    
    * Minor cleanup on the process instance properties display.
    
    * linting
    
    ---------
    
    Co-authored-by: burnettk <burnettk@users.noreply.github.com>
    
    * Fix a linting error.
    
    * minor permissions related fixes for the new ui.
    
    * Revert "Revert "Feature/better subworkflow management (#331)""
    
    This reverts commit 3becffc14f03f79f31582ea2b7f1610eed0e71bc.
    
    * do not execute tasks from the process instance show page w/ burnettk
    
    * pyl w/ burnettk
    
    * Very minor UI tweak to resolve a number of issues mentioned in SPIFF-316 Notion Ticket:
    
    * Markdown links now open in a new window
    * Tables on the home pages are now contained within Grids to better align them with the other content that is in a grid.
    * Right aligned the "process instance list link" button to it is flush right with the table underneath
    * Gave a little more breathing room the content on the info/metadata in the process instance view .
    
    * updated docker image build action to contain the appropriate version info w/ burnettk
    
    * properly resize to the available columns depending on s/m/l
    
    ---------
    
    Co-authored-by: jasquat <jasquat@users.noreply.github.com>
    Co-authored-by: Dan Funk <daniel.h.funk@gmail.com>
    Co-authored-by: burnettk <burnettk@users.noreply.github.com>

--- spiffworkflow-frontend/src/components/InstructionsForEndUser.tsx
+++ spiffworkflow-frontend/src/components/InstructionsForEndUser.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // @ts-ignore
 import MDEditor from '@uiw/react-md-editor';
+import { Toggle } from '@carbon/react';
 
 type OwnProps = {
   task: any;
   defaultMessage?: string;
+  allowCollapse?: boolean;
 };
 
 export default function InstructionsForEndUser({
   task,
   defaultMessage = '',
+  allowCollapse = false,
 }: OwnProps) {
-  if (!task) {
-    return null;
-  }
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+  const [collapsable, setCollapsable] = useState<boolean>(false);
   let instructions = defaultMessage;
   let { properties } = task;
   if (!properties) {
@@ -23,15 +25,80 @@ export default function InstructionsForEndUser({
   if (instructionsForEndUser) {
     instructions = instructionsForEndUser;
   }
+
+  const maxLineCount: number = 8;
+  const maxWordCount: number = 75;
+
+  const lineCount = (arg: string) => {
+    return arg.split('\n').length;
+  };
+
+  const wordCount = (arg: string) => {
+    return arg.split(' ').length;
+  };
+
+  useEffect(() => {
+    if (
+      allowCollapse &&
+      (lineCount(instructions) >= maxLineCount ||
+        wordCount(instructions) > maxWordCount)
+    ) {
+      setCollapsable(true);
+      setCollapsed(true);
+    } else {
+      setCollapsable(false);
+      setCollapsed(false);
+    }
+  }, [allowCollapse, instructions]);
+
+  if (!task) {
+    return null;
+  }
+
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
+  const showCollapseToggle = () => {
+    if (collapsable) {
+      return (
+        <Toggle
+          labelA="Show More"
+          labelB="Show Less"
+          onToggle={toggleCollapse}
+          id="toggle-collapse"
+        />
+      );
+    }
+    return null;
+  };
+
+  let instructionsShown = instructions;
+  if (collapsed) {
+    if (wordCount(instructions) > maxWordCount) {
+      instructionsShown = instructions
+        .split(' ')
+        .slice(0, maxWordCount)
+        .join(' ');
+      instructionsShown += '...';
+    } else if (lineCount(instructions) > maxLineCount) {
+      instructionsShown = instructions.split('\n').slice(0, 5).join(' ');
+      instructionsShown += '...';
+    }
+  }
+
   return (
-    <div className="markdown">
-      {/*
-        https://www.npmjs.com/package/@uiw/react-md-editor switches to dark mode by default by respecting @media (prefers-color-scheme: dark)
-        This makes it look like our site is broken, so until the rest of the site supports dark mode, turn off dark mode for this component.
-      */}
-      <div data-color-mode="light">
-        <MDEditor.Markdown source={instructions} />
+    <div style={{ margin: '20px 0 20px 0' }}>
+      <div className="markdown">
+        {/*
+          https://www.npmjs.com/package/@uiw/react-md-editor switches to dark mode by default by respecting @media (prefers-color-scheme: dark)
+          This makes it look like our site is broken, so until the rest of the site supports dark mode, turn off dark mode for this component.
+        */}
+        <div data-color-mode="light">
+          <MDEditor.Markdown linkTarget="_blank" source={instructionsShown} />
+        </div>
       </div>
+      {showCollapseToggle()}
     </div>
   );
 }
