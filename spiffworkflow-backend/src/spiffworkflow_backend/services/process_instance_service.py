2b6d1c334f407c6d8a7a6fd42bb0cd8840385f0c
Dan Funk <daniel.h.funk@gmail.com>
2023-07-07 15:49:52 -0400

    make sure we have a current processor so we don't return null (#379)
    * make sure we have a current processor so we don't return null
    
    * remove sleep
    
    * The background processor now only picks up processes that were last updated more than a minute ago to avoid conflicting with the interstitial page.  With the understanding that we can rmeove this limitation when we can refactor to allow the backend processes to provide updates on what they are doing.
    
    * pyl w/ burnettk
    
    ---------
    
    Co-authored-by: jasquat <jasquat@users.noreply.github.com>

--- spiffworkflow-backend/src/spiffworkflow_backend/services/process_instance_service.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/services/process_instance_service.py
@@ -187,8 +187,9 @@ class ProcessInstanceService:
     @classmethod
     def do_waiting(cls, status_value: str) -> None:
         run_at_in_seconds_threshold = round(time.time())
+        min_age_in_seconds = 60  # to avoid conflicts with the interstitial page, we wait 60 seconds before processing
         process_instance_ids_to_check = ProcessInstanceQueueService.peek_many(
-            status_value, run_at_in_seconds_threshold
+            status_value, run_at_in_seconds_threshold, min_age_in_seconds
         )
         if len(process_instance_ids_to_check) == 0:
             return
