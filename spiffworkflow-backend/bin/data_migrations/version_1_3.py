21ff50f0eb6b6909bf6693e13bb7e53d01078c35
jasquat <jasquat@users.noreply.github.com>
2023-07-14 16:41:17 -0400

    updates to migration script so it can work w/ burnettk

--- spiffworkflow-backend/bin/data_migrations/version_1_3.py
+++ spiffworkflow-backend/bin/data_migrations/version_1_3.py
@@ -1,12 +1,17 @@
 import json
 import os
+from SpiffWorkflow.task import TaskStateNames # type: ignore
+import copy
 import uuid
 from hashlib import sha256
+from sqlalchemy import or_
+from sqlalchemy.orm.attributes import flag_modified  # type: ignore
 
 from spiffworkflow_backend import create_app
 from spiffworkflow_backend.models.db import db
 from spiffworkflow_backend.models.task import TaskModel  # noqa: F401
 from spiffworkflow_backend.models.task_definition import TaskDefinitionModel
+from spiffworkflow_backend.models.task import Task
 
 
 class VersionOneThree:
@@ -24,6 +29,16 @@ class VersionOneThree:
                 relating_task_models = TaskModel.query.filter_by(task_definition_id=task_definition.id).all()
                 for task_model in relating_task_models:
                     self.process_task_model(task_model, task_definition)
+
+            task_definitions_with_events = TaskDefinitionModel.query.filter(
+                or_(
+                    TaskDefinitionModel.typename.like("%Event%"),  # type: ignore
+                    TaskDefinitionModel.typename.in_(["SendTask", "ReceiveTask"]),  # type: ignore
+                )
+            ).all()
+            for tdwe in task_definitions_with_events:
+                self.update_event_definitions(tdwe)
+
             db.session.commit()
 
     def get_relevant_task_definitions(self) -> list[TaskDefinitionModel]:
@@ -38,11 +53,12 @@ class VersionOneThree:
             "BoundaryEventParent", "BoundaryEventSplit"
         )
 
-        properties_json = task_definition.properties_json
+        properties_json = copy.copy(task_definition.properties_json)
         properties_json.pop("main_child_task_spec")
         properties_json["typename"] = task_definition.typename
         properties_json["name"] = task_definition.bpmn_identifier
         task_definition.properties_json = properties_json
+        flag_modified(task_definition, 'properties_json')  # type: ignore
         db.session.add(task_definition)
 
         join_properties_json = {
@@ -75,6 +91,7 @@ class VersionOneThree:
                 name.replace("BoundaryEventParent", "BoundaryEventSplit")
                 for name in parent_task_definition.properties_json["outputs"]
             ]
+            flag_modified(parent_task_definition, 'properties_json')  # type: ignore
             db.session.add(parent_task_definition)
 
         for child_bpmn_identifier in properties_json["outputs"]:
@@ -87,21 +104,25 @@ class VersionOneThree:
                 name.replace("BoundaryEventParent", "BoundaryEventSplit")
                 for name in child_task_definition.properties_json["inputs"]
             ]
+            flag_modified(child_task_definition, 'properties_json')  # type: ignore
             db.session.add(child_task_definition)
 
     def process_task_model(self, task_model: TaskModel, task_definition: TaskDefinitionModel) -> None:
         task_model.properties_json["task_spec"] = task_definition.bpmn_identifier
+        flag_modified(task_model, 'properties_json')  # type: ignore
         db.session.add(task_model)
 
         child_task_models = []
         all_children_completed = True
 
         # Ruff keeps complaining unless it's done like this
-        blank_json = json.dumps("{}")
+        blank_json = json.dumps({})
         blank_json_data_hash = sha256(blank_json.encode("utf8")).hexdigest()
 
         for child_task_guid in task_model.properties_json["children"]:
             child_task_model = TaskModel.query.filter_by(guid=child_task_guid).first()
+            if child_task_model is None:
+                continue
             if child_task_model.state not in ["COMPLETED", "CANCELLED"]:
                 all_children_completed = False
             child_task_models.append(child_task_model)
@@ -118,7 +139,7 @@ class VersionOneThree:
             if child_task_model.state in ["MAYBE", "LIKELY", "FUTURE"]:
                 new_task_state = child_task_model.state
             elif child_task_model.state in ["WAITING", "READY", "STARTED"]:
-                new_task_state = "FUTURUE"
+                new_task_state = "FUTURE"
             elif child_task_model.state == "COMPLETED":
                 if all_children_completed:
                     new_task_state = "COMPLETED"
@@ -134,7 +155,7 @@ class VersionOneThree:
                 "id": str(uuid.uuid4()),
                 "parent": child_task_model.guid,
                 "children": [],
-                "state": new_task_state,
+                "state": Task.task_state_name_to_int(new_task_state),
                 "task_spec": task_definition.bpmn_identifier.replace("BoundaryEventSplit", "BoundaryEventJoin"),
                 "last_state_change": None,
                 "triggered": False,
@@ -146,7 +167,7 @@ class VersionOneThree:
                 bpmn_process_id=task_model.bpmn_process_id,
                 process_instance_id=task_model.process_instance_id,
                 task_definition_id=task_model.task_definition_id,
-                state=new_task_properties_json["state"],
+                state=new_task_state,
                 properties_json=new_task_properties_json,
                 start_in_seconds=start_in_seconds,
                 end_in_seconds=end_in_seconds,
@@ -156,7 +177,22 @@ class VersionOneThree:
             db.session.add(new_task_model)
 
             child_task_model.properties_json["children"].append(new_task_model.guid)
+            flag_modified(child_task_model, 'properties_json')  # type: ignore
             db.session.add(child_task_model)
+
+
+    def update_event_definitions(self, task_definition: TaskDefinitionModel) -> None:
+        if 'event_definition' in task_definition.properties_json:
+            properties_json = copy.copy(task_definition.properties_json)
+            properties_json['event_definition'].pop('internal', None)
+            properties_json['event_definition'].pop('external', None)
+            if 'escalation_code' in properties_json['event_definition']:
+                properties_json['event_definition']['code'] = properties_json['event_definition'].pop('escalation_code')
+            if 'error_code' in properties_json['event_definition']:
+                properties_json['event_definition']['code'] = properties_json['event_definition'].pop('error_code')
+            task_definition.properties_json = properties_json
+            flag_modified(task_definition, 'properties_json')  # type: ignore
+            db.session.add(task_definition)
 
 
 VersionOneThree().run()
