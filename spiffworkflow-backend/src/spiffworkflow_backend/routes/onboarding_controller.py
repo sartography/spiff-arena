5b7a015f49da973085f84b7ed9e85f871a427251
Jon Herron <jon.herron@yahoo.com>
2023-07-18 18:21:22 -0400

    Suppress any exceptions during onboarding request

--- spiffworkflow-backend/src/spiffworkflow_backend/routes/onboarding_controller.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/routes/onboarding_controller.py
@@ -1,4 +1,6 @@
 """APIs for dealing with process groups, process models, and process instances."""
+from contextlib import suppress
+
 from flask import make_response
 from flask.wrappers import Response
 
@@ -6,18 +8,20 @@ from spiffworkflow_backend.routes.process_instances_controller import process_in
 
 
 def get_onboarding() -> Response:
-    process_instance, processor = process_instance_start("misc/jonjon/onboarding1")
     result = {}
 
-    if processor is not None:
-        if process_instance.status == "complete":
-            workflow_data = processor.bpmn_process_instance.data
-            result = workflow_data.get("onboarding", {})
-        elif process_instance.status == "user_input_required" and len(process_instance.active_human_tasks) > 0:
-            result = {
-                "type": "user_input_required",
-                "process_instance_id": process_instance.id,
-                "task_id": process_instance.active_human_tasks[0].task_id,
-            }
+    with suppress(Exception):
+        process_instance, processor = process_instance_start("misc/jonjon/onboarding2")
+
+        if processor is not None:
+            if process_instance.status == "complete":
+                workflow_data = processor.bpmn_process_instance.data
+                result = workflow_data.get("onboarding", {})
+            elif process_instance.status == "user_input_required" and len(process_instance.active_human_tasks) > 0:
+                result = {
+                    "type": "user_input_required",
+                    "process_instance_id": process_instance.id,
+                    "task_id": process_instance.active_human_tasks[0].task_id,
+                }
 
     return make_response(result, 200)
