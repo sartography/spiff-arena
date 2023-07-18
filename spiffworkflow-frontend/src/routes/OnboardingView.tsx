f2c5b80d1fcb3f54f4f8d8f5cb4c977c92110e44
Jon Herron <jon.herron@yahoo.com>
2023-07-18 18:13:12 -0400

    FE lint

--- spiffworkflow-frontend/src/routes/OnboardingView.tsx
+++ spiffworkflow-frontend/src/routes/OnboardingView.tsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import HttpService from '../services/HttpService';
 import InProgressInstances from './InProgressInstances';
 import { Onboarding } from '../interfaces';
 import MyTasks from './MyTasks';
-import { useNavigate } from 'react-router-dom';
 
 export default function OnboardingView() {
   const [onboarding, setOnboarding] = useState<Onboarding | null>(null);
 
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     HttpService.makeCallToBackend({
       path: `/onboarding`,
@@ -19,19 +19,24 @@ export default function OnboardingView() {
 
   const onboardingElement = () => {
     if (onboarding) {
-      if (onboarding.type === "default_view") {
-        if (onboarding.value === "my_tasks") {
+      if (onboarding.type === 'default_view') {
+        if (onboarding.value === 'my_tasks') {
           return <MyTasks />;
         }
-      }
-      else if (onboarding.type === "user_input_required" && onboarding.process_instance_id && onboarding.task_id) {
-      console.log("HERE");
-        navigate(`/tasks/${onboarding.process_instance_id}/${onboarding.task_id}`);
+      } else if (
+        onboarding.type === 'user_input_required' &&
+        onboarding.process_instance_id &&
+        onboarding.task_id
+      ) {
+        console.log('HERE');
+        navigate(
+          `/tasks/${onboarding.process_instance_id}/${onboarding.task_id}`
+        );
       }
     }
-    
+
     return <InProgressInstances />;
-  }
+  };
 
   return onboardingElement();
 }
