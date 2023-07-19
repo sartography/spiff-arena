9ec7f3766f23f1eb47eb9659cd953a8e97d6fe79
Jon Herron <jon.herron@yahoo.com>
2023-07-19 10:48:48 -0400

    Better default location

--- spiffworkflow-backend/src/spiffworkflow_backend/routes/onboarding_controller.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/routes/onboarding_controller.py
@@ -11,7 +11,7 @@ def get_onboarding() -> Response:
     result = {}
 
     with suppress(Exception):
-        process_instance, processor = process_instance_start("misc/jonjon/onboarding1")
+        process_instance, processor = process_instance_start("site-administration/onboarding")
 
         if processor is not None:
             if process_instance.status == "complete":
