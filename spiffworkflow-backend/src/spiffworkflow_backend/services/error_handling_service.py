503e44a543f33fc9110d6b41dba74309f26c6e17
jasquat <jasquat@users.noreply.github.com>
2023-08-01 12:19:32 -0400

    run the error handler from the dequeued method instead of marking all failed process instances as error w/ burnettk

--- spiffworkflow-backend/src/spiffworkflow_backend/services/error_handling_service.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/services/error_handling_service.py
@@ -1,11 +1,9 @@
 from flask import current_app
 from flask import g
 from spiffworkflow_backend.models.db import db
-from spiffworkflow_backend.models.message_instance import MessageInstanceModel
 from spiffworkflow_backend.models.process_instance import ProcessInstanceModel
 from spiffworkflow_backend.models.process_instance import ProcessInstanceStatus
 from spiffworkflow_backend.models.process_model import ProcessModelInfo
-from spiffworkflow_backend.services.message_service import MessageService
 from spiffworkflow_backend.services.process_model_service import ProcessModelService
 
 
@@ -54,6 +52,12 @@ class ErrorHandlingService:
         process_instance: ProcessInstanceModel,
     ) -> None:
         """Send a BPMN Message - which may kick off a waiting process."""
+
+        # importing here to avoid circular imports since these imports are only needed here at runtime.
+        # we were not able to figure out which specific import was causing the issue.
+        from spiffworkflow_backend.models.message_instance import MessageInstanceModel
+        from spiffworkflow_backend.services.message_service import MessageService
+
         message_text = (
             f"There was an exception running process {process_model.id}.\nOriginal Error:\n{error.__repr__()}"
         )
