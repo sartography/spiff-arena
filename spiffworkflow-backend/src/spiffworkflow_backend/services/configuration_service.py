ce6ed8e4a4b7cf1ac2451307c864f276cbd88904
Jon Herron <jon.herron@yahoo.com>
2023-08-08 13:49:54 -0400

    Getting bin_pyl to pass

--- spiffworkflow-backend/src/spiffworkflow_backend/services/configuration_service.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/services/configuration_service.py
@@ -1,3 +1,4 @@
+import json
 from typing import Any
 
 from spiffworkflow_backend.models.configuration import ConfigurationModel
@@ -8,9 +9,11 @@ class ConfigurationService:
     @staticmethod
     def configuration_for_category(category: str) -> Any:
         config = db.session.query(ConfigurationModel).filter(ConfigurationModel.category == category).first()
-        if config is None:
+        try:
+            value = config.value if config is not None else ""
+            return json.loads(value)  # type: ignore
+        except Exception:
             return {}
-        return config.value
 
     @staticmethod
     def update_configuration_for_category(category: str, new_config: dict[str, Any]) -> None:
