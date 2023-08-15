620df632d2b8091b32837c3a9d2645555185719c
danfunk <daniel.h.funk@gmail.com>
2023-08-15 11:45:09 -0400

    The onboarding controller should not save the process model to the database.  It creates a pile of pointless noise.  So just cleaning that up.

--- spiffworkflow-backend/src/spiffworkflow_backend/routes/onboarding_controller.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/routes/onboarding_controller.py
@@ -1,39 +1,45 @@
 """APIs for dealing with process groups, process models, and process instances."""
 from contextlib import suppress
 
-from flask import make_response
+from flask import make_response, g
 from flask.wrappers import Response
 
 from spiffworkflow_backend import db
+from spiffworkflow_backend.models.process_instance import ProcessInstanceModel, ProcessInstanceStatus
 from spiffworkflow_backend.models.task import TaskModel
 from spiffworkflow_backend.routes.process_instances_controller import _process_instance_start
 from spiffworkflow_backend.services.jinja_service import JinjaService
+from spiffworkflow_backend.services.process_instance_processor import ProcessInstanceProcessor, CustomBpmnScriptEngine
 
 
 def get_onboarding() -> Response:
     result = {}
 
     with suppress(Exception):
-        process_instance, processor = _process_instance_start("site-administration/onboarding")
-
+        process_instance = ProcessInstanceModel(
+            status=ProcessInstanceStatus.not_started.value,
+            process_initiator_id=g.user.id,
+            process_model_identifier="site-administration/onboarding",
+            process_model_display_name="Onboarding",
+            persistence_level="none",
+        )
+        processor = ProcessInstanceProcessor(
+            process_instance, script_engine=CustomBpmnScriptEngine(use_restricted_script_engine=True)
+        )
+        processor.do_engine_steps(save=False, execution_strategy_name="greedy")
         if processor is not None:
-            if process_instance.status == "complete":
-                workflow_data = processor.bpmn_process_instance.data
+            bpmn_process = processor.bpmn_process_instance
+            if bpmn_process.is_completed():
+                workflow_data = bpmn_process.data
                 result = workflow_data.get("onboarding", {})
-            elif process_instance.status == "user_input_required" and len(process_instance.active_human_tasks) > 0:
+            elif len(bpmn_process.get_ready_user_tasks()) > 0:
                 result = {
                     "type": "user_input_required",
                     "process_instance_id": process_instance.id,
                 }
-
             task = processor.next_task()
-            db.session.flush()
             if task:
-                task_model: TaskModel | None = TaskModel.query.filter_by(
-                    guid=str(task.id), process_instance_id=process_instance.id
-                ).first()
-                if task_model is not None:
-                    result["task_id"] = task_model.guid
-                    result["instructions"] = JinjaService.render_instructions_for_end_user(task_model)
+                result["task_id"] = task.id
+                result["instructions"] = JinjaService.render_instructions_for_end_user(task)
 
     return make_response(result, 200)
