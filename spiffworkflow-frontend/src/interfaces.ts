c1642afd5e3611bcc7c5ff1642bf83ce5ea80a08
jasquat <2487833+jasquat@users.noreply.github.com>
2023-09-07 14:22:40 -0400

    Feature/user preference extension (#472)
    * added ability to display navigation items in user profile toggle
    
    * updated naming of some extension elements
    
    * added user property table and updates for extensions to use it w/ burnettk
    
    * moved extension ui interfaces to own file and linting issues
    
    * some updates to render markdown results on load w/ burnettk
    
    * added migration merge file w/ burnettk
    
    * moved code to fix linting issues w/ burnettk
    
    * resolved db migration conflict
    
    * removed unnecessary migrations and added just one w/ burnettk
    
    ---------
    
    Co-authored-by: jasquat <jasquat@users.noreply.github.com>

--- spiffworkflow-frontend/src/interfaces.ts
+++ spiffworkflow-frontend/src/interfaces.ts
@@ -435,29 +435,3 @@ export interface DataStore {
   name: string;
   type: string;
 }
-
-export interface UiSchemaNavItem {
-  label: string;
-  route: string;
-}
-export interface UiSchemaPageDefinition {
-  header: string;
-  api: string;
-
-  form_schema_filename?: any;
-  form_ui_schema_filename?: any;
-  markdown_instruction_filename?: string;
-  navigate_to_on_form_submit?: string;
-}
-export interface UiSchemaRoute {
-  [key: string]: UiSchemaPageDefinition;
-}
-export interface ExtensionUiSchema {
-  navigation_items?: UiSchemaNavItem[];
-  routes: UiSchemaRoute;
-}
-
-export interface ExtensionPostBody {
-  extension_input: any;
-  ui_schema_page_definition?: UiSchemaPageDefinition;
-}
