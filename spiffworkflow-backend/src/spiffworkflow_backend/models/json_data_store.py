a6640df1df52c4fb60d9b6a68796e86bc5a9c2ff
Jon Herron <jon.herron@yahoo.com>
2023-09-12 17:47:13 -0400

    WIP

--- spiffworkflow-backend/src/spiffworkflow_backend/models/json_data_store.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/models/json_data_store.py
@@ -7,7 +7,6 @@ from spiffworkflow_backend.models.db import db
 @dataclass
 class JSONDataStoreModel(SpiffworkflowBaseDBModel):
     __tablename__ = "json_data_store"
-    # TODO: revert migration, add unique constraint on name+location
 
     id: int = db.Column(db.Integer, primary_key=True)
     name: str = db.Column(db.String(255), index=True)
