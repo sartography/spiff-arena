f6d3bc8e7383e461aca531664802a86bf3a4a806
jasquat <2487833+jasquat@users.noreply.github.com>
2023-09-15 10:10:57 -0400

    Feature/api keys (#489)
    * some initial work to support user api keys w/ burnettk
    
    * some updates to store and use service accounts - migrations do not work in sqlite atm
    
    * pyl
    
    * minor tweak to the migration
    
    * refactored user route
    
    * this is working if returning user that created the service account
    
    * put back migrations from main w/ burnettk
    
    * tests pass with new migration w/ burnettk
    
    * do not remove service account permissions on refresh_permissions w/ burnettk
    
    * added new component to make some api calls to populate child components and routes w/ burnettk
    
    * allow displaying extensions in configuration tab w/ burnettk
    
    * removed service accounts controller in favor of extension and encrypt the api keys
    
    * add fuzz to username to make deleting and recreating service accounts easier
    
    * allow specifying the process id to use when running an extension w/ burnettk
    
    * allow extensions to navigate to each other on form submit w/ burnettk
    
    * removed commented out debug code
    
    ---------
    
    Co-authored-by: jasquat <jasquat@users.noreply.github.com>

---
+++ spiffworkflow-backend/src/spiffworkflow_backend/models/service_account.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import time
+import uuid
+from dataclasses import dataclass
+from hashlib import sha256
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+from spiffworkflow_backend.models.db import SpiffworkflowBaseDBModel
+from spiffworkflow_backend.models.db import db
+
+# this is designed to be used for the "service" column on the user table, which is designed to hold
+# information about which authentiation system is used to authenticate this user.
+# in this case, we are authenticating based on X-API-KEY which correlates to a known value in the spiff db.
+SPIFF_SERVICE_ACCOUNT_AUTH_SERVICE = "spiff_service_account"
+SPIFF_SERVICE_ACCOUNT_AUTH_SERVICE_ID_PREFIX = "service_account_"
+
+
+@dataclass
+class ServiceAccountModel(SpiffworkflowBaseDBModel):
+    __tablename__ = "service_account"
+    __allow_unmapped__ = True
+    __table_args__ = (db.UniqueConstraint("name", "created_by_user_id", name="service_account_uniq"),)
+
+    id: int = db.Column(db.Integer, primary_key=True)
+    name: str = db.Column(db.String(255), nullable=False, unique=False, index=True)
+    user_id: int = db.Column(ForeignKey("user.id"), nullable=False, index=True)
+    created_by_user_id: int = db.Column(ForeignKey("user.id"), nullable=False, index=True)
+
+    api_key_hash: str = db.Column(db.String(255), nullable=False, unique=True, index=True)
+
+    user = relationship("UserModel", uselist=False, cascade="delete", foreign_keys=[user_id])  # type: ignore
+
+    updated_at_in_seconds: int = db.Column(db.Integer)
+    created_at_in_seconds: int = db.Column(db.Integer)
+
+    # only to used when the service account first created to tell the user what the key is
+    api_key: str | None = None
+
+    @classmethod
+    def generate_api_key(cls) -> str:
+        return str(uuid.uuid4())
+
+    @classmethod
+    def hash_api_key(cls, unencrypted_api_key: str) -> str:
+        return sha256(unencrypted_api_key.encode("utf8")).hexdigest()
+
+    @classmethod
+    def generate_username_for_related_user(cls, service_account_name: str, created_by_user_id: int) -> str:
+        # add fuzz to username so a user can delete and recreate an api_key with the same name
+        # also make the username readable so we know where it came from even after the service account is deleted
+        creation_time_for_fuzz = time.time()
+        return f"{service_account_name}_{created_by_user_id}_{creation_time_for_fuzz}"
