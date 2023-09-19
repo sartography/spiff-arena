bc0e8a31537aac434a29754e19aa7f3eb8849b3e
jasquat <jasquat@users.noreply.github.com>
2023-09-19 09:03:06 -0400

    create a custom validation for checkboxes instead of trying to set the value to undefined

--- spiffworkflow-frontend/src/components/ReactFormBuilder/ReactFormBuilder.tsx
+++ spiffworkflow-frontend/src/components/ReactFormBuilder/ReactFormBuilder.tsx
@@ -97,7 +97,7 @@ export default function ReactFormBuilder({
         postBody: submission,
       });
     },
-    [processModelId]
+    [processModelId, fileName]
   );
 
   const createFiles = (base: string) => {
