f50eea4759c6c0ab55ff9e291bafeb742ba31155
jasquat <jasquat@users.noreply.github.com>
2023-09-25 11:31:02 -0400

    some fixes for the error page when backend is down w/ burnettk

--- spiffworkflow-frontend/src/App.tsx
+++ spiffworkflow-frontend/src/App.tsx
@@ -3,16 +3,10 @@ import { defineAbility } from '@casl/ability';
 import React from 'react';
 
 import { AbilityContext } from './contexts/Can';
-import UserService from './services/UserService';
 import APIErrorProvider from './contexts/APIErrorContext';
 import ContainerForExtensions from './ContainerForExtensions';
 
 export default function App() {
-  if (!UserService.isLoggedIn()) {
-    UserService.doLogin();
-    return null;
-  }
-
   const ability = defineAbility(() => {});
   return (
     <div className="cds--white">
