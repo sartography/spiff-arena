6c2eef081c911eb5662976b7de998c4fdeceafc8
jasquat <2487833+jasquat@users.noreply.github.com>
2023-10-03 16:00:03 -0400

    feature/use-context-with-git-commands (#524)
    * use the c option with git commands instead of using cd from python w/ burnettk
    
    * removed the cd method since we should not be using it since it is not threadsafe
    
    * pyl
    
    ---------
    
    Co-authored-by: jasquat <jasquat@users.noreply.github.com>

--- spiffworkflow-backend/src/spiffworkflow_backend/services/file_system_service.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/services/file_system_service.py
@@ -2,7 +2,6 @@ import json
 import os
 from collections.abc import Callable
 from collections.abc import Generator
-from contextlib import contextmanager
 from datetime import datetime
 from typing import Any
 
@@ -32,17 +31,6 @@ class FileSystemService:
 
     PROCESS_GROUP_JSON_FILE = "process_group.json"
     PROCESS_MODEL_JSON_FILE = "process_model.json"
-
-    # https://stackoverflow.com/a/24176022/6090676
-    @staticmethod
-    @contextmanager
-    def cd(newdir: str) -> Generator:
-        prevdir = os.getcwd()
-        os.chdir(os.path.expanduser(newdir))
-        try:
-            yield
-        finally:
-            os.chdir(prevdir)
 
     @classmethod
     def walk_files(
