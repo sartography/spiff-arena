ceb06cc22785438f52d32d394f08e669375ef9bc
jasquat <2487833+jasquat@users.noreply.github.com>
2023-10-10 11:17:09 -0400

    Hotfix/user task with timer cancel (#533)
    * cherry picked changes from b12af9f3bc625a6b12cfa0b8d908b378b6be9442 to pin form json files
    
    * use the class name to determine what a task type is w/ burnettk
    
    * initial thoughts to fix cancel timer issue w/ burnettk
    
    * added migration to run predict on all open instances w/ burnettk
    
    * remove debug, refactor data migrations, add benchmark_log_func
    
    * log progress of script
    
    * only process predicted tasks and their parents in the version 2 data miagration w/ burnettk
    
    * added data migrator and using that to run version 2 migrations when needed w/ burnettk
    
    * removed some unwanted code
    
    * fix issue, but tests still need updating
    
    * fix tests by returning code to closer to what it was
    
    ---------
    
    Co-authored-by: jasquat <jasquat@users.noreply.github.com>
    Co-authored-by: burnettk <burnettk@users.noreply.github.com>

---
+++ spiffworkflow-backend/src/spiffworkflow_backend/data_migrations/version_2.py
@@ -0,0 +1,39 @@
+import time
+
+from flask import current_app
+from SpiffWorkflow.task import Task as SpiffTask  # type: ignore
+from spiffworkflow_backend.models.process_instance import ProcessInstanceModel
+from spiffworkflow_backend.services.process_instance_processor import ProcessInstanceProcessor
+from spiffworkflow_backend.services.task_service import TaskService
+
+
+class Version2:
+    VERSION = "2"
+
+    @classmethod
+    def run(cls, process_instance: ProcessInstanceModel) -> None:
+        initial_time = time.time()
+        try:
+            processor = ProcessInstanceProcessor(process_instance)
+            processor.bpmn_process_instance._predict()
+
+            spiff_tasks = processor.bpmn_process_instance.get_tasks(updated_ts=initial_time)
+            task_service = TaskService(
+                process_instance, processor._serializer, processor.bpmn_definition_to_task_definitions_mappings
+            )
+
+            # implicit begin db transaction
+            for spiff_task in spiff_tasks:
+                cls.update_spiff_task_parents(spiff_task, task_service)
+
+            task_service.save_objects_to_database()
+        except Exception as ex:
+            current_app.logger.warning(
+                f"Failed to migrate process_instance '{process_instance.id}'. The error was {str(ex)}"
+            )
+
+    @classmethod
+    def update_spiff_task_parents(cls, spiff_task: SpiffTask, task_service: TaskService) -> None:
+        task_service.update_task_model_with_spiff_task(spiff_task)
+        if spiff_task.parent is not None:
+            cls.update_spiff_task_parents(spiff_task.parent, task_service)
