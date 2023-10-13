c8b6872442a8c1e81999502233abff8f2f811059
jasquat <jasquat@users.noreply.github.com>
2023-10-13 15:32:19 -0400

    fixed broken test w/ burnettk

--- spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_service_task_delegate.py
+++ spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_service_task_delegate.py
@@ -2,11 +2,13 @@ from unittest.mock import patch
 
 import pytest
 from flask.app import Flask
+from spiffworkflow_backend.services.process_instance_processor import ProcessInstanceProcessor
 from spiffworkflow_backend.services.secret_service import SecretService
 from spiffworkflow_backend.services.service_task_service import ConnectorProxyError
 from spiffworkflow_backend.services.service_task_service import ServiceTaskDelegate
 
 from tests.spiffworkflow_backend.helpers.base_test import BaseTest
+from tests.spiffworkflow_backend.helpers.test_data import load_test_spec
 
 
 class TestServiceTaskDelegate(BaseTest):
@@ -39,12 +41,22 @@ class TestServiceTaskDelegate(BaseTest):
         assert result == {"Authorization": "TOKEN my_secret_value-haha"}
 
     def test_invalid_call_returns_good_error_message(self, app: Flask, with_db_and_bpmn_file_cleanup: None) -> None:
+        process_model = load_test_spec(
+            process_model_id="test_group/model_with_lanes",
+            bpmn_file_name="lanes.bpmn",
+            process_model_source_directory="model_with_lanes",
+        )
+        process_instance = self.create_process_instance_from_process_model(process_model=process_model)
+        processor = ProcessInstanceProcessor(process_instance)
+        processor.do_engine_steps(save=True)
+        spiff_task = processor.next_task()
+
         with patch("requests.post") as mock_post:
             mock_post.return_value.status_code = 404
             mock_post.return_value.ok = True
             mock_post.return_value.json.return_value = ""
             with pytest.raises(ConnectorProxyError) as ae:
-                ServiceTaskDelegate.call_connector("my_invalid_operation", {}, {})
+                ServiceTaskDelegate.call_connector("my_invalid_operation", {}, spiff_task)
         assert "404" in str(ae)
         assert "The service did not find the requested resource." in str(ae)
         assert "A critical component (The connector proxy) is not responding correctly." in str(ae)
