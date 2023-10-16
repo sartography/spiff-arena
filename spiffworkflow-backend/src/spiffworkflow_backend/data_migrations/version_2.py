604e7a93e6a3a6b628643eec9a8f5c68f08e1ae5
jasquat <jasquat@users.noreply.github.com>
2023-10-16 15:02:19 -0400

    do not save events to db when running data migrations w/ burnettk

--- spiffworkflow-backend/src/spiffworkflow_backend/data_migrations/version_2.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/data_migrations/version_2.py
@@ -29,7 +29,7 @@ class Version2(DataMigrationBase):
             for spiff_task in spiff_tasks:
                 cls.update_spiff_task_parents(spiff_task, task_service)
 
-            task_service.save_objects_to_database()
+            task_service.save_objects_to_database(save_process_instance_events=False)
         except Exception as ex:
             current_app.logger.warning(
                 f"Failed to migrate process_instance '{process_instance.id}'. The error was {str(ex)}"
