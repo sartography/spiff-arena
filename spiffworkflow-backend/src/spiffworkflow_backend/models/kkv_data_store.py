ba0d39ab40c43803c7c20634ee0518a5d6249462
Jon Herron <jon.herron@yahoo.com>
2023-10-17 10:03:31 -0400

    WIP

--- spiffworkflow-backend/src/spiffworkflow_backend/models/kkv_data_store.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/models/kkv_data_store.py
@@ -9,8 +9,8 @@ class KKVDataStoreModel(SpiffworkflowBaseDBModel):
     __tablename__ = "kkv_data_store"
 
     id: int = db.Column(db.Integer, primary_key=True)
-    top_level_key: str = db.Column(db.String(255), index=True)
-    secondary_key: str = db.Column(db.String(255), index=True)
-    value: dict = db.Column(db.JSON)
-    updated_at_in_seconds: int = db.Column(db.Integer)
-    created_at_in_seconds: int = db.Column(db.Integer)
+    top_level_key: str = db.Column(db.String(255), nullable=False, index=True)
+    secondary_key: str = db.Column(db.String(255), nullable=False, index=True)
+    value: dict = db.Column(db.JSON, nullable=False)
+    updated_at_in_seconds: int = db.Column(db.Integer, nullable=False)
+    created_at_in_seconds: int = db.Column(db.Integer, nullable=False)
