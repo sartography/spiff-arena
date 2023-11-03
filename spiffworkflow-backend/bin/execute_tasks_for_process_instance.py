024066b69ef9e1a51375612ea2e4c8faeab5685e
burnettk <burnettk@users.noreply.github.com>
2023-11-03 17:53:58 -0400

    fix serialization issue with custom service task, w/ essweine

--- spiffworkflow-backend/bin/execute_tasks_for_process_instance.py
+++ spiffworkflow-backend/bin/execute_tasks_for_process_instance.py
@@ -7,7 +7,7 @@ def main() -> None:
     app = create_app()
     with app.app_context():
         execution_strategy_name = app.config["SPIFFWORKFLOW_BACKEND_ENGINE_STEP_DEFAULT_STRATEGY_BACKGROUND"]
-        process_instance = ProcessInstanceModel.query.filter_by(id=29).first()
+        process_instance = ProcessInstanceModel.query.filter_by(id=2).first()
         ProcessInstanceService.run_process_instance_with_processor(
             process_instance, execution_strategy_name=execution_strategy_name
         )
