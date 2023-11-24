9a27536d9dbf21b7005f56eb5834470e366651c0
Jon Herron <jon.herron@yahoo.com>
2023-11-24 08:06:55 -0500

    Refactor

--- spiffworkflow-backend/src/spiffworkflow_backend/routes/data_store_controller.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/routes/data_store_controller.py
@@ -4,6 +4,7 @@ import flask.wrappers
 from flask import jsonify
 from flask import make_response
 
+from spiffworkflow_backend.data_stores.crud import DataStoreCRUD
 from spiffworkflow_backend.data_stores.json import JSONDataStore
 from spiffworkflow_backend.data_stores.kkv import KKVDataStore
 from spiffworkflow_backend.data_stores.typeahead import TypeaheadDataStore
@@ -22,62 +23,35 @@ def data_store_list() -> flask.wrappers.Response:
 
     return make_response(jsonify(data_stores), 200)
 
+def _build_response(data_store_class: DataStoreCRUD, name: str, page: int, per_page: int) -> flask.wrappers.Response:
+        data_store_query = data_store_class.query_data_store(name)
+        data = data_store_query.paginate(page=page, per_page=per_page, error_out=False)
+        results = []
+        for item in data.items:
+            result = data_store_class.build_response_item(item)
+            results.append(result)
+        response_json = {
+            "results": results,
+            "pagination": {
+                "count": len(data.items),
+                "total": data.total,
+                "pages": data.pages,
+            },
+        }
+        return make_response(jsonify(response_json), 200)
 
 def data_store_item_list(
     data_store_type: str, name: str, page: int = 1, per_page: int = 100
 ) -> flask.wrappers.Response:
     """Returns a list of the items in a data store."""
 
-    # TODO: refactor
     if data_store_type == "typeahead":
-        data_store_query = TypeaheadDataStore.query_data_store(name)
-        data = data_store_query.paginate(page=page, per_page=per_page, error_out=False)
-        results = []
-        for typeahead in data.items:
-            result = TypeaheadDataStore.build_response_item(typeahead)
-            results.append(result)
-        response_json = {
-            "results": results,
-            "pagination": {
-                "count": len(data.items),
-                "total": data.total,
-                "pages": data.pages,
-            },
-        }
-        return make_response(jsonify(response_json), 200)
+        return _build_response(TypeaheadDataStore, name, page, per_page)
 
     if data_store_type == "kkv":
-        data_store_query = KKVDataStore.query_data_store(name)
-        data = data_store_query.paginate(page=page, per_page=per_page, error_out=False)
-        results = []
-        for kkv in data.items:
-            result = KKVDataStore.build_response_item(kkv)
-            results.append(result)
-        response_json = {
-            "results": results,
-            "pagination": {
-                "count": len(data.items),
-                "total": data.total,
-                "pages": data.pages,
-            },
-        }
-        return make_response(jsonify(response_json), 200)
+        return _build_response(KKVDataStore, name, page, per_page)
 
     if data_store_type == "json":
-        data_store_query = JSONDataStore.query_data_store(name)
-        data = data_store_query.paginate(page=page, per_page=per_page, error_out=False)
-        results = []
-        for item in data.items:
-            result = JSONDataStore.build_response_item(item)
-            results.append(result)
-        response_json = {
-            "results": results,
-            "pagination": {
-                "count": len(data.items),
-                "total": data.total,
-                "pages": data.pages,
-            },
-        }
-        return make_response(jsonify(response_json), 200)
+        return _build_response(JSONDataStore, name, page, per_page)
 
     raise ApiError("unknown_data_store", f"Unknown data store type: {data_store_type}", status_code=400)
