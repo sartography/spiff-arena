2565dee19ff10da3b8d1ba0c844822d276afb575
jasquat <jasquat@users.noreply.github.com>
2023-12-04 16:46:37 -0500

    cleaned up migrations w/ burnettk

--- spiffworkflow-backend/migrations/versions/e1eb75bb0c0b_.py
+++ spiffworkflow-backend/migrations/versions/e1eb75bb0c0b_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: b036313a7ee0
+Revision ID: e1eb75bb0c0b
 Revises: d8901960326e
-Create Date: 2023-11-21 15:12:33.971161
+Create Date: 2023-12-04 16:46:21.683482
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'b036313a7ee0'
+revision = 'e1eb75bb0c0b'
 down_revision = 'd8901960326e'
 branch_labels = None
 depends_on = None
@@ -21,22 +21,25 @@ def upgrade():
     op.create_table('future_task',
     sa.Column('guid', sa.String(length=36), nullable=False),
     sa.Column('run_at_in_seconds', sa.Integer(), nullable=False),
-    sa.Column('status', sa.String(length=50), nullable=False),
+    sa.Column('completed', sa.Boolean(), nullable=False),
     sa.Column('updated_at_in_seconds', sa.Integer(), nullable=False),
     sa.PrimaryKeyConstraint('guid')
     )
     with op.batch_alter_table('future_task', schema=None) as batch_op:
-        batch_op.create_index(batch_op.f('ix_future_task_status'), ['status'], unique=False)
+        batch_op.create_index(batch_op.f('ix_future_task_completed'), ['completed'], unique=False)
+        batch_op.create_index(batch_op.f('ix_future_task_run_at_in_seconds'), ['run_at_in_seconds'], unique=False)
 
     op.create_table('task_instructions_for_end_user',
     sa.Column('task_guid', sa.String(length=36), nullable=False),
-    sa.Column('instruction', sa.String(length=255), nullable=False),
+    sa.Column('instruction', sa.Text(), nullable=False),
     sa.Column('process_instance_id', sa.Integer(), nullable=False),
+    sa.Column('has_been_retrieved', sa.Boolean(), nullable=False),
     sa.Column('timestamp', sa.DECIMAL(precision=17, scale=6), nullable=False),
     sa.ForeignKeyConstraint(['process_instance_id'], ['process_instance.id'], ),
     sa.PrimaryKeyConstraint('task_guid')
     )
     with op.batch_alter_table('task_instructions_for_end_user', schema=None) as batch_op:
+        batch_op.create_index(batch_op.f('ix_task_instructions_for_end_user_has_been_retrieved'), ['has_been_retrieved'], unique=False)
         batch_op.create_index(batch_op.f('ix_task_instructions_for_end_user_process_instance_id'), ['process_instance_id'], unique=False)
         batch_op.create_index(batch_op.f('ix_task_instructions_for_end_user_timestamp'), ['timestamp'], unique=False)
 
@@ -48,10 +51,12 @@ def downgrade():
     with op.batch_alter_table('task_instructions_for_end_user', schema=None) as batch_op:
         batch_op.drop_index(batch_op.f('ix_task_instructions_for_end_user_timestamp'))
         batch_op.drop_index(batch_op.f('ix_task_instructions_for_end_user_process_instance_id'))
+        batch_op.drop_index(batch_op.f('ix_task_instructions_for_end_user_has_been_retrieved'))
 
     op.drop_table('task_instructions_for_end_user')
     with op.batch_alter_table('future_task', schema=None) as batch_op:
-        batch_op.drop_index(batch_op.f('ix_future_task_status'))
+        batch_op.drop_index(batch_op.f('ix_future_task_run_at_in_seconds'))
+        batch_op.drop_index(batch_op.f('ix_future_task_completed'))
 
     op.drop_table('future_task')
     # ### end Alembic commands ###
