b2158d625498efda439879a50f08085741fb9319
Jon Herron <jon.herron@yahoo.com>
2023-12-04 09:12:39 -0500

    Getting ./bin/pyl to pass

--- spiffworkflow-backend/src/spiffworkflow_backend/models/feature_flag.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/models/feature_flag.py
@@ -1,10 +1,13 @@
 from dataclasses import dataclass
+from typing import Any
 
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+from spiffworkflow_backend.models.cache_generation import CacheGenerationModel
 from spiffworkflow_backend.models.db import SpiffworkflowBaseDBModel
 from spiffworkflow_backend.models.db import db
 
-from sqlalchemy.orm import relationship
-from spiffworkflow_backend.models.cache_generation import CacheGenerationModel
 
 @dataclass
 class FeatureFlagModel(SpiffworkflowBaseDBModel):
@@ -15,14 +18,14 @@ class FeatureFlagModel(SpiffworkflowBaseDBModel):
     value: dict = db.Column(db.JSON, nullable=False)
     updated_at_in_seconds: int = db.Column(db.Integer, nullable=False)
     created_at_in_seconds: int = db.Column(db.Integer, nullable=False)
-    
+
     generation = relationship(CacheGenerationModel)
 
     @classmethod
-    def most_recent_feature_flags(cls) -> dict:
+    def most_recent_feature_flags(cls) -> dict[str, Any]:
         cache_generation = CacheGenerationModel.newest_generation_for_table("feature_flags")
         if cache_generation is None:
-            return None
+            return {}
 
         result = cls.query.filter_by(generation_id=cache_generation.id).first()
-        return {} if result is None else result.value
+        return {} if result is None else result.value  # type: ignore
--- spiffworkflow-backend/src/spiffworkflow_backend/services/feature_flag_service.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/services/feature_flag_service.py
@@ -1,19 +1,22 @@
-from flask import g, current_app
+from typing import Any
+
+from flask import current_app
+from flask import g
+
 from spiffworkflow_backend.models.feature_flag import FeatureFlagModel
 
 GLOBAL_FEATURE_FLAGS = "__spiff_global_feature_flags"
 
-class FeatureFlagService:
 
+class FeatureFlagService:
     @classmethod
     def _process_model_specific_value(cls, name: str, feature_flags: dict[str, Any]) -> bool | None:
         tld = current_app.config.get("THREAD_LOCAL_DATA")
-        
+
         if not tld or not hasattr(tld, "process_model_identifier"):
             return None
 
-        return feature_flags.get(tld.process_model_identifier, {}).get(name)
-        
+        return feature_flags.get(tld.process_model_identifier, {}).get(name)  # type: ignore
 
     @classmethod
     def feature_enabled(cls, name: str, enabled_by_default: bool = False) -> bool:
@@ -26,4 +29,4 @@ class FeatureFlagService:
         if value is not None:
             return value
 
-        return g.feature_flags.get(GLOBAL_FEATURE_FLAGS, {}).get(name, enabled_by_default)
+        return g.feature_flags.get(GLOBAL_FEATURE_FLAGS, {}).get(name, enabled_by_default)  # type: ignore
