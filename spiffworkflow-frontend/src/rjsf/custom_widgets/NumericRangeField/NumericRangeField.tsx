270b96023ab93d693d1edfa8d4ef33e674bf4d3d
Kevin Burnett <18027+burnettk@users.noreply.github.com>
2023-12-13 10:50:26 -0800

    Numeric range field added (#799) (#800)
    * Numeric range field added (#799)
    
    Added a custom field to handle numeric ranges. The field checks for the first key in the properties of the schema and will add min and max to it when the values are put in.
    
    * autofix lint stuff
    
    * updates to simplify the code, fix a few ux issues, and make more inline with current conventions w/ burnettk
    
    * removed subkey from numeric range and added validation to ensure max is greater than min w/ burnettk
    
    * added some comments to clarify how to use numeric range w/ burnettk
    
    * added some documentation for the numeric range field w/ burnettk
    
    ---------
    
    Co-authored-by: Kayvon-Martinez <142514475+Kayvon-Martinez@users.noreply.github.com>
    Co-authored-by: burnettk <burnettk@users.noreply.github.com>
    Co-authored-by: jasquat <jasquat@users.noreply.github.com>

---
+++ spiffworkflow-frontend/src/rjsf/custom_widgets/NumericRangeField/NumericRangeField.tsx
@@ -0,0 +1,155 @@
+import {
+  descriptionId,
+  FieldProps,
+  getTemplate,
+  getUiOptions,
+} from '@rjsf/utils';
+import { TextInput } from '@carbon/react';
+import { getCommonAttributes } from '../../helpers';
+
+// Example jsonSchema - NOTE: the "min" and "max" properties are special names and must be used:
+//    compensation":{
+//      "title": "Compensation (yearly), USD",
+//      "type": "object",
+//      "properties": {
+//        "min": {
+//          "type": "number"
+//        },
+//        "max": {
+//          "type": "number"
+//        }
+//      }
+//    }
+//
+//  Example uiSchema:
+//    "compensation": {
+//      "ui:field": "numeric-range",
+//    }
+
+// eslint-disable-next-line sonarjs/cognitive-complexity
+export default function NumericRangeField({
+  id,
+  schema,
+  uiSchema,
+  idSchema,
+  disabled,
+  readonly,
+  onChange,
+  autofocus,
+  label,
+  rawErrors = [],
+  formData,
+  registry,
+}: FieldProps) {
+  const commonAttributes = getCommonAttributes(
+    label,
+    schema,
+    uiSchema,
+    rawErrors
+  );
+
+  const description = schema?.description || uiSchema?.['ui:description'];
+
+  const uiOptions = getUiOptions(uiSchema || {});
+  const DescriptionFieldTemplate = getTemplate(
+    'DescriptionFieldTemplate',
+    registry,
+    uiOptions
+  );
+
+  const formatNumberString = (numberString: string): string => {
+    if (numberString) {
+      return numberString.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+    return '0';
+  };
+
+  const parseNumberString = (numberString: string) =>
+    Number(numberString.replace(/,/g, ''));
+
+  // create two number inputs for min and max compensation
+  const min = formData?.min || 0;
+  const max = formData?.max || 0;
+
+  // the text input eventually breaks when the number gets too big.
+  // we are not sure what the cut off really is but seems unlikely
+  // people will need to go this high.
+  const maxNumber = 999_999_999_999;
+
+  const onChangeLocal = (nameToChange: any, event: any) => {
+    event.preventDefault();
+    const numberValue = parseNumberString(event.target.value);
+    if (numberValue > maxNumber) {
+      return;
+    }
+    if (!disabled && !readonly) {
+      if (nameToChange === 'min' && numberValue > max) {
+        onChange({
+          ...(formData || {}),
+          min: numberValue,
+          max: numberValue,
+        });
+      } else {
+        onChange({
+          ...(formData || {}),
+          ...{ max, min },
+          [nameToChange]: numberValue,
+        });
+      }
+    }
+  };
+
+  return (
+    <div className="numeric--range-field-wrapper">
+      <div className="numeric--range-field-label">
+        <h5>{commonAttributes.label}</h5>
+        {description && (
+          <div className="markdown-field-desc-text">
+            <DescriptionFieldTemplate
+              id={descriptionId(idSchema)}
+              description={description}
+              schema={schema}
+              uiSchema={uiSchema}
+              registry={registry}
+            />
+          </div>
+        )}
+      </div>
+      <div className="numeric--range-field-inputs">
+        <TextInput
+          id={`${id}-min`}
+          labelText={`Minimum ${schema.title || ''}`}
+          disabled={disabled}
+          readonly={readonly}
+          value={formatNumberString(min)}
+          onChange={(values: any) => {
+            onChangeLocal('min', values);
+          }}
+          invalid={commonAttributes.invalid}
+          defaultValue="0"
+          autofocus={autofocus}
+        />
+        <TextInput
+          id={`${id}-max`}
+          labelText={`Maximum ${schema.title || ''}`}
+          disabled={disabled}
+          readonly={readonly}
+          value={formatNumberString(max)}
+          onChange={(values: any) => onChangeLocal('max', values)}
+          defaultValue="0"
+          invalid={commonAttributes.invalid}
+        />
+      </div>
+      {commonAttributes.errorMessageForField && (
+        <div className="error-message">
+          {commonAttributes.errorMessageForField}
+        </div>
+      )}
+      {commonAttributes.helperText && (
+        <p className="numeric--range-field-help-text">
+          {commonAttributes.helperText}
+        </p>
+      )}
+    </div>
+  );
+}
