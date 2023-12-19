cad215b8d4378205fbf05ef0f210be28e131ad96
Jon Herron <jon.herron@yahoo.com>
2023-12-19 08:08:37 -0500

    Add drop table migration

--- spiffworkflow-backend/src/spiffworkflow_backend/models/json_data_store.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/models/json_data_store.py
@@ -4,14 +4,15 @@ from spiffworkflow_backend.models.db import SpiffworkflowBaseDBModel
 from spiffworkflow_backend.models.db import db
 
 
-@dataclass
-class JSONDataStoreModel(SpiffworkflowBaseDBModel):
-    __tablename__ = "json_data_store"
+#@dataclass
+class JSONDataStoreModel(): #SpiffworkflowBaseDBModel):
+   #__tablename__ = "json_data_store"
 
-    id: int = db.Column(db.Integer, primary_key=True)
-    name: str = db.Column(db.String(255), index=True)
-    location: str = db.Column(db.String(255))
-    schema: dict = db.Column(db.JSON)
-    data: dict = db.Column(db.JSON)
-    updated_at_in_seconds: int = db.Column(db.Integer)
-    created_at_in_seconds: int = db.Column(db.Integer)
+   id: int #= db.Column(db.Integer, primary_key=True)
+   name: str #= db.Column(db.String(255), index=True)
+   location: str #= db.Column(db.String(255))
+   schema: dict #= db.Column(db.JSON)
+   data: dict #= db.Column(db.JSON)
+   updated_at_in_seconds: int #= db.Column(db.Integer)
+   created_at_in_seconds: int #= db.Column(db.Integer)
+ 
