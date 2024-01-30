711da2b048d00349526d16d5afa4670f3d0195f9
Kevin Burnett <18027+burnettk@users.noreply.github.com>
2024-01-30 12:48:44 -0800

    unique bpmn process ids (#927)
    * remove SpecReferenceCache
    
    * make sure strings are sometimes unique
    
    * lint
    
    * more random
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: burnettk <burnettk@users.noreply.github.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- spiffworkflow-backend/src/spiffworkflow_backend/routes/process_models_controller.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/routes/process_models_controller.py
@@ -1,7 +1,9 @@
 """APIs for dealing with process groups, process models, and process instances."""
 import json
 import os
+import random
 import re
+import string
 from hashlib import sha256
 from typing import Any
 
@@ -83,6 +85,15 @@ def process_model_create(
     with open(template_file) as f:
         contents = f.read()
     process_model_id_for_bpmn_file = process_model_info.id.split("/")[-1]
+
+    # convert dashes to underscores for process id
+    underscored_process_id = process_model_id_for_bpmn_file.replace("-", "_")
+
+    # make process id unique by adding random string to add
+    fuzz = "".join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(7))
+    process_id_with_fuzz = f"Process_{underscored_process_id}_{fuzz}"
+    contents = contents.replace("Process_replace_me_just_for_template", process_id_with_fuzz)
+
     SpecFileService.update_file(process_model_info, f"{process_model_id_for_bpmn_file}.bpmn", contents.encode())
 
     _commit_and_push_to_git(f"User: {g.user.username} created process model {process_model_info.id}")
