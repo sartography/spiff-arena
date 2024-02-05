235746c91cc5ae5e49b62a90a64d6bf36763423a
Jon Herron <jon.herron@yahoo.com>
2024-02-05 10:52:47 -0500

    Fixing tests

--- spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_kkv_data_store.py
+++ spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_kkv_data_store.py
@@ -23,26 +23,32 @@ def with_clean_data_store(app: Flask, with_db_and_bpmn_file_cleanup: None) -> Ge
     
     db.session.query(KKVDataStoreModel).delete()
     db.session.commit()
-    
-    db.session.add(KKVDataStoreModel(identifier="the_id", name="the_name", location="the_location", schema={}))
+
+    model = KKVDataStoreModel(identifier="the_id", name="the_name", location="the_location", schema={})
+    db.session.add(model)
+    db.session.commit()
+
+    other_model = KKVDataStoreModel(identifier="other_id", name="other_name", location="other_location", schema={})
+    db.session.add(other_model)
     db.session.commit()
     
-    yield
+    yield model
 
 
 @pytest.fixture()
-def with_key1_key2_record(with_clean_data_store: None) -> Generator[None, None, None]:
-    instance_model = db.session.query(KKVDataStoreModel).filter_by(identifier="the_id", location="the_location").first()
-    
-    entry_model = KKVDataStoreEntryModel(instance_id=instance_model.id, top_level_key="key1", secondary_key="key2", value="value1")
-    db.session.add(entry_model)
+def with_key1_key2_record(with_clean_data_store: KKVDataStoreModel) -> Generator[None, None, None]:
+    model = KKVDataStoreEntryModel(instance_id=with_clean_data_store.id, top_level_key="key1", secondary_key="key2", value="value1")
+    db.session.add(model)
     db.session.commit()
     
-    yield
+    yield with_clean_data_store
 
 
 class TestKKVDataStore(BaseTest):
     """Infer from class name."""
+
+    def _entry_count(self, model: KKVDataStoreModel) -> int:
+        return db.session.query(KKVDataStoreEntryModel).filter_by(instance_id=model.id).count()
 
     def test_returns_none_if_no_records_exist(self, with_clean_data_store: None) -> None:
         kkv_data_store = KKVDataStore("the_id", "the_name")
@@ -76,25 +82,25 @@ class TestKKVDataStore(BaseTest):
         result = my_task.data["the_id"]("key1", "key22")
         assert result is None
 
-    def test_can_insert_a_value(self, with_clean_data_store: None) -> None:
+    def test_can_insert_a_value(self, with_clean_data_store: KKVDataStoreModel) -> None:
         kkv_data_store = KKVDataStore("the_id", "the_name")
         my_task = MockTask(data={"the_id": {"newKey1": {"newKey2": "newValue"}}})
         kkv_data_store.set(my_task)
-        count = db.session.query(KKVDataStoreModel).count()
+        count = self._entry_count(with_clean_data_store)
         assert count == 1
 
-    def test_can_insert_mulitple_values_for_same_top_key(self, with_clean_data_store: None) -> None:
+    def test_can_insert_mulitple_values_for_same_top_key(self, with_clean_data_store: KKVDataStoreModel) -> None:
         kkv_data_store = KKVDataStore("the_id", "the_name")
         my_task = MockTask(data={"the_id": {"newKey1": {"newKey2": "newValue", "newKey3": "newValue2"}}})
         kkv_data_store.set(my_task)
-        count = db.session.query(KKVDataStoreModel).count()
+        count = db.session.query(KKVDataStoreEntryModel).filter_by(instance_id=with_clean_data_store.id).count()
         assert count == 2
 
-    def test_can_insert_mulitple_values_for_different_top_key(self, with_clean_data_store: None) -> None:
+    def test_can_insert_mulitple_values_for_different_top_key(self, with_clean_data_store: KKVDataStoreModel) -> None:
         kkv_data_store = KKVDataStore("the_id", "the_name")
         my_task = MockTask(data={"the_id": {"newKey1": {"newKey2": "newValue"}, "newKey3": {"newKey4": "newValue2"}}})
         kkv_data_store.set(my_task)
-        count = db.session.query(KKVDataStoreModel).count()
+        count = db.session.query(KKVDataStoreEntryModel).filter_by(instance_id=with_clean_data_store.id).count()
         assert count == 2
 
     def test_value_is_removed_from_task_data_after_insert(self, with_clean_data_store: None) -> None:
@@ -113,44 +119,44 @@ class TestKKVDataStore(BaseTest):
         result2 = my_task.data["the_id"]("newKey3", "newKey4")
         assert result2 == "newValue2"
 
-    def test_can_update_a_value(self, with_clean_data_store: None) -> None:
+    def test_can_update_a_value(self, with_clean_data_store: KKVDataStore) -> None:
         kkv_data_store = KKVDataStore("the_id", "the_name")
         my_task = MockTask(data={"the_id": {"newKey1": {"newKey2": "newValue"}}})
         kkv_data_store.set(my_task)
         my_task.data = {"the_id": {"newKey1": {"newKey2": "newValue2"}}}
         kkv_data_store.set(my_task)
-        count = db.session.query(KKVDataStoreModel).count()
+        count = self._entry_count(with_clean_data_store)
         assert count == 1
         kkv_data_store.get(my_task)
         result2 = my_task.data["the_id"]("newKey1", "newKey2")
         assert result2 == "newValue2"
 
-    def test_can_delete_record_by_nulling_a_secondary_key(self, with_key1_key2_record: None) -> None:
+    def test_can_delete_record_by_nulling_a_secondary_key(self, with_key1_key2_record: KKVDataStoreModel) -> None:
         kkv_data_store = KKVDataStore("the_id", "the_name")
         my_task = MockTask(data={"the_id": {"key1": {"key2": None}}})
         kkv_data_store.set(my_task)
         kkv_data_store.get(my_task)
         result = my_task.data["the_id"]("key1", "key2")
         assert result is None
-        count = db.session.query(KKVDataStoreModel).count()
+        count = self._entry_count(with_key1_key2_record)
         assert count == 0
 
-    def test_can_delete_all_records_by_nulling_a_top_level_key(self, with_clean_data_store: None) -> None:
+    def test_can_delete_all_records_by_nulling_a_top_level_key(self, with_clean_data_store: KKVDataStoreModel) -> None:
         kkv_data_store = KKVDataStore("the_id", "the_name")
         my_task = MockTask(data={"the_id": {"newKey1": {"newKey2": "newValue", "newKey3": "newValue2"}}})
         kkv_data_store.set(my_task)
         my_task.data = {"the_id": {"newKey1": None}}
         kkv_data_store.set(my_task)
-        count = db.session.query(KKVDataStoreModel).count()
+        count = self._entry_count(with_clean_data_store)
         assert count == 0
 
-    def test_top_key_delete_does_not_delete_for_other_top_levels(self, with_clean_data_store: None) -> None:
+    def test_top_key_delete_does_not_delete_for_other_top_levels(self, with_clean_data_store: KKVDataStoreModel) -> None:
         kkv_data_store = KKVDataStore("the_id", "the_name")
         my_task = MockTask(data={"the_id": {"newKey1": {"newKey2": "newValue"}, "newKey3": {"newKey4": "newValue2"}}})
         kkv_data_store.set(my_task)
         my_task.data = {"the_id": {"newKey1": None}}
         kkv_data_store.set(my_task)
-        count = db.session.query(KKVDataStoreModel).count()
+        count = self._entry_count(with_clean_data_store)
         assert count == 1
         kkv_data_store.get(my_task)
         result = my_task.data["the_id"]("newKey3", "newKey4")
