121c2dbca209329ce043a549d537d86904798a2d
jasquat <jasquat@users.noreply.github.com>
2024-02-08 15:58:29 -0500

    added api endpoint for a generic webhook w/ burnettk

--- spiffworkflow-backend/src/spiffworkflow_backend/routes/process_api_blueprint.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/routes/process_api_blueprint.py
@@ -28,6 +28,7 @@ from spiffworkflow_backend.services.authorization_service import AuthorizationSe
 from spiffworkflow_backend.services.git_service import GitService
 from spiffworkflow_backend.services.process_caller_service import ProcessCallerService
 from spiffworkflow_backend.services.process_instance_processor import ProcessInstanceProcessor
+from spiffworkflow_backend.services.process_instance_service import ProcessInstanceService
 from spiffworkflow_backend.services.process_model_service import ProcessModelService
 
 process_api_blueprint = Blueprint("process_api", __name__)
@@ -217,10 +218,38 @@ def process_data_file_download(
 # npm install -g localtunnel && lt --port 7000 --subdomain oh-so-hot
 # where 7000 is the port the app is running on locally
 def github_webhook_receive(body: dict) -> Response:
-    auth_header = request.headers.get("X-Hub-Signature-256")
-    AuthenticationService.verify_sha256_token(auth_header)
+    _enforce_github_auth()
     result = GitService.handle_web_hook(body)
     return Response(json.dumps({"git_pull": result}), status=200, mimetype="application/json")
+
+
+def webhook(body: dict) -> Response:
+    if current_app.config["SPIFFWORKFLOW_BACKEND_WEBHOOK_ENFORCES_GITHUB_AUTH"] is True:
+        _enforce_github_auth()
+
+    if current_app.config["SPIFFWORKFLOW_BACKEND_WEBHOOK_PROCESS_MODEL_IDENTIFIER"] is None:
+        error_message = "Webhook process model implementation not configured"
+        raise ApiError(
+            error_code="webhook_not_configured",
+            message=error_message,
+            status_code=501,
+        )
+
+    process_model = _get_process_model_for_instantiation(
+        _un_modify_modified_process_model_id(current_app.config["SPIFFWORKFLOW_BACKEND_WEBHOOK_PROCESS_MODEL_IDENTIFIER"])
+    )
+    ProcessInstanceService.create_and_run_process_instance(
+        process_model=process_model,
+        persistence_level="none",
+        data_to_inject={"headers": request.headers, "body": body},
+    )
+
+    return Response(json.dumps({"ok": True}), status=200, mimetype="application/json")
+
+
+def _enforce_github_auth() -> None:
+    auth_header = request.headers.get("X-Hub-Signature-256")
+    AuthenticationService.verify_sha256_token(auth_header)
 
 
 def _get_required_parameter_or_raise(parameter: str, post_body: dict[str, Any]) -> Any:
@@ -357,3 +386,19 @@ def _find_process_instance_for_me_or_raise(
             process_instance.actions = {"read": {"path": target_uri, "method": "GET"}}
 
     return process_instance
+
+
+def _get_process_model_for_instantiation(
+    process_model_identifier: str,
+) -> ProcessModelInfo:
+    process_model = _get_process_model(process_model_identifier)
+    if process_model.primary_file_name is None:
+        raise ApiError(
+            error_code="process_model_missing_primary_bpmn_file",
+            message=(
+                f"Process Model '{process_model_identifier}' does not have a primary"
+                " bpmn file. One must be set in order to instantiate this model."
+            ),
+            status_code=400,
+        )
+    return process_model
