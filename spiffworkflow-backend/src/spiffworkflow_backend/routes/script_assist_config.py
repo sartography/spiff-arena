7ca168e14bcc64f311a1c8926ef87d2923c35879
Tim Consolazio <tcoz@tcoz.com>
2024-02-20 11:34:54 -0500

    Implement API to return if script assist should be enabled
    Along with route and function, api config, etc.

---
+++ spiffworkflow-backend/src/spiffworkflow_backend/routes/script_assist_config.py
@@ -0,0 +1,10 @@
+from flask import make_response
+from flask.wrappers import Response
+from flask import current_app as app
+
+from spiffworkflow_backend.models.process_instance import ProcessInstanceModel
+
+def status() -> Response:
+    ProcessInstanceModel.query.filter().first()
+    response = app.config["SCRIPT_ASSIST_ENABLED"]
+    return make_response({"ok": response}, 200)
