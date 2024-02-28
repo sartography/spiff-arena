c1ed2395e329f71715327c5da4ef3534839b17db
Jon Herron <jon.herron@yahoo.com>
2024-02-28 10:39:21 -0500

    WIP

---
+++ spiffworkflow-backend/src/spiffworkflow_backend/scripts/set_secret.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+from flask import g
+from spiffworkflow_backend.models.script_attributes_context import ScriptAttributesContext
+from spiffworkflow_backend.scripts.script import Script
+from spiffworkflow_backend.services.secret_service import SecretService
+
+
+class SetSecret(Script):
+    def get_description(self) -> str:
+        return "Allows setting a secret value programmatically."
+
+    def run(self, script_attributes_context: ScriptAttributesContext, *args: Any, **kwargs: Any) -> Any:
+        secret_key = args[0]
+        secret_value = args[1]
+        SecretService.update_secret(secret_key, secret_value, g.user.id, True)
