53424b619d2c6ef0f14c9de5460fbfb498ecada1
jasquat <jasquat@users.noreply.github.com>
2024-03-12 16:30:06 -0400

    we can submit a public message form w/ burnettk

--- spiffworkflow-frontend/src/routes/public/MessageStartEventForm.tsx
+++ spiffworkflow-frontend/src/routes/public/MessageStartEventForm.tsx
@@ -4,11 +4,16 @@ import { useParams } from 'react-router-dom';
 import HttpService from '../../services/HttpService';
 import CustomForm from '../../components/CustomForm';
 import Page404 from '../Page404';
+import { recursivelyChangeNullAndUndefined } from '../../helpers';
+import useAPIError from '../../hooks/UseApiError';
 
 export default function MessageStartEventForm() {
   const params = useParams();
   const [messageResponse, setMessageResponse] = useState<any>(null);
   const [taskData, setTaskData] = useState<any>(null);
+  const [formButtonsDisabled, setFormButtonsDisabled] = useState(false);
+  const { addError, removeError } = useAPIError();
+  const [formSubmitResult, setFormSubmitResult] = useState<string | null>(null);
 
   useEffect(() => {
     HttpService.makeCallToBackend({
@@ -21,6 +26,38 @@ export default function MessageStartEventForm() {
     });
   }, [params.modified_message_name]);
 
+  const processSubmitResult = (result: any) => {
+    removeError();
+    console.log('result', result);
+    setFormSubmitResult('YAY IT DONE');
+  };
+
+  const handleFormSubmit = (formObject: any, _event: any) => {
+    if (formButtonsDisabled) {
+      return;
+    }
+
+    const dataToSubmit = formObject?.formData;
+
+    setFormButtonsDisabled(true);
+    removeError();
+
+    recursivelyChangeNullAndUndefined(dataToSubmit, null);
+
+    HttpService.makeCallToBackend({
+      path: `/public/messages/submit/${params.modified_message_name}`,
+      successCallback: processSubmitResult,
+      failureCallback: (error: any) => {
+        addError(error);
+      },
+      httpMethod: 'POST',
+      postBody: dataToSubmit,
+    });
+  };
+
+  if (formSubmitResult) {
+    return <h1>{formSubmitResult}</h1>;
+  }
   if (messageResponse) {
     if (messageResponse.form_schema) {
       return (
@@ -29,11 +66,12 @@ export default function MessageStartEventForm() {
             <Column sm={4} md={5} lg={8}>
               <CustomForm
                 id="form-to-submit"
+                disabled={formButtonsDisabled}
                 formData={taskData}
                 onChange={(obj: any) => {
                   setTaskData(obj.formData);
                 }}
-                onSubmit={() => console.log('WE SUBMIT')}
+                onSubmit={handleFormSubmit}
                 schema={messageResponse.form_schema}
                 uiSchema={messageResponse.form_ui_schema}
                 restrictedWidth
