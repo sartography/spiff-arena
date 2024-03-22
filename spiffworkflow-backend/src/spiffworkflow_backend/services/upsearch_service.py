3001963ad287128fd0123d1bca6b54de3470396b
Jon Herron <jon.herron@yahoo.com>
2024-03-22 13:06:01 -0400

    Add upsearch api

--- spiffworkflow-backend/src/spiffworkflow_backend/services/upsearch_service.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/services/upsearch_service.py
@@ -3,8 +3,8 @@ import os
 
 class UpsearchService:
     @classmethod
-    def upsearch_locations(cls, process_model_identifier: str) -> list[str]:
-        location = process_model_identifier
+    def upsearch_locations(cls, process_model_identifier: str | None) -> list[str]:
+        location = process_model_identifier or ""
         locations = []
 
         while location != "":
