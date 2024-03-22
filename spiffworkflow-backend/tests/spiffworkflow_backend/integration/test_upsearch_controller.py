394f3761fa3e6ed1f8890a1308147c5e825a4a33
Jon Herron <jon.herron@yahoo.com>
2024-03-22 13:21:15 -0400

    Getting ./bin/pyl to pass

--- spiffworkflow-backend/tests/spiffworkflow_backend/integration/test_upsearch_controller.py
+++ spiffworkflow-backend/tests/spiffworkflow_backend/integration/test_upsearch_controller.py
@@ -13,20 +13,19 @@ class TestUpsearchControllerController(BaseTest):
             ("a/b/c/d", ["a/b/c/d", "a/b/c", "a/b", "a", ""]),
             ("a", ["a", ""]),
             ("", [""]),
-
-        ])
+        ],
+    )
     def test_return_upsearch_locations_for_path(
         self,
         app: Flask,
         client: FlaskClient,
         with_db_and_bpmn_file_cleanup: None,
         with_super_admin_user: UserModel,
-            location: str,
-            expected: list[str],
+        location: str,
+        expected: list[str],
     ) -> None:
         response = client.get(
-            f"/v1.0/upsearch-locations?location={location}",
-            headers=self.logged_in_headers(with_super_admin_user)
+            f"/v1.0/upsearch-locations?location={location}", headers=self.logged_in_headers(with_super_admin_user)
         )
 
         assert response.status_code == 200
