b5b8031b44dd14dcf697d8bf7b402160a3773733
jasquat <2487833+jasquat@users.noreply.github.com>
2024-03-25 18:32:44 +0000

    fix-human-task-guid-fk (#1260)
    * removed human_task_ibfk_5 from old migration file and updated task id removal migration to work with both mysql and postgres w/ burnettk
    
    * use sqlalchemy error classes instead of mysql w/ burnettk
    
    * mypy w/ burnettk
    
    * remove deprecated cypress config w/ burnettk
    
    ---------
    
    Co-authored-by: jasquat <jasquat@users.noreply.github.com>

--- spiffworkflow-frontend/cypress.config.js
+++ spiffworkflow-frontend/cypress.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
-const { defineConfig } = require("cypress");
-const { rm } = require("fs/promises");
+const { defineConfig } = require('cypress');
+const { rm } = require('fs/promises');
 
 // yes use video compression in CI, where we will set the env var so we upload to cypress dashboard
 const useVideoCompression = !!process.env.CYPRESS_RECORD_KEY;
@@ -8,15 +8,15 @@ const useVideoCompression = !!process.env.CYPRESS_RECORD_KEY;
 // https://github.com/cypress-io/cypress/issues/2522
 const deleteVideosOnSuccess = (on) => {
   const filesToDelete = [];
-  on("after:spec", (_spec, results) => {
+  on('after:spec', (_spec, results) => {
     if (results.stats.failures === 0 && results.video) {
       filesToDelete.push(results.video);
     }
   });
-  on("after:run", async () => {
+  on('after:run', async () => {
     if (filesToDelete.length) {
       console.log(
-        "after:run hook: Deleting %d video(s) from successful specs",
+        'after:run hook: Deleting %d video(s) from successful specs',
         filesToDelete.length
       );
       await Promise.all(filesToDelete.map((videoFile) => rm(videoFile)));
@@ -33,15 +33,14 @@ if (process.env.SPIFFWORKFLOW_FRONTEND_URL) {
 }
 
 const cypressConfig = {
-  projectId: "crax1q",
+  projectId: 'crax1q',
   defaultCommandTimeout: 20000,
-  videoUploadOnPasses: false,
   chromeWebSecurity: false,
   e2e: {
     baseUrl: spiffWorkflowFrontendUrl,
     setupNodeEvents(on, config) {
       deleteVideosOnSuccess(on);
-      require("@cypress/grep/src/plugin")(config);
+      require('@cypress/grep/src/plugin')(config);
       return config;
     },
   },
@@ -49,7 +48,7 @@ const cypressConfig = {
   // this scrolls away from the elements for some reason with carbon when set to top
   // https://github.com/cypress-io/cypress/issues/2353
   // https://docs.cypress.io/guides/core-concepts/interacting-with-elements#Scrolling
-  scrollBehavior: "center",
+  scrollBehavior: 'center',
 };
 
 if (!process.env.CYPRESS_RECORD_KEY) {
@@ -57,4 +56,4 @@ if (!process.env.CYPRESS_RECORD_KEY) {
   cypressConfig.videoCompression = false;
 }
 
-module.exports = defineConfig(cypressConfig)
+module.exports = defineConfig(cypressConfig);
