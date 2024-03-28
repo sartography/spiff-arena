121f8d15adc6ad78112a9c6a077dcfcaad9709b3
jasquat <2487833+jasquat@users.noreply.github.com>
2024-03-28 19:58:35 +0000

    added a formatter to change a date string into a formatted date in markdown w/ burnettk (#1296)
    Co-authored-by: jasquat <jasquat@users.noreply.github.com>

--- spiffworkflow-frontend/src/services/DateAndTimeService.test.tsx
+++ spiffworkflow-frontend/src/services/DateAndTimeService.test.tsx
@@ -6,21 +6,17 @@ test('it can keep the correct date when converting seconds to date', () => {
   expect(dateString).toEqual('2022-10-21');
 });
 test('it can properly format a duration', () => {
-  expect(DateAndTimeService.formatDurationForDisplay(null, '0')).toEqual('0s');
-  expect(DateAndTimeService.formatDurationForDisplay(null, '60')).toEqual('1m');
-  expect(DateAndTimeService.formatDurationForDisplay(null, '65')).toEqual(
-    '1m 5s'
-  );
-  expect(DateAndTimeService.formatDurationForDisplay(null, 65)).toEqual(
-    '1m 5s'
-  );
-  expect(DateAndTimeService.formatDurationForDisplay(null, 86500)).toEqual(
+  expect(DateAndTimeService.formatDurationForDisplay('0')).toEqual('0s');
+  expect(DateAndTimeService.formatDurationForDisplay('60')).toEqual('1m');
+  expect(DateAndTimeService.formatDurationForDisplay('65')).toEqual('1m 5s');
+  expect(DateAndTimeService.formatDurationForDisplay(65)).toEqual('1m 5s');
+  expect(DateAndTimeService.formatDurationForDisplay(86500)).toEqual(
     '1d 1m 40s'
   );
-  expect(DateAndTimeService.formatDurationForDisplay(null, 2629746)).toEqual(
+  expect(DateAndTimeService.formatDurationForDisplay(2629746)).toEqual(
     '30d 10h 29m 6s'
   );
-  expect(DateAndTimeService.formatDurationForDisplay(null, 31536765)).toEqual(
+  expect(DateAndTimeService.formatDurationForDisplay(31536765)).toEqual(
     '365d 12m 45s'
   );
 });
---
+++ spiffworkflow-frontend/src/services/FormattingService.test.tsx
@@ -0,0 +1,26 @@
+import FormattingService from './FormattingService';
+
+test('it can convert date string to date for display', () => {
+  const markdown =
+    'HEY SPIFF_FORMAT:::convert_date_to_date_for_display(2024-03-01)';
+  expect(FormattingService.checkForSpiffFormats(markdown)).toEqual(
+    'HEY 2024-03-01'
+  );
+});
+
+// timezones for the lose
+// test('it can convert seconds to date time for display', () => {
+//   const markdown =
+//     'HEY SPIFF_FORMAT:::convert_seconds_to_date_time_for_display(10000)';
+//   expect(FormattingService.checkForSpiffFormats(markdown)).toEqual(
+//     'HEY 1969-12-31 21:46:40'
+//   );
+// });
+
+test('it can convert seconds to duration time for display', () => {
+  const markdown =
+    'HEY SPIFF_FORMAT:::convert_seconds_to_duration_for_display(10000)';
+  expect(FormattingService.checkForSpiffFormats(markdown)).toEqual(
+    'HEY 2h 46m 40s'
+  );
+});
