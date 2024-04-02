d09b4604c00d4a80390d79050db54b79d89303f2
jasquat <jasquat@users.noreply.github.com>
2024-04-02 15:42:48 -0400

    tests are all passing now w/ burnettk

--- spiffworkflow-backend/tests/spiffworkflow_backend/integration/test_process_api.py
+++ spiffworkflow-backend/tests/spiffworkflow_backend/integration/test_process_api.py
@@ -10,7 +10,6 @@ import flask
 import pytest
 from flask.app import Flask
 from flask.testing import FlaskClient
-from SpiffWorkflow.util.task import TaskState  # type: ignore
 from spiffworkflow_backend.exceptions.process_entity_not_found_error import ProcessEntityNotFoundError
 from spiffworkflow_backend.models.db import db
 from spiffworkflow_backend.models.process_group import ProcessGroup
@@ -2204,9 +2203,9 @@ class TestProcessApi(BaseTest):
         spiff_task = processor.get_task_by_bpmn_identifier("script_task_two", processor.bpmn_process_instance)
         assert spiff_task is not None
 
-        # TODO: remove these 2 lines if we enable no task data on rehydration again from pr-661
-        assert spiff_task.state == TaskState.ERROR
-        assert spiff_task.data == {"my_var": "THE VAR"}
+        # # TODO: remove these 2 lines if we enable no task data on rehydration again from pr-661
+        # assert spiff_task.state == TaskState.ERROR
+        # assert spiff_task.data == {"my_var": "THE VAR"}
 
         task_model = TaskModel.query.filter_by(guid=str(spiff_task.id)).first()
         assert task_model is not None
