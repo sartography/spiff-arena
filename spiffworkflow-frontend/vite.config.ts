f3091e9a4a0177532bea6e6b022d696c36b185ac
jasquat <jasquat@users.noreply.github.com>
2024-04-11 10:30:20 -0400

    some base updates for vite w/ burnettk

---
+++ spiffworkflow-frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  // depending on your application, base can also be "/"
+  base: '',
+  plugins: [react(), viteTsconfigPaths()],
+  server: {
+    // this ensures that the browser DOES NOT open upon server start
+    open: false,
+    port: 7000,
+  },
+});
