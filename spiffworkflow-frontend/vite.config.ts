cad15d1eb7a61d0784a8abc9244af031e4794e08
burnettk <burnettk@users.noreply.github.com>
2024-04-12 13:54:55 -0400

    use process.env.HOME

--- spiffworkflow-frontend/vite.config.ts
+++ spiffworkflow-frontend/vite.config.ts
@@ -26,8 +26,7 @@ export default defineConfig({
   // },
   resolve: {
     alias: {
-      'bpmn-js':
-        '/home/jason/projects/github/sartography/spiff-arena/spiffworkflow-frontend/node_modules/bpmn-js',
+      'bpmn-js': `${process.env.HOME}/projects/github/sartography/spiff-arena/spiffworkflow-frontend/node_modules/bpmn-js`,
       inferno:
         process.env.NODE_ENV !== 'production'
           ? 'inferno/dist/index.dev.esm.js'
