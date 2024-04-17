ebd5c607584ce21a52684db1c91642be68fcf732
jasquat <jasquat@users.noreply.github.com>
2024-04-17 10:43:15 -0400

    add query params back into admin redirect url w/ burnettk

--- spiffworkflow-frontend/src/routes/AdminRedirect.tsx
+++ spiffworkflow-frontend/src/routes/AdminRedirect.tsx
@@ -4,7 +4,9 @@ import { useNavigate } from 'react-router-dom';
 export default function AdminRedirect() {
   const navigate = useNavigate();
   useEffect(() => {
-    const newPath = window.location.pathname.replace(/^\/admin\//, '/');
+    let newPath = window.location.pathname.replace(/^\/admin\//, '/');
+    const queryParams = window.location.search;
+    newPath = `${newPath}${queryParams}`;
     navigate(newPath);
   }, [navigate]);
 
