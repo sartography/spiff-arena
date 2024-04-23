4134b618bf23335a012d766df377d43eb9468158
jasquat <jasquat@users.noreply.github.com>
2024-04-23 10:39:57 -0400

    tweaks to help with layout for numeric ranges

--- spiffworkflow-frontend/src/rjsf/custom_widgets/NumericRangeField/NumericRangeField.tsx
+++ spiffworkflow-frontend/src/rjsf/custom_widgets/NumericRangeField/NumericRangeField.tsx
@@ -117,17 +117,19 @@ export default function NumericRangeField({
   };
 
   let minHelperText = '';
-  if (minNumber !== undefined) {
-    minHelperText = `Min: ${formatNumberString(minNumber?.toString() || '')}`;
-  }
   let maxHelperText = '';
-  if (maxNumber !== undefined) {
-    maxHelperText = `Max: ${formatNumberString(maxNumber?.toString() || '')}`;
+  if (minNumber !== undefined || maxNumber !== undefined) {
+    minHelperText = `Min: ${
+      formatNumberString(minNumber?.toString() || '') || '∞'
+    }`;
+    maxHelperText = `Max: ${
+      formatNumberString(maxNumber?.toString() || '') || '∞'
+    }`;
   }
 
   return (
     <div className="numeric--range-field-wrapper">
-      <div className="numeric--range-field-label">
+      <div>
         <h5>
           {required
             ? commonAttributes.labelWithRequiredIndicator
@@ -137,7 +139,7 @@ export default function NumericRangeField({
           <div className="markdown-field-desc-text">
             <DescriptionFieldTemplate
               id={descriptionId(idSchema)}
-              description={description}
+              description={`${description} OUR DESCR`}
               schema={schema}
               uiSchema={uiSchema}
               registry={registry}
