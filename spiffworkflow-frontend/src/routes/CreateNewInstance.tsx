9d098282adeb60e22d55206ed2e2585fe4c2d3fc
Mailon Bruno <113206774+mailoncampos@users.noreply.github.com>
2024-04-23 07:39:23 -0400

    feat: traduzido o arquivo CreateNewInstance

--- spiffworkflow-frontend/src/routes/CreateNewInstance.tsx
+++ spiffworkflow-frontend/src/routes/CreateNewInstance.tsx
@@ -3,7 +3,7 @@ import ProcessModelListTiles from '../components/ProcessModelListTiles';
 export default function CreateNewInstance() {
   return (
     <ProcessModelListTiles
-      headerElement={<h2>Processes I can start</h2>}
+      headerElement={<h2>Processos que posso iniciar</h2>}
       checkPermissions={false}
     />
   );
