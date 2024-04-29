8fdfcd4478310de769ac86d4f6affc61f4c8e169
Tim Consolazio <tcoz@tcoz.com>
2024-04-29 10:26:30 -0400

    Configure app to use Poppins
    Was a little tricky, had to isolate this from the rest of the app.

--- spiffworkflow-frontend/src/routes/SpiffUIV2.tsx
+++ spiffworkflow-frontend/src/routes/SpiffUIV2.tsx
@@ -1,29 +1,42 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Divider, Stack } from '@mui/material';
 import SideMenu from '../a-spiffui-v2/components/sidemenu/SideMenu';
 
 export default function SpiffUIV2() {
+  const muiTheme = createTheme({
+    typography: {
+      fontFamily: `"Poppins", "Roboto", "Arial", "Helvetica", sans-serif`,
+      fontSize: 14,
+      fontWeightLight: 300,
+      fontWeightRegular: 400,
+      fontWeightMedium: 500,
+    },
+  });
+
   return (
-    <Stack
-      direction="row"
-      spacing={8}
-      divider={
-        <Divider
-          orientation="vertical"
-          sx={{ height: '100%', backgroundColor: 'grey' }}
-        />
-      }
-      sx={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-      }}
-    >
-      <Stack>
-        <SideMenu />
+    <ThemeProvider theme={muiTheme}>
+      <Stack
+        direction="row"
+        spacing={8}
+        divider={
+          <Divider
+            orientation="vertical"
+            sx={{ height: '100%', backgroundColor: 'grey' }}
+          />
+        }
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <Stack>
+          <SideMenu />
+        </Stack>
+        <Stack>fffffff</Stack>
       </Stack>
-      <Stack>Content</Stack>
-    </Stack>
+    </ThemeProvider>
   );
 }
