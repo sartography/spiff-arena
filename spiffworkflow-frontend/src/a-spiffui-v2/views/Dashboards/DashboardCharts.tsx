54339bc561e3c6b73fcaa74482cfd4e46e85acd6
Tim Consolazio <tcoz@tcoz.com>
2024-05-15 19:48:44 -0400

    Responsive!

--- spiffworkflow-frontend/src/a-spiffui-v2/views/Dashboards/DashboardCharts.tsx
+++ spiffworkflow-frontend/src/a-spiffui-v2/views/Dashboards/DashboardCharts.tsx
@@ -1,10 +1,5 @@
-import { Stack, useTheme } from '@mui/material';
-import {
-  BarChart,
-  PieChart,
-  SparkLineChart,
-  ScatterChart,
-} from '@mui/x-charts';
+import { Paper, Stack, useTheme } from '@mui/material';
+import { BarChart, PieChart, SparkLineChart } from '@mui/x-charts';
 import { useEffect, useState } from 'react';
 
 export default function DashboardCharts({
@@ -35,115 +30,129 @@ export default function DashboardCharts({
 
   return (
     <Stack
-      gap={2}
+      gap={4}
       padding={2}
       direction="row"
       sx={{
+        horizontalAlign: 'center',
         flexWrap: 'wrap',
-        width: '100%',
-        height: '100%',
+        justifyContent: 'center',
       }}
     >
-      <BarChart
-        borderRadius={4}
-        skipAnimation
-        margin={{
-          left: 50,
-          right: 0,
-          top: 20,
-          bottom: 20,
-        }}
-        xAxis={[
-          {
-            scaleType: 'band',
-            data: ['PI Status Counts'],
-          },
-        ]}
-        series={[
-          {
-            label: 'Open',
-            data: [times.openCount],
-            color: palette.warning.main,
-          },
-          {
-            label: 'Complete',
-            data: [times.completeCount],
-            color: palette.success.main,
-          },
-          {
-            label: 'Error',
-            data: [times.errorCount],
-            color: palette.error.main,
-          },
-        ]}
-        slotProps={{ legend: { hidden: true } }}
-        width={250}
-        height={150}
-        leftAxis={null}
-      />
+      <Paper elevation={3} sx={{ padding: 2, borderRadius: 4 }}>
+        <BarChart
+          sx={{
+            borderRadius: 4,
+            margin: 'auto',
+          }}
+          borderRadius={4}
+          skipAnimation
+          margin={{
+            left: 10,
+            right: 0,
+            top: 20,
+            bottom: 20,
+          }}
+          xAxis={[
+            {
+              scaleType: 'band',
+              data: ['PI Status Counts'],
+            },
+          ]}
+          series={[
+            {
+              label: 'Open',
+              data: [times.openCount],
+              color: palette.warning.main,
+            },
+            {
+              label: 'Complete',
+              data: [times.completeCount],
+              color: palette.success.main,
+            },
+            {
+              label: 'Error',
+              data: [times.errorCount],
+              color: palette.error.main,
+            },
+          ]}
+          slotProps={{ legend: { hidden: true } }}
+          width={250}
+          height={150}
+          leftAxis={null}
+        />
+      </Paper>
 
-      <PieChart
-        title="Time Distribution"
-        series={[
-          {
-            data: Object.keys(durations).map((d: string, i: number) => ({
-              id: i,
-              label: durations[d].displayName,
-              value: durations[d].duration,
-            })),
-          },
-        ]}
-        slotProps={{ legend: { hidden: true } }}
-        width={250}
-        height={150}
-        margin={{
-          left: 50,
-          right: 0,
-          top: 20,
-          bottom: 20,
-        }}
-      />
+      <Paper elevation={3} sx={{ padding: 2, borderRadius: 4 }}>
+        <PieChart
+          title="Time Distribution"
+          series={[
+            {
+              data: Object.keys(durations).map((d: string, i: number) => ({
+                id: i,
+                label: durations[d].displayName,
+                value: durations[d].duration,
+              })),
+            },
+          ]}
+          slotProps={{ legend: { hidden: true } }}
+          width={250}
+          height={150}
+          margin={{
+            left: 20,
+            right: 20,
+            top: 20,
+            bottom: 20,
+          }}
+        />
+      </Paper>
 
-      <SparkLineChart
-        data={Object.keys(durations).map((d: string) => durations[d].duration)}
-        margin={{
-          left: 50,
-          right: 0,
-          top: 20,
-          bottom: 20,
-        }}
-        height={150}
-        width={250}
-        curve="natural"
-        area
-        showHighlight
-        showTooltip
-      />
+      <Paper elevation={3} sx={{ padding: 2, borderRadius: 4 }}>
+        <SparkLineChart
+          data={Object.keys(durations).map(
+            (d: string) => durations[d].duration
+          )}
+          margin={{
+            left: 20,
+            right: 20,
+            top: 20,
+            bottom: 20,
+          }}
+          height={150}
+          width={250}
+          curve="natural"
+          area
+          showHighlight
+          showTooltip
+        />
+      </Paper>
 
-      <BarChart
-        borderRadius={4}
-        skipAnimation
-        margin={{
-          left: 50,
-          right: 0,
-          top: 20,
-          bottom: 20,
-        }}
-        xAxis={[
-          {
-            scaleType: 'band',
-            data: ['Duration Distribution'],
-          },
-        ]}
-        series={Object.keys(durations).map((d: string) => ({
-          label: durations[d].displayName,
-          data: [durations[d].duration],
-        }))}
-        slotProps={{ legend: { hidden: true } }}
-        width={250}
-        height={150}
-        leftAxis={null}
-      />
+      <Paper elevation={3} sx={{ padding: 2, borderRadius: 4 }}>
+        <BarChart
+          borderRadius={4}
+          skipAnimation
+          margin={{
+            left: 20,
+            right: 20,
+            top: 20,
+            bottom: 20,
+          }}
+          xAxis={[
+            {
+              scaleType: 'band',
+              data: ['Duration Distribution'],
+            },
+          ]}
+          series={Object.keys(durations).map((d: string) => ({
+            label: durations[d].displayName,
+            data: [durations[d].duration],
+          }))}
+          slotProps={{ legend: { hidden: true } }}
+          width={250}
+          height={150}
+          leftAxis={null}
+        />
+      </Paper>
     </Stack>
   );
 }
