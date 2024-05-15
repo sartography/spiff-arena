04935a06d3d959d152f0d32d4847d641cf7c14e3
burnettk <burnettk@users.noreply.github.com>
2024-05-15 09:53:00 -0400

    print just token to stdout in case we want to shell out to this script for some reason

--- spiffworkflow-backend/bin/local_dev_get_token.py
+++ spiffworkflow-backend/bin/local_dev_get_token.py
@@ -1,21 +1,22 @@
-"""
-get a token object for local development
-"""
+import sys
 
-import requests, base64, json
+"""Get a token object for local development."""
+
+import base64
+import json
+
+import requests
 
 OPEN_ID_CODE = ":this_is_not_secure_do_not_use_in_production"
 USER = "admin"
 client_id = "spiffworkflow-backend"
 client_secret = "JXeQExm0JhQPLumgHtIIqf52bDalHz0q"
 redirect_uri = "http://localhost:7000/v1.0/login_return"
-token_url = 'http://localhost:7000/openid/token'
+token_url = "http://localhost:7000/openid/token"
 
 
-def get_auth_token_object():
-    backend_basic_auth_string = (
-        f"{client_id}:{client_secret}"
-    )
+def get_auth_token_object() -> dict:
+    backend_basic_auth_string = f"{client_id}:{client_secret}"
     backend_basic_auth_bytes = bytes(backend_basic_auth_string, encoding="ascii")
     backend_basic_auth = base64.b64encode(backend_basic_auth_bytes)
     headers = {
@@ -33,6 +34,15 @@ def get_auth_token_object():
     return auth_token_object
 
 
-for k, v in get_auth_token_object().items():
-    print(f"{k}: {v}")
-    print('\n')
+for token_identifier, token in get_auth_token_object().items():
+    # if the k is access_token, print just it to stdout
+    print(f"{token_identifier}:", file=sys.stderr)
+    if token_identifier == "access_token":
+        # print token with no newline
+        print(token, end="")
+        # flush the buffer to stdout
+        sys.stdout.flush()
+        print("\n", file=sys.stderr)
+    else:
+        # print the rest of the key value pairs to stderr
+        print(f"{token}\n", file=sys.stderr)
