4997501f43c437fe581274582bea9a3cf120dcfb
Tim Consolazio <tcoz@tcoz.com>
2024-05-31 08:10:27 -0400

    Config seemed broken (I couldn't start the app, Vite not found etc.)
    I fixed by moving @preact/preset-vite to dependencies (this seems required, probably because it adds types and primitives that are used in runtime code or something) and making sure Node was up to date.

--- spiffworkflow-frontend/vite.config.ts
+++ spiffworkflow-frontend/vite.config.ts
@@ -4,7 +4,7 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
 let host = process.env.HOST ?? 'localhost';
-let port = process.env.PORT ? parseInt(process.env.PORT) : 7001;
+let port = process.env.PORT ? parseInt(process.env.PORT, 10) : 7001;
 
 export default defineConfig({
   // depending on your application, base can also be "/"
