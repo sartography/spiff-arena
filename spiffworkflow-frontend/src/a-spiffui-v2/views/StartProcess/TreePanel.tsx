433c3ce6f492fdd718ea38289ec3a985309d1c0e
Tim Consolazio <tcoz@tcoz.com>
2024-06-01 20:36:42 -0400

    Fully working tree with counters.

--- spiffworkflow-frontend/src/a-spiffui-v2/views/StartProcess/TreePanel.tsx
+++ spiffworkflow-frontend/src/a-spiffui-v2/views/StartProcess/TreePanel.tsx
@@ -1,4 +1,4 @@
-import { Paper } from '@mui/material';
+import { Box, Paper, Stack, useTheme } from '@mui/material';
 import { SimpleTreeView } from '@mui/x-tree-view/SimpleTreeView';
 import { TreeItem } from '@mui/x-tree-view/TreeItem';
 
@@ -7,9 +7,31 @@ export default function TreePanel({
 }: {
   processGroups: Record<string, any>;
 }) {
+  const isDark = useTheme().palette.mode === 'dark';
+  const treeItemStyle = {
+    borderRadius: 1,
+    minWidth: 20,
+    maxHeight: 18,
+    textAlign: 'center',
+    fontWeight: 600,
+    backgroundColor: isDark ? `background.paper` : `background.bluegreymedium`,
+  };
+
   const buildTree = (groups: Record<string, any>[]) => {
     return groups.map((group: Record<string, any>) => (
-      <TreeItem key={group.id} itemId={group.id} label={group.display_name}>
+      <TreeItem
+        key={group.id}
+        itemId={group.id}
+        label={
+          <Stack direction="row">
+            <Box sx={{ width: '100%' }}>{group.display_name}</Box>
+            <Box sx={treeItemStyle}>
+              {(group?.process_models?.length || 0) +
+                (group?.process_groups?.length || 0)}
+            </Box>
+          </Stack>
+        }
+      >
         {group?.process_models?.map((pm: Record<string, any>) => (
           <TreeItem key={pm.id} itemId={pm.id} label={pm.display_name} />
         ))}
@@ -29,6 +51,7 @@ export default function TreePanel({
         borderRightStyle: 'solid',
         padding: 2,
         overflowY: 'auto',
+        overflowX: 'hidden',
       }}
     >
       <SimpleTreeView
@@ -44,82 +67,3 @@ export default function TreePanel({
     </Paper>
   );
 }
-
-// const CustomTreeItem = React.forwardRef(function CustomTreeItem(
-//     props: StyledTreeItemProps,
-//     ref: React.Ref<HTMLLIElement>,
-//   ) {
-//     const theme = useTheme();
-//     const {
-//       id,
-//       itemId,
-//       label,
-//       disabled,
-//       children,
-//       bgColor,
-//       color,
-//       labelIcon: LabelIcon,
-//       labelInfo,
-//       colorForDarkMode,
-//       bgColorForDarkMode,
-//       ...other
-//     } = props;
-
-//     const {
-//       getRootProps,
-//       getContentProps,
-//       getIconContainerProps,
-//       getLabelProps,
-//       getGroupTransitionProps,
-//       status,
-//     } = useTreeItem({ id, itemId, children, label, disabled, rootRef: ref });
-
-//     const style = {
-//       '--tree-view-color': theme.palette.mode !== 'dark' ? color : colorForDarkMode,
-//       '--tree-view-bg-color':
-//         theme.palette.mode !== 'dark' ? bgColor : bgColorForDarkMode,
-//     };
-
-//     return (
-//       <TreeItem2Provider itemId={itemId}>
-//         <CustomTreeItemRoot {...getRootProps({ ...other, style })}>
-//           <CustomTreeItemContent
-//             {...getContentProps({
-//               className: clsx('content', {
-//                 expanded: status.expanded,
-//                 selected: status.selected,
-//                 focused: status.focused,
-//               }),
-//             })}
-//           >
-//             <CustomTreeItemIconContainer {...getIconContainerProps()}>
-//               <TreeItem2Icon status={status} />
-//             </CustomTreeItemIconContainer>
-//             <Box
-//               sx={{
-//                 display: 'flex',
-//                 flexGrow: 1,
-//                 alignItems: 'center',
-//                 p: 0.5,
-//                 pr: 0,
-//               }}
-//             >
-//               <Box component={LabelIcon} color="inherit" sx={{ mr: 1 }} />
-//               <Typography
-//                 {...getLabelProps({
-//                   variant: 'body2',
-//                   sx: { display: 'flex', fontWeight: 'inherit', flexGrow: 1 },
-//                 })}
-//               />
-//               <Typography variant="caption" color="inherit">
-//                 {labelInfo}
-//               </Typography>
-//             </Box>
-//           </CustomTreeItemContent>
-//           {children && (
-//             <CustomTreeItemGroupTransition {...getGroupTransitionProps()} />
-//           )}
-//         </CustomTreeItemRoot>
-//       </TreeItem2Provider>
-//     );
-//   });
