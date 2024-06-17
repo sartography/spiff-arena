3db9cdc4189884d8f666fa3d39d8dc130823f69e
jasquat <jasquat@users.noreply.github.com>
2024-06-17 12:02:49 -0400

    use a regex to determine if a date is potentially valid

--- spiffworkflow-frontend/src/services/DateAndTimeService.test.tsx
+++ spiffworkflow-frontend/src/services/DateAndTimeService.test.tsx
@@ -1,5 +1,42 @@
 import DateAndTimeService from './DateAndTimeService';
 
+test('it can tell if a string looks like a date', () => {
+  const validDates = [
+    '2024-06-01',
+    '14-06-2024',
+    '1-06-2024',
+    '14-6-2024',
+    '14-Jun-2024',
+    '14-March-2024',
+    'March 4, 2024',
+    '4 March 2024',
+    '04/03/2024',
+    '04.03.2024',
+  ];
+  validDates.forEach((dateString: string) => {
+    try {
+      expect(DateAndTimeService.stringLooksLikeADate(dateString)).toBe(true);
+    } catch (exception) {
+      throw Error(`Date failed: ${dateString}: ${exception}`);
+    }
+  });
+
+  const invalidDates = [
+    '2024-06-0',
+    '2024-6-01',
+    '14-06-202',
+    '14-Jun-202',
+    '14#06#20204',
+  ];
+  invalidDates.forEach((dateString: string) => {
+    try {
+      expect(DateAndTimeService.stringLooksLikeADate(dateString)).toBe(false);
+    } catch (exception) {
+      throw Error(`Date failed: ${dateString}: ${exception}`);
+    }
+  });
+});
+
 test('it can keep the correct date when converting seconds to date', () => {
   const dateString =
     DateAndTimeService.convertSecondsToFormattedDateString(1666325400);
@@ -60,12 +97,6 @@ test('it can get the correct date format from string', () => {
   expect(
     DateAndTimeService.attemptToConvertUnknownDateStringFormatToKnownFormat(
       '04-03-2024',
-      newDateFormat,
-    ),
-  ).toEqual(expectedDateString);
-  expect(
-    DateAndTimeService.attemptToConvertUnknownDateStringFormatToKnownFormat(
-      'March 4, 2024',
       newDateFormat,
     ),
   ).toEqual(expectedDateString);
