7a5b3e12af97fa9e32937538576a142cff55aa3f
jasquat <jasquat@users.noreply.github.com>
2024-06-20 14:36:58 -0400

    added tests to ensure migrations do not change w/ burnettk

--- spiffworkflow-backend/src/spiffworkflow_backend/data_migrations/process_instance_migrator.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/data_migrations/process_instance_migrator.py
@@ -1,7 +1,11 @@
+import glob
+import hashlib
+import os
 import time
 from typing import Any
 
 from flask import current_app
+from spiffworkflow_backend.constants import SPIFFWORKFLOW_BACKEND_SERIALIZER_VERSION
 from spiffworkflow_backend.data_migrations.data_migration_base import DataMigrationBase
 from spiffworkflow_backend.data_migrations.version_2 import Version2
 from spiffworkflow_backend.data_migrations.version_3 import Version3
@@ -9,6 +13,10 @@ from spiffworkflow_backend.data_migrations.version_4 import Version4
 from spiffworkflow_backend.data_migrations.version_5 import Version5
 from spiffworkflow_backend.models.db import db
 from spiffworkflow_backend.models.process_instance import ProcessInstanceModel
+
+
+class DataMigrationFilesNotFoundError(Exception):
+    pass
 
 
 # simple decorator to time the func
@@ -70,3 +78,38 @@ class ProcessInstanceMigrator:
             process_instance.spiff_serializer_version = data_migration_version_class.version()
             db.session.add(process_instance)
             db.session.commit()
+
+    @classmethod
+    def get_migration_files(cls) -> list[str]:
+        file_glob = os.path.join(current_app.instance_path, "..", "spiffworkflow_backend", "data_migrations", "version_*.py")
+        files = sorted(glob.glob(file_glob))
+        if len(files) == 0:
+            raise DataMigrationFilesNotFoundError(f"Could not find data migration with expected glob: {file_glob}")
+        return files
+
+    # modified from https://gist.github.com/vinovator/d864555d9e82d25e52fd
+    @classmethod
+    def generate_md5_for_file(cls, file: str, chunk_size: int = 4096) -> str:
+        """
+        Function which takes a file name and returns md5 checksum of the file
+        """
+        hash = hashlib.md5()  # noqa: S324
+        with open(file, "rb") as f:
+            # Read the 1st block of the file
+            chunk = f.read(chunk_size)
+            # Keep reading the file until the end and update hash
+            while chunk:
+                hash.update(chunk)
+                chunk = f.read(chunk_size)
+
+        # Return the hex checksum
+        return hash.hexdigest()
+
+    @classmethod
+    def generate_migration_checksum(cls) -> dict[str, str]:
+        files = cls.get_migration_files()
+        md5checksums = {}
+        for file in files:
+            md5checksum = cls.generate_md5_for_file(file)
+            md5checksums[os.path.basename(file)] = md5checksum
+        return md5checksums
