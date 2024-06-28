b0f8b43f0f462e07dc4a8290da9a55e68e060516
Mailon Bruno <113206774+mailoncampos@users.noreply.github.com>
2024-06-28 10:19:57 -0400

    feat: traduzindo pagina de not found

--- spiffworkflow-frontend/src/routes/Page404.tsx
+++ spiffworkflow-frontend/src/routes/Page404.tsx
@@ -2,7 +2,7 @@ export default function Page404() {
   return (
     <div>
       <h1>404 not found</h1>
-      <p>This page does not exist. Please check the url.</p>
+      <p>Essa pagina não existe, verifique a URL</p>
     </div>
   );
 }
