999388ef573a937852c6c0879bb1d32cdd19a54c
jasquat <jasquat@users.noreply.github.com>
2024-06-28 15:35:45 -0400

    unit tests are passing w/ burnettk

--- spiffworkflow-backend/src/spiffworkflow_backend/constants.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/constants.py
@@ -5,6 +5,6 @@ SPIFFWORKFLOW_BACKEND_DATA_MIGRATION_CHECKSUM = {
     "version_1_3.py": "22636f5ffb8e6d56fa460d82f62a854c",
     "version_2.py": "962b4fda4d466758bdbdc09d75099603",
     "version_3.py": "0e7154d0575c54b59011e3acedebe8b5",
-    "version_4.py": "889399d1c37e230a669d099f3a485fd4",
-    "version_5.py": "a5a9e62798b51741d4dd9d8f69868ded",
+    "version_4.py": "ed8a7a06d21fb3fbc7d1db5435c79058",
+    "version_5.py": "249d9a406098d15b7bf549b51d68c85e",
 }
