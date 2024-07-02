48fe7bcb4bd918233369e3fb7691acf358cebb9a
Jon Herron <jon.herron@yahoo.com>
2024-07-02 11:21:10 -0400

    WIP

--- spiffworkflow-backend/src/spiffworkflow_backend/services/workflow_execution_service.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/services/workflow_execution_service.py
@@ -331,7 +331,6 @@ class TaskModelSavingDelegate(EngineStepDelegate):
         # but it didn't quite work in all cases, so we deleted it. you can find it in commit
         # 1ead87b4b496525df8cc0e27836c3e987d593dc0 if you are curious.
         for waiting_spiff_task in bpmn_process_instance.get_tasks(
-            first_task=self.last_completed_spiff_task, 
             state=TaskState.WAITING
             | TaskState.CANCELLED
             | TaskState.READY
