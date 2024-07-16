4992586719b2a61931fd046d5d06f27645f174da
jasquat <2487833+jasquat@users.noreply.github.com>
2024-07-16 14:27:05 -0400

    pi-migration-delete-tasks (#1929)
    * remove tasks from the db that were removed in the migration
    
    * ensure the correct number of cycles when we migrate a pi
    
    * delete the tasks that spiff returns instead of calculating it in backend
    
    ---------
    
    Co-authored-by: jasquat <jasquat@users.noreply.github.com>

--- spiffworkflow-backend/src/spiffworkflow_backend/services/process_instance_service.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/services/process_instance_service.py
@@ -40,6 +40,7 @@ from spiffworkflow_backend.exceptions.error import ProcessInstanceMigrationNotSa
 from spiffworkflow_backend.exceptions.error import ProcessInstanceMigrationUnnecessaryError
 from spiffworkflow_backend.exceptions.error import UserDoesNotHaveAccessToTaskError
 from spiffworkflow_backend.helpers.spiff_enum import ProcessInstanceExecutionMode
+from spiffworkflow_backend.models.bpmn_process import BpmnProcessModel
 from spiffworkflow_backend.models.bpmn_process_definition import BpmnProcessDefinitionModel
 from spiffworkflow_backend.models.db import db
 from spiffworkflow_backend.models.group import GroupModel
@@ -229,9 +230,9 @@ class ProcessInstanceService:
             subprocesses_diffs,
         ) = cls.check_process_instance_can_be_migrated(process_instance, target_bpmn_process_hash=target_bpmn_process_hash)
 
-        migrate_workflow(top_level_bpmn_process_diff, processor.bpmn_process_instance, target_bpmn_process_spec)
+        deleted_tasks = migrate_workflow(top_level_bpmn_process_diff, processor.bpmn_process_instance, target_bpmn_process_spec)
         for sp_id, sp in processor.bpmn_process_instance.subprocesses.items():
-            migrate_workflow(subprocesses_diffs[sp_id], sp, target_subprocess_specs.get(sp.spec.name))
+            deleted_tasks += migrate_workflow(subprocesses_diffs[sp_id], sp, target_subprocess_specs.get(sp.spec.name))
         processor.bpmn_process_instance.subprocess_specs = target_subprocess_specs
 
         if preserve_old_process_instance:
@@ -280,6 +281,14 @@ class ProcessInstanceService:
                 "target_bpmn_process_hash": target_bpmn_process_hash or "",
             },
         )
+        deleted_task_guids = [str(dt.id) for dt in deleted_tasks]
+        tasks_to_delete = TaskModel.query.filter(TaskModel.guid.in_(deleted_task_guids)).all()  # type: ignore
+        bpmn_processes_to_delete = BpmnProcessModel.query.filter(BpmnProcessModel.guid.in_(deleted_task_guids)).all()  # type: ignore
+        for td in tasks_to_delete:
+            db.session.delete(td)
+        for bpd in bpmn_processes_to_delete:
+            db.session.delete(bpd)
+
         db.session.commit()
 
     @classmethod
