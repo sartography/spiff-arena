116632bbb262cde3fa284e76533f109168e438dc
Emerson Leandro Gregorio <emersonlgregorio@gmail.com>
2025-04-23 09:01:36 -0400

    feat(i18n): add internationalization and translations for Spanish, Finnish, German, and Portuguese (Portugal)
    feat(i18n): add internationalization and translations for Spanish, Finnish, German, and Portuguese (Portugal)

--- spiffworkflow-frontend/src/i18n.js
+++ spiffworkflow-frontend/src/i18n.js
@@ -2,18 +2,26 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import en from './locales/en/translation.json';
-import pt from './locales/pt/translation.json';
+import en_us from './locales/en_us/translation.json';
+import pt_br from './locales/pt_br/translation.json';
+import es from './locales/es/translation.json';
+import de from './locales/de/translation.json';
+import fi from './locales/fi/translation.json';
+import pt_pt from './locales/pt_pt/translation.json';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      en: { translation: en },
-      pt: { translation: pt },
+      'en-US': { translation: en_us },
+      'pt-BR': { translation: pt_br },
+      'es': { translation: es },
+      'de': { translation: de },
+      'fi': { translation: fi },
+      'pt-PT': { translation: pt_pt },
     },
-    fallbackLng: 'en',
+    fallbackLng: 'en-US',
     interpolation: {
       escapeValue: false,
     },
