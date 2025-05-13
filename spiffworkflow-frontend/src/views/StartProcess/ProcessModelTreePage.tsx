94d104623a24c98a96015ef86913380efab77e1d
jasquat <jasquat@users.noreply.github.com>
2025-05-13 13:42:17 -0400

    display top level data stores as well

--- spiffworkflow-frontend/src/views/StartProcess/ProcessModelTreePage.tsx
+++ spiffworkflow-frontend/src/views/StartProcess/ProcessModelTreePage.tsx
@@ -452,7 +452,10 @@ export default function ProcessModelTreePage({
   const dataStoresForProcessGroup = dataStores.filter(
     (dataStore: DataStore) => {
       return (
-        currentProcessGroup && dataStore.location === currentProcessGroup.id
+        (currentProcessGroup &&
+          dataStore.location === currentProcessGroup.id) ||
+        (!currentProcessGroup &&
+          (!dataStore.location || dataStore.location === '/'))
       );
     },
   );
