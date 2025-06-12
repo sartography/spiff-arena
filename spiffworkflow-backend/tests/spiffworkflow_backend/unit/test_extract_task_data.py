a62c8442edfb7638370962aa7884e96e5961d45f
Jon Herron <jon.herron@yahoo.com>
2025-06-12 12:53:49 -0400

    More test cases

--- spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_extract_task_data.py
+++ spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_extract_task_data.py
@@ -5,12 +5,52 @@ from collections import namedtuple
 from spiffworkflow_backend.scripts.extract_from_task_data import ExtractFromTaskData
 from spiffworkflow_backend.models.script_attributes_context import ScriptAttributesContext
 
-@pytest.mark.parametrize("in_task_data, out_task_data, pred, expected", [
-    ({}, {}, None, {})
+MockTask = namedtuple("MockTask", ["data"])
+
+def islower(k: str) -> bool:
+    return k.islower()
+
+@pytest.mark.parametrize("pred, in_task_data, expected, out_task_data", [
+    (None, {}, {}, {}),
+    (None, { "a": 1 }, { "a": 1 }, {}),
+    (lambda k: True, { "a": 1 }, { "a": 1 }, {}),
+    (lambda k: False, { "a": 1 }, {}, { "a": 1 }),
+    (islower, { "a": 1, "B": 2, "c": 3 }, { "a": 1, "c": 3 }, { "B": 2 }),
+    (
+        "efile_",
+        {
+            "efile_var_1": "efile_var_1",
+            "efile_var_2": "efile_var_2",
+            "efile_var_3": "efile_var_3",
+            "efile_var_4": "efile_var_4",
+            "efile_var_5": "efile_var_5",
+        },
+        {
+            "efile_var_1": "efile_var_1",
+            "efile_var_2": "efile_var_2",
+            "efile_var_3": "efile_var_3",
+            "efile_var_4": "efile_var_4",
+            "efile_var_5": "efile_var_5",
+        },
+        {}
+    ),
+    (
+        "efile_",
+        {
+            "efile_var_1": "efile_var_1",
+            "var_2": "var_2",
+        },
+        {
+            "efile_var_1": "efile_var_1",
+        },
+        {
+            "var_2": "var_2",
+        }
+    ),
 ])
-def test_extracts(in_task_data, out_task_data, pred, expected) -> None:
+def test_extracts(pred, in_task_data, expected, out_task_data) -> None:
     script_attributes_context = ScriptAttributesContext(
-        task=namedtuple("MockTask", ["data"])(in_task_data),
+        task=MockTask(in_task_data),
         environment_identifier="testing",
         process_instance_id=1,
         process_model_identifier="my_test",
