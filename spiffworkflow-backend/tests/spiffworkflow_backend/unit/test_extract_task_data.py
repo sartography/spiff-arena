d5375d67f55475d21972694aac39ed11ce1706fb
Jon Herron <jon.herron@yahoo.com>
2025-06-12 12:58:52 -0400

    Linting

--- spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_extract_task_data.py
+++ spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_extract_task_data.py
@@ -1,54 +1,60 @@
+from collections import namedtuple
+from typing import Any
+
 import pytest
 
-from collections import namedtuple
-
-from spiffworkflow_backend.scripts.extract_from_task_data import ExtractFromTaskData
 from spiffworkflow_backend.models.script_attributes_context import ScriptAttributesContext
+from spiffworkflow_backend.scripts.extract_from_task_data import ExtractFromTaskData
 
 MockTask = namedtuple("MockTask", ["data"])
+
 
 def islower(k: str) -> bool:
     return k.islower()
 
-@pytest.mark.parametrize("pred, in_task_data, expected, out_task_data", [
-    (None, {}, {}, {}),
-    (None, { "a": 1 }, { "a": 1 }, {}),
-    (lambda k: True, { "a": 1 }, { "a": 1 }, {}),
-    (lambda k: False, { "a": 1 }, {}, { "a": 1 }),
-    (islower, { "a": 1, "B": 2, "c": 3 }, { "a": 1, "c": 3 }, { "B": 2 }),
-    (
-        "efile_",
-        {
-            "efile_var_1": "efile_var_1",
-            "efile_var_2": "efile_var_2",
-            "efile_var_3": "efile_var_3",
-            "efile_var_4": "efile_var_4",
-            "efile_var_5": "efile_var_5",
-        },
-        {
-            "efile_var_1": "efile_var_1",
-            "efile_var_2": "efile_var_2",
-            "efile_var_3": "efile_var_3",
-            "efile_var_4": "efile_var_4",
-            "efile_var_5": "efile_var_5",
-        },
-        {}
-    ),
-    (
-        "efile_",
-        {
-            "efile_var_1": "efile_var_1",
-            "var_2": "var_2",
-        },
-        {
-            "efile_var_1": "efile_var_1",
-        },
-        {
-            "var_2": "var_2",
-        }
-    ),
-])
-def test_extracts(pred, in_task_data, expected, out_task_data) -> None:
+
+@pytest.mark.parametrize(
+    "pred, in_task_data, expected, out_task_data",
+    [
+        (None, {}, {}, {}),
+        (None, {"a": 1}, {"a": 1}, {}),
+        (lambda k: True, {"a": 1}, {"a": 1}, {}),
+        (lambda k: False, {"a": 1}, {}, {"a": 1}),
+        (islower, {"a": 1, "B": 2, "c": 3}, {"a": 1, "c": 3}, {"B": 2}),
+        (
+            "efile_",
+            {
+                "efile_var_1": "efile_var_1",
+                "efile_var_2": "efile_var_2",
+                "efile_var_3": "efile_var_3",
+                "efile_var_4": "efile_var_4",
+                "efile_var_5": "efile_var_5",
+            },
+            {
+                "efile_var_1": "efile_var_1",
+                "efile_var_2": "efile_var_2",
+                "efile_var_3": "efile_var_3",
+                "efile_var_4": "efile_var_4",
+                "efile_var_5": "efile_var_5",
+            },
+            {},
+        ),
+        (
+            "efile_",
+            {
+                "efile_var_1": "efile_var_1",
+                "var_2": "var_2",
+            },
+            {
+                "efile_var_1": "efile_var_1",
+            },
+            {
+                "var_2": "var_2",
+            },
+        ),
+    ],
+)
+def test_extracts(pred: Any, in_task_data: Any, expected: Any, out_task_data: Any) -> None:
     script_attributes_context = ScriptAttributesContext(
         task=MockTask(in_task_data),
         environment_identifier="testing",
@@ -58,4 +64,4 @@ def test_extracts(pred, in_task_data, expected, out_task_data) -> None:
 
     extracted = ExtractFromTaskData().run(script_attributes_context, pred)
     assert extracted == expected
-    assert script_attributes_context.task.data == out_task_data
+    assert script_attributes_context.task.data == out_task_data  # type: ignore
