7e1cdf2bc1e5980ec8893625040f688154c24e87
jasquat <jasquat@users.noreply.github.com>
2025-07-03 14:12:28 -0400

    some code rabbit w/ burnettk

--- spiffworkflow-backend/app.py
+++ spiffworkflow-backend/app.py
@@ -1,3 +1,6 @@
 from spiffworkflow_backend import create_app
 
+# this is used by flask when running with it directly like:
+#   uv run flask db
+# to run a server, use uvicorn like in ./bin/run_server_locally and ./bin/boot_server_in_docker
 app = create_app().app
