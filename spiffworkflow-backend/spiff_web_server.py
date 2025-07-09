4f38e5247006df7d6f7c4887913ad75c037adaf7
jasquat <jasquat@users.noreply.github.com>
2025-07-09 12:39:17 -0400

    new middleware to fix proxies w/ burnettk

--- spiffworkflow-backend/spiff_web_server.py
+++ spiffworkflow-backend/spiff_web_server.py
@@ -1,6 +1,7 @@
 import os
 
 from spiffworkflow_backend import create_app
+from spiffworkflow_backend.middleware.asgi_proxy_fix import ASGIProxyFix
 from spiffworkflow_backend.services.acceptance_test_fixtures import load_acceptance_test_fixtures
 
 connexion_app = create_app()
@@ -16,16 +17,7 @@ if connexion_app.app.config["SPIFFWORKFLOW_BACKEND_PROXY_COUNT_FOR_PROXY_FIX"]:
     num_proxies = int(connexion_app.app.config["SPIFFWORKFLOW_BACKEND_PROXY_COUNT_FOR_PROXY_FIX"])
 
 if num_proxies > 0:
-    from werkzeug.middleware.proxy_fix import ProxyFix
-
-    # https://flask.palletsprojects.com/en/2.2.x/deploying/proxy_fix/
-    connexion_app.app.wsgi_app = ProxyFix(
-        connexion_app.app.wsgi_app, x_for=num_proxies, x_proto=num_proxies, x_host=num_proxies, x_prefix=num_proxies
-    )
-
-    from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
-
-    connexion_app.add_middleware(ProxyHeadersMiddleware, trusted_hosts=["*"])
+    connexion_app.add_middleware(ASGIProxyFix, x_for=num_proxies, x_proto=num_proxies, x_host=num_proxies, x_prefix=num_proxies)
 
 # this is in here because when we put it in the create_app function,
 # it also loaded when we were running migrations, which resulted in a chicken/egg thing.
