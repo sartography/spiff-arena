e7d0e2532b2ea09a2a87a4c5791a942de70b6a18
jasquat <jasquat@users.noreply.github.com>
2025-07-21 14:19:34 -0400

    instead of upgrading just remove the lib as well as some others w/ burnettk

--- spiffworkflow-frontend/src/index.tsx
+++ spiffworkflow-frontend/src/index.tsx
@@ -52,9 +52,3 @@ const doRender = () => {
 };
 
 doRender();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// @ts-expect-error TS(2554) FIXME: Expected 1 arguments, but got 0.
-reportWebVitals();
