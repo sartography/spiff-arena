bc5b9a14cb3cc20795962542a8b6983b596fb200
burnettk <burnettk@users.noreply.github.com>
2025-09-06 23:42:16 -0400

    styles

--- spiffworkflow-frontend/src/views/FrontendAccessDenied.tsx
+++ spiffworkflow-frontend/src/views/FrontendAccessDenied.tsx
@@ -1,11 +1,25 @@
+import { Typography, Box, Paper } from '@mui/material';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { setPageTitle } from '../helpers';
 
 export default function FrontendAccessDenied() {
   const { t } = useTranslation();
+  
+  useEffect(() => {
+    setPageTitle([t('frontend_access_denied_title')]);
+  }, [t]);
+  
   return (
-    <div>
-      <h1>{t('frontend_access_denied_title')}</h1>
-      <p>{t('frontend_access_denied_message')}</p>
-    </div>
+    <Box sx={{ mt: 2, mb: 4, mx: 2 }}>
+      <Typography variant="h1" sx={{ mb: 2 }}>
+        {t('frontend_access_denied_title')}
+      </Typography>
+      <Paper elevation={2} sx={{ p: 3, maxWidth: '800px' }}>
+        <Typography variant="body1">
+          {t('frontend_access_denied_message')}
+        </Typography>
+      </Paper>
+    </Box>
   );
 }
