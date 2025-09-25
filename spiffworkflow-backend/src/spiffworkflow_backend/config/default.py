caebcaa9df4b71a767d4ae526c901dc80ea131be
burnettk <burnettk@users.noreply.github.com>
2025-09-25 09:03:31 -0400

    lint

--- spiffworkflow-backend/src/spiffworkflow_backend/config/default.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/config/default.py
@@ -215,7 +215,7 @@ config_from_env("SPIFFWORKFLOW_BACKEND_GIT_USERNAME")
 config_from_env("SPIFFWORKFLOW_BACKEND_GIT_USER_EMAIL")
 config_from_env("SPIFFWORKFLOW_BACKEND_GITHUB_WEBHOOK_SECRET")
 config_from_env("SPIFFWORKFLOW_BACKEND_GIT_SSH_PRIVATE_KEY_PATH")
-# If the BPMN spec directory is a subdirectory within a git repo, rather than the entire git repo, this is the path to that subdirectory.
+# If the BPMN spec dir is a subdir within a git repo, rather than the entire git repo, this is the path to that subdir
 config_from_env("SPIFFWORKFLOW_BACKEND_BPMN_SPEC_SUBDIR_WITHIN_REPO")
 
 ### webhook
