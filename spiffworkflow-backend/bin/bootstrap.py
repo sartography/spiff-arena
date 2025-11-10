93da070cf36719f192b7bf6f6ec79e3dfef50879
Jon Herron <jon.herron@yahoo.com>
2025-11-10 12:30:27 -0500

    Bootstrap

--- spiffworkflow-backend/bin/bootstrap.py
+++ spiffworkflow-backend/bin/bootstrap.py
@@ -4,66 +4,78 @@ from spiffworkflow_backend import create_app
 from spiffworkflow_backend.models.db import db
 from spiffworkflow_backend.models.service_account import ServiceAccountModel
 from spiffworkflow_backend.models.user import UserModel
+from spiffworkflow_backend.routes.process_api_blueprint import _get_process_model_for_instantiation
+from spiffworkflow_backend.services.authorization_service import AuthorizationService
+from spiffworkflow_backend.services.process_instance_service import ProcessInstanceService
 from spiffworkflow_backend.services.secret_service import SecretService
 
-api_key_name = "SPIFFWORKFLOW_API_KEY"
-api_key_username = "adminX"
 
-def _tmp() -> None:
-    svc = ServiceAccountModel.query.filter(ServiceAccountModel.name == api_key_username).first()
-    if svc:
-        db.session.delete(svc)
-        
-    usr = UserModel.query.filter(UserModel.username == api_key_username).first()
-    if usr:
-        db.session.delete(usr)
-    
+def _bootstrap_users() -> None:
+    permissions = AuthorizationService.load_permissions_yaml()
+    usernames_to_bootstrap = [k for k, v in permissions["users"].items() if v["service"] == "bootstrap"]
+
+    for username in usernames_to_bootstrap:
+        user = AuthorizationService.create_user_from_sign_in(
+            {
+                "iss": "bootstrap",
+                "sub": username,
+            }
+        )
+
+        db.session.add(user)
+
+        if ServiceAccountModel.query.filter(ServiceAccountModel.user_id == user.id).first():
+            continue
+
+        api_key = ServiceAccountModel.generate_api_key()
+        api_key_hash = ServiceAccountModel.hash_api_key(api_key)
+
+        service_account = ServiceAccountModel(
+            name=user.username,
+            created_by_user_id=user.id,
+            user=user,
+            api_key_hash=api_key_hash,
+        )
+
+        db.session.add(service_account)
+
+        api_key_name = f"SPIFFWORKFLOW_API_KEY_{username}"
+        SecretService.add_secret(api_key_name, api_key, user.id)
+
+        print(f"Bootstrapped user {username} with api key: {api_key}")
+
     db.session.commit()
 
-    try:
-        SecretService.delete_secret(api_key_name, 1)
-    except:
-        pass
 
-def _bootstrap() -> None:
-    admin_user = UserModel.query.filter(UserModel.username == api_key_username).first()
-    if not admin_user:
-        admin_user = UserModel(
-            username=api_key_username,
-            service="http://localhost:8000/openid",
-            service_id=api_key_username,
-        )
-        db.session.add(admin_user)
-        db.session.commit()
-
-    if ServiceAccountModel.query.filter(ServiceAccountModel.user_id == admin_user.id).first():
+def _run_bootstrap_process_model() -> None:
+    process_model_name = os.environ.get("SPIFFWORKFLOW_BACKEND_BOOTSTRAP_PROCESS_MODEL")
+    if not process_model_name:
         return
 
-    api_key = ServiceAccountModel.generate_api_key()
-    api_key_hash = ServiceAccountModel.hash_api_key(api_key)
-    
-    service_account = ServiceAccountModel(
-        name=api_key_username,
-        created_by_user_id=admin_user.id,
-        user=admin_user,
-        api_key_hash=api_key_hash,
+    username = os.environ.get("SPIFFWORKFLOW_BACKEND_BOOTSTRAP_USERNAME")
+    if not username:
+        raise Exception("Bootstrap user not specified.")
+
+    user = UserModel.query.filter(UserModel.username == username).first()
+    if not user:
+        raise Exception(f"Bootstrap user '{username}' not found.")
+
+    print(f"Running bootstrap process model {process_model_name} as {user.username}")
+
+    process_model = _get_process_model_for_instantiation(process_model_name)
+    ProcessInstanceService.create_and_run_process_instance(
+        process_model=process_model,
+        persistence_level="full",
+        user=user,
     )
 
-    db.session.add(service_account)
     db.session.commit()
 
-    SecretService.add_secret(api_key_name, api_key, admin_user.id)
-    
-    print(api_key)
 
 def main() -> None:
-    bootstrap_process_model = os.environ.get("SPIFFWORKFLOW_BACKEND_BOOTSTRAP_PROCESS_MODEL")
-    #if not bootstrap_process_model:
-    #    return
-    app = create_app()
-    with app.app.app_context():
-        _tmp()
-        _bootstrap()
+    with create_app().app.app_context():
+        _bootstrap_users()
+        _run_bootstrap_process_model()
 
 
 if __name__ == "__main__":
--- spiffworkflow-backend/src/spiffworkflow_backend/services/authorization_service.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/services/authorization_service.py
@@ -827,7 +827,7 @@ class AuthorizationService:
         return actions
 
     @classmethod
-    def parse_permissions_yaml_into_group_info(cls) -> list[GroupPermissionsDict]:
+    def load_permissions_yaml(cls) -> Any:
         if current_app.config["SPIFFWORKFLOW_BACKEND_PERMISSIONS_FILE_ABSOLUTE_PATH"] is None:
             raise (
                 PermissionsFileNotSetError(
@@ -835,9 +835,12 @@ class AuthorizationService:
                 )
             )
 
-        permission_configs = None
         with open(current_app.config["SPIFFWORKFLOW_BACKEND_PERMISSIONS_FILE_ABSOLUTE_PATH"]) as file:
-            permission_configs = yaml.safe_load(file)
+            return yaml.safe_load(file)
+
+    @classmethod
+    def parse_permissions_yaml_into_group_info(cls) -> list[GroupPermissionsDict]:
+        permission_configs = cls.load_permissions_yaml()
 
         group_permissions_by_group: dict[str, GroupPermissionsDict] = {}
         if current_app.config["SPIFFWORKFLOW_BACKEND_DEFAULT_USER_GROUP"]:
