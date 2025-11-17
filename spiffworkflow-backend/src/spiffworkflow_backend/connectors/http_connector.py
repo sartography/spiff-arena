dce9e99c1fc79940c063fcc81ac67b98ab6e7d84
Jon Herron <jon.herron@yahoo.com>
2025-11-17 08:54:43 -0500

    Skip real http requests for local calls

--- spiffworkflow-backend/src/spiffworkflow_backend/connectors/http_connector.py
+++ spiffworkflow-backend/src/spiffworkflow_backend/connectors/http_connector.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import requests
+from flask import current_app
 
 from spiffworkflow_backend.config import CONNECTOR_PROXY_COMMAND_TIMEOUT
 
@@ -11,12 +12,22 @@ def does(id: str) -> bool:
 
 def do(id: str, params: dict[str, Any]) -> Any:
     handler = _config[id]["handler"]
+    url = params["url"]
     headers = params.get("headers")
     auth = _auth(params)
     data = params.get("data")
 
-    return handler(  # type: ignore
-        params["url"],
+    if url.startswith("http://local/"):
+        with current_app.test_client() as client:
+            return getattr(client, handler)(  # type: ignore
+                url,
+                query_string=params.get("params"),
+                headers=headers,
+                json=data,
+            )
+
+    return getattr(requests, handler)(  # type: ignore
+        url,
         params.get("params"),
         headers=headers,
         auth=auth,
@@ -55,12 +66,12 @@ _rw_params = [
 ]
 
 _config = {
-    "http/DeleteRequest": {"params": _rw_params, "handler": requests.delete},
-    "http/GetRequest": {"params": _ro_params, "handler": requests.get},
-    "http/HeadRequest": {"params": _ro_params, "handler": requests.head},
-    "http/PatchRequest": {"params": _rw_params, "handler": requests.patch},
-    "http/PostRequest": {"params": _rw_params, "handler": requests.post},
-    "http/PutRequest": {"params": _rw_params, "handler": requests.put},
+    "http/DeleteRequest": {"params": _rw_params, "handler": "delete"},
+    "http/GetRequest": {"params": _ro_params, "handler": "get"},
+    "http/HeadRequest": {"params": _ro_params, "handler": "head"},
+    "http/PatchRequest": {"params": _rw_params, "handler": "patch"},
+    "http/PostRequest": {"params": _rw_params, "handler": "post"},
+    "http/PutRequest": {"params": _rw_params, "handler": "put"},
 }
 
 commands = [{"id": k, "parameters": v["params"]} for k, v in _config.items()]
