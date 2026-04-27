6289bbe3b5b1b5a872aa850382b2cb841f1a7352
jbirddog <jbirddog@users.noreply.github.com>
2026-04-27 14:15:21 +0000

    Remove silly test file

--- spiff-arena-common/tests/spiff_arena_common/test_runner.py
+++
@@ -1,7 +0,0 @@
-def test_bpmn_ids_match_by_exact_string_equality():
-    assert "any_task" == "any_task"
-
-
-def test_bpmn_ids_do_not_normalize_child_suffixes():
-    assert "any_task [child]" != "any_task"
-    assert "any_task" != "any_task [child]"
