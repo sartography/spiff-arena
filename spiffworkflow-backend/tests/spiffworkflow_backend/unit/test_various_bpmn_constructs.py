56c7eaddaf36f703bac3085422e56f1605f11e6f
jasquat <2487833+jasquat@users.noreply.github.com>
2026-08-19 17:13:31 -0400

    Fix terminate end events (#2977)

--- spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_various_bpmn_constructs.py
+++ spiffworkflow-backend/tests/spiffworkflow_backend/unit/test_various_bpmn_constructs.py
@@ -4,6 +4,7 @@ from flask.app import Flask
 from starlette.testclient import TestClient
 
 from spiffworkflow_backend.models.process_instance import ProcessInstanceModel
+from spiffworkflow_backend.models.process_instance import ProcessInstanceStatus
 from spiffworkflow_backend.services.process_instance_runtime import ProcessInstanceRuntime
 from spiffworkflow_backend.services.process_instance_service import ProcessInstanceService
 from tests.spiffworkflow_backend.helpers.base_test import BaseTest
@@ -11,6 +12,21 @@ from tests.spiffworkflow_backend.helpers.test_data import load_test_spec
 
 
 class TestVariousBpmnConstructs(BaseTest):
+    def test_running_process_with_terminate_end_event_completes_cleanly(
+        self,
+        app: Flask,
+        client: TestClient,
+        with_db_and_bpmn_file_cleanup: None,
+    ) -> None:
+        process_model = load_test_spec(
+            process_model_id="test_group/terminate_end_event",
+            process_model_source_directory="terminate_end_event",
+        )
+        process_instance = self.create_process_instance_from_process_model(process_model)
+        runtime = ProcessInstanceRuntime(process_instance)
+        runtime.do_engine_steps(save=True)
+        assert process_instance.status == ProcessInstanceStatus.complete.value
+
     def test_running_process_with_timer_intermediate_catch_event(
         self,
         app: Flask,
